fb673e5704188d8ff28019fd6d8d43f54904bcc2
dngMattEasley <measley@dng.co.uk>
2018-12-03 09:14:02 +0000

    Updated project to Angular 7

---
+++ src/app/applicant-matching/applicant-matching.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-applicant-matching',
+  templateUrl: './applicant-matching.component.html',
+  styleUrls: ['./applicant-matching.component.css']
+})
+export class ApplicantMatchingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
