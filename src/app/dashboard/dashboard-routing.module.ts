77b1954b44d7cdba028772080c534673c2bcedd6
dngAlexA <aagyapong@dng.co.uk>
2019-02-19 10:58:42 +0000

    Extracted Dashboard into MyDashboard and TeamDashboard components

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MyDashboardComponent } from './my-dashboard/my-dashboard.component';
+import { TeamDashboardComponent } from './team-dashboard/team-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'staffmembers/:id/dashboard', component: MyDashboardComponent },
+  { path: 'staffmembers/:id/teammembers/dashboard', component: TeamDashboardComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'staffmembers/:id/dashboard' }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { MyDashboardComponent } from './my-dashboard/my-dashboard.component';
@@ -9,7 +11,10 @@ import { TeamDashboardComponent } from './team-dashboard/team-dashboard.componen
   declarations: [MyDashboardComponent, TeamDashboardComponent],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     DashboardRoutingModule
-  ]
+  ],
+  exports: [MyDashboardComponent, TeamDashboardComponent]
 })
 export class DashboardModule { }
