eafcdd657f8663a7265a9bd2c23dd60557d2eaa6
dngAlexA <aagyapong@dng.co.uk>
2019-03-13 12:12:15 +0000

    Extracted enums from Property  into enums file

---
+++ src/app/property/shared/enums.ts
@@ -0,0 +1,31 @@
+export enum Category {
+  Lettings,
+  LongTerm,
+  ShortTerm,
+  Sales
+}
+
+export enum LetTerm {
+  Short,
+  Long
+}
+
+export enum StatusType {
+  Valuations,
+  Instructions,
+  Viewings,
+  Offers
+}
+
+export enum PropertySaleType {
+  Sales,
+  Lettings,
+  All,
+  None
+}
+
+export enum Managed {
+  Yes,
+  No,
+  Either
+}
--- src/app/property/shared/property.ts
+++ src/app/property/shared/property.ts
@@ -1,35 +1,3 @@
-export enum Category {
-  Lettings,
-  LongTerm,
-  ShortTerm,
-  Sales
-}
-
-export enum LetTerm {
-  Short,
-  Long
-}
-
-export enum StatusType {
-  Valuations,
-  Instructions,
-  Viewings,
-  Offers
-}
-
-export enum PropertySaleType {
-  Sales,
-  Lettings,
-  All,
-  None
-}
-
-export enum Managed {
-  Yes,
-  No,
-  Either
-}
-
 export interface Property {
   propertyType: string;
   propertyId: string;
