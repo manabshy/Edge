28fc122be38ffc831e238446ed280fdfbabfc197
dngAlexA <aagyapong@dng.co.uk>
2019-03-19 15:03:05 +0000

    Changed api version to v10

--- src/app/core/shared/app-constants.ts
+++ src/app/core/shared/app-constants.ts
@@ -1,4 +1,4 @@
 export class AppConstants {
-  public static get leaderboardBaseUrl(): string { return 'http://localhost:57211/v1/staffmembers/leaderboard'; }
-  public static get baseUrl(): string { return'http://localhost:57211/v1/staffmembers'; }
+  public static get leaderboardBaseUrl(): string { return 'http://localhost:57211/v10/staffmembers/leaderboard'; }
+  public static get baseUrl(): string { return'http://localhost:57211/v10/staffmembers'; }
 }
