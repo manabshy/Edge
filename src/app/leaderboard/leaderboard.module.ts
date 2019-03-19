60dbf2f1946d64023cc9b14964ccbc07c0084339
dngElia <efulchignoni@dng.co.uk>
2019-03-19 12:03:45 +0000

    loading and orderBy

--- src/app/leaderboard/leaderboard.module.ts
+++ src/app/leaderboard/leaderboard.module.ts
@@ -7,12 +7,18 @@ import { InstructionsComponent } from './instructions/instructions.component';
 import { ExchangesComponent } from './exchanges/exchanges.component';
 import { CoreModule } from '../core/core.module';
 
+
+
+//vendor
+import { OrderModule } from 'ngx-order-pipe';
+
 @NgModule({
   declarations: [PipelineComponent, InstructionsComponent, ExchangesComponent],
   exports : [PipelineComponent, InstructionsComponent, ExchangesComponent],
   imports: [
     CommonModule,
     LeaderboardRoutingModule,
+    OrderModule,
     CoreModule
   ]
 })
