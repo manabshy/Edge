6dbe12e2c7e4705c7a0b57ff4bdb8e90a72ac27a
dngElia <efulchignoni@dng.co.uk>
2019-03-20 16:41:26 +0000

    add appointment WIP

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,21 +6,28 @@ import { ShortenNamePipe } from './shared/shorten-name.pipe';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AddHeaderInterceptor } from './add-header.interceptor';
 import { CacheInterceptor } from './cache.interceptor';
+
+//bootstrap
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown/';
 import { CollapseModule } from 'ngx-bootstrap/collapse/';
 import { TabsModule } from 'ngx-bootstrap/tabs/';
+import { TimepickerModule } from 'ngx-bootstrap/timepicker';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+
+//components
 import { BreadcrumbComponent } from '../breadcrumb/breadcrumb.component';
 
 @NgModule({
   declarations: [RoundingPipe, TruncatingPipe, ShortenNamePipe, BreadcrumbComponent],
   exports: [RoundingPipe, TruncatingPipe, ShortenNamePipe, BreadcrumbComponent, BsDropdownModule,
-    CollapseModule,
-    TabsModule],
+    CollapseModule, TimepickerModule, TabsModule, BsDatepickerModule],
   imports: [
     CommonModule,
     BsDropdownModule.forRoot(),
     CollapseModule.forRoot(),
     TabsModule.forRoot(),
+    TimepickerModule.forRoot(),
+    BsDatepickerModule.forRoot()
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AddHeaderInterceptor, multi: true },
