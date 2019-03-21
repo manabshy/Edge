006a8829aad301d71adb4f176d10066a0be95e18
dngElia <efulchignoni@dng.co.uk>
2019-03-21 14:09:17 +0000

    gestures + UI fixes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { AuthService } from '../core/services/auth.service';
 import { UserResult, User } from '../core/models/user';
 import { UserService } from '../core/services/user.service';
+
+import { TabDirective, TabsetComponent } from 'ngx-bootstrap/tabs';
 
 @Component({
   selector: 'app-home',
@@ -15,6 +17,11 @@ export class HomeComponent implements OnInit {
   staffMemberId: number;
   userResult: UserResult;
   user: User;
+  selectedTab: number = 0;
+  containerClass: string = '';
+
+  @ViewChild('homeTabs') homeTabs: TabsetComponent;
+
   constructor(private authService: AuthService, private userService: UserService) { }
 
   ngOnInit() {
@@ -32,5 +39,59 @@ export class HomeComponent implements OnInit {
       },
         err => console.log(err)
       );
+  }
+
+  onSelect(data: TabDirective): void {
+    setTimeout(() => {
+      this.selectedTab = data.tabset.tabs.findIndex(item => item.active);
+    });
+  }
+
+  navLeft() {
+    if (this.homeTabs.tabs[this.selectedTab - 1]) {
+      this.homeTabs.tabs[this.selectedTab - 1].active = true;
+      this.containerClass = '';
+    }
+  }
+
+  navRight() {
+    if (this.homeTabs.tabs[this.selectedTab + 1]) {
+      this.homeTabs.tabs[this.selectedTab + 1].active = true;
+      this.containerClass = '';
+    }
+  }
+
+  navLeftHint(event) {
+    if (this.homeTabs.tabs[this.selectedTab - 1] && event.distance > (window.innerWidth * 0.5) && !this.containerClass) {
+      this.containerClass = 'nav-left';
+
+      if(event.isFinal) {
+        this.containerClass = '';
+      }
+    }
+  }
+
+  navRightHint(event) {
+    if (this.homeTabs.tabs[this.selectedTab + 1] && event.distance > (window.innerWidth * 0.5) && !this.containerClass) {
+      this.containerClass = 'nav-right';
+
+      if(event.isFinal) {
+        this.containerClass = '';
+      }
+    }
+  }
+
+  onPanEnd(event) {
+    if (event.distance > (window.innerWidth * 0.5)) {
+      if (event.additionalEvent == "panleft") {
+        this.navRight();
+      } else if (event.additionalEvent == "panright") {
+        this.navLeft();
+      }
+    }
+
+    setTimeout(() => {
+      this.containerClass = '';
+    });
   }
 }
