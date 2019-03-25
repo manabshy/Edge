edb6fcaff9d364d090d8da77bca9e7708ea6eb80
dngAlexA <aagyapong@dng.co.uk>
2019-03-25 08:49:42 +0000

    changed api base urls from localhost

--- src/app/core/shared/app-constants.ts
+++ src/app/core/shared/app-constants.ts
@@ -1,4 +1,6 @@
 export class AppConstants {
-  public static get leaderboardBaseUrl(): string { return 'http://localhost:57211/v10/staffmembers/leaderboard'; }
-  public static get baseUrl(): string { return'http://localhost:57211/v10/staffmembers'; }
+  public static get leaderboardBaseUrl(): string { return 'https://dandg-api-wedge.azurewebsites.net/v10/staffmembers/leaderboard'; }
+  public static get baseUrl(): string { return'https://dandg-api-wedge.azurewebsites.net/v10/staffmembers'; }
+  // public static get leaderboardBaseUrl(): string { return 'http://localhost:57211/v10/staffmembers/leaderboard'; }
+  // public static get baseUrl(): string { return'http://localhost:57211/v10/staffmembers'; }
 }
