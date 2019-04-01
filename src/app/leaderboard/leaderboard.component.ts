7cfbfbf0b82e41ddac939f380b780bf9ac92122c
dngAlexA <aagyapong@dng.co.uk>
2019-04-01 17:22:39 +0100

    Refactored Dashboard service

--- src/app/leaderboard/leaderboard.component.ts
+++ src/app/leaderboard/leaderboard.component.ts
@@ -23,23 +23,28 @@ export class LeaderboardComponent implements OnInit {
   private readonly salesManager = 'salesManager';
   filterVisibility = 'visible';
 
- set selectedPeriod(val: string) {
+  set selectedPeriod(val: string) {
     this._selectedPeriod = val;
     this.selectedPeriodLabel = this.periodList.find(o => o.key === val).value;
-    this.getExchanges( this.salesManager, this.selectedPeriod);
+    this.getExchanges(this.salesManager, this.selectedPeriod);
     this.getInstructions(this.salesManager, this.selectedPeriod, 100);
   }
- get selectedPeriod() {
+  get selectedPeriod() {
     return this._selectedPeriod;
   }
 
-  constructor(private leaderboardService: LeaderboardService, private route: ActivatedRoute) { }
+  constructor(
+    private leaderboardService: LeaderboardService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     //  this.route.data.subscribe(data => {this.pipeline = data['pipelineResolver']; });
-    this.route.queryParams
-      .subscribe(params => this.selectedPeriod = params['period'] || 'ThisQuarter');
-    this.leaderboardService.getStaffMemberPipeline(this.salesManager)
+    this.route.queryParams.subscribe(
+      params => (this.selectedPeriod = params['period'] || 'ThisQuarter')
+    );
+    this.leaderboardService
+      .getStaffMemberPipeline(this.salesManager)
       .subscribe(result => {
         this.pipelineList = result;
         console.log(`The pipeline result is: ${this.pipelineList}`);
@@ -47,19 +52,26 @@ export class LeaderboardComponent implements OnInit {
   }
 
   getExchanges(role: string, period: string) {
-    this.leaderboardService.getStaffMemberExchanges(role, period).subscribe(result => {
-      this.exchanges = result;
-    });
+    this.leaderboardService
+      .getStaffMemberExchanges(role, period)
+      .subscribe(result => {
+        this.exchanges = result;
+      });
   }
+
   getInstructions(role: string, period: string, pageSize: any) {
-    this.leaderboardService.getStaffMemberInstructions(role, period, pageSize).subscribe(result => {
-      this.originalInstructions = result;
-      if (result !== null) {
-        this.instructions = result.slice(0, 16);
-      } else {
-        console.log(`No instructions ${this.selectedPeriodLabel}: ${this.instructions}`);
-      }
-    });
+    this.leaderboardService
+      .getStaffMemberInstructions(role, period, pageSize)
+      .subscribe(result => {
+        this.originalInstructions = result;
+        if (result !== null) {
+          this.instructions = result.slice(0, 16);
+        } else {
+          console.log(
+            `No instructions ${this.selectedPeriodLabel}: ${this.instructions}`
+          );
+        }
+      });
   }
 
   showFilter(val: string) {
