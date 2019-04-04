a3d4884d9e7bfa5b4d008373b604d20a542f37d4
dngAlexA <aagyapong@dng.co.uk>
2019-04-04 16:22:08 +0100

    Added getContactGroupById (WIP)

--- src/app/contactgroups/shared/contact-groups.service.ts
+++ src/app/contactgroups/shared/contact-groups.service.ts
@@ -1,20 +1,32 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { AppConstants } from 'src/app/core/shared/app-constants';
 import { ContactGroupAutoCompleteResult, ContactGroupAutoCompleteData } from './contact-group';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactGroupsService {
-
+private contactGroups: ContactGroupAutoCompleteResult[];
   constructor(private http: HttpClient) { }
 
   // get all the people that belong to a contact group
   getAutocompleteContactGroups(searchTerm: string): Observable<ContactGroupAutoCompleteResult[]> {
     const url = `${AppConstants.baseContactGroupUrl}/search?SearchTerm=${searchTerm}`;
+    if (this.contactGroups) {
+      return of(this.contactGroups);
+    }
+    return this.http.get<ContactGroupAutoCompleteData>(url)
+    .pipe(
+         map(response => response.result),
+         tap(data => this.contactGroups = data),
+         tap(data => console.log(JSON.stringify(data)))
+      );
+  }
+  getContactGroupbyId(contactGroupId: number): Observable<ContactGroupAutoCompleteResult[]> {
+    const url = `${AppConstants.baseContactGroupUrl}/{ContactGroupId}?ContactGroupId=${contactGroupId}`;
     return this.http.get<ContactGroupAutoCompleteData>(url).pipe(map(response => response.result));
   }
 }
