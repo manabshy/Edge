44e8571e4aea99e60bdcb7155281505ace8b128e
dngAlexA <aagyapong@dng.co.uk>
2019-04-26 11:08:51 +0100

    small refactoring

--- src/app/dashboard/dashboard-list/dashboard-list.component.ts
+++ src/app/dashboard/dashboard-list/dashboard-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Pipeline, Instruction, Applicant, Valuation } from '../shared/dashboard';
+import { Instruction, Applicant, Valuation } from '../shared/dashboard';
 import { Constants } from 'src/app/core/shared/period-list';
 import { DashboardService } from '../shared/dashboard.service';
 import { ActivatedRoute } from '@angular/router';
@@ -32,18 +32,11 @@ export class DashboardListComponent implements OnInit {
   get selectedPeriod() {
     return this._selectedPeriod;
   }
-  // set selectedInstruction(address: string) {
-  //  this.instruction = this.instructions.find(x => x.propertyAddress === address);
-  // }
-  // get selectedInstruction() {
-  //   return this.instruction;
-  // }
 
   constructor( private dashboardService: DashboardService, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.staffMemberId = 0;
-    console.log('before getting id from route', this.staffMemberId);
     this.route.queryParams.subscribe(params =>  {
       this.selectedPeriod = params['periodFilter'] || 'ThisQuarter';
       this.isValuation = params['isValuation'];  } );
@@ -52,28 +45,24 @@ export class DashboardListComponent implements OnInit {
        this.staffMemberId = +this.route.snapshot.paramMap.get('id') || 0;
        this.getDashboardInstructions(this.staffMemberId, this.role, this.selectedPeriod, 100);
        this.getDashboardValuations(this.staffMemberId, this.role, this.selectedPeriod, 100);
-       console.log('after getting id from route', this.staffMemberId);
       });
   }
   getDashboardInstructions(id: number, role: string, period?: string, pageSize?: number): void {
     this.dashboardService.getDashboardInstructions(id, role, period, pageSize)
       .subscribe(result => {
         this.instructions = result;
-       console.log(this.instructions);
       });
   }
   getDashboardValuations(id: number, role: string, period?: string, pageSize?: number): void {
     this.dashboardService.getDashboardValuations(id, role, period, pageSize)
       .subscribe(result => {
         this.valuations = result;
-       console.log(this.valuations);
       });
   }
   getDashboardApplicants(id: number, role: string): void {
     this.dashboardService.getDashboardApplicants(id, role)
       .subscribe(data => {
         this.applicants = data.result;
-        console.log(this.applicants);
       });
   }
 
