0ca5add217ee5155eef71c44a74b763630c1f5cf
dngElia <efulchignoni@dng.co.uk>
2019-04-30 14:57:14 +0100

    diary half hours

--- src/app/diary/diary.component.ts
+++ src/app/diary/diary.component.ts
@@ -26,6 +26,7 @@ export class DiaryComponent implements OnInit {
   viewMode = 'workingWeek';
   setTodayLabel = 'This ' + this.viewMode;
   monthLabel = dayjs().month(this.viewedMonth).format('MMMM');
+  hours: any[] = ['00','01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23'];
   @ViewChildren(PopoverDirective) popovers: QueryList<PopoverDirective>;
 
   appUtils = AppUtils;
