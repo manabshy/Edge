c89460bcead22cbddf43b0623ff09d1c797714d3
dngAlexA <aagyapong@dng.co.uk>
2019-04-30 16:35:35 +0100

    Move getters for dashboard list into my dashboard component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -39,41 +39,41 @@ export class DashboardComponent implements OnInit {
   private _selectedPeriod: string;
   periodList = Constants.PeriodList;
   private readonly role = 'salesManager';
-   valuationTile: Tiles = Tiles.Valuations;
-   bddTile: Tiles = Tiles.BusinessDevelopment;
-   allInstructionsTile: Tiles = Tiles.AllInstructions;
-   exchangesTile = Tiles.Exchanges;
-   pipelineTile = Tiles.Pipeline;
-   instructionsTile = Tiles.Instructions;
+  //  valuationTile: Tiles = Tiles.Valuations;
+  //  bddTile: Tiles = Tiles.BusinessDevelopment;
+  //  allInstructionsTile: Tiles = Tiles.AllInstructions;
+  //  exchangesTile = Tiles.Exchanges;
+  //  pipelineTile = Tiles.Pipeline;
+  //  instructionsTile = Tiles.Instructions;
   @ViewChild('dashboardTabs') dashboardTabs: TabsetComponent;
   selectedTab: number;
-  get valuations(): string {
-    return this.valuationTile = Tiles.Valuations;
-  }
-  get allInstructions(): string{
-    return this.allInstructionsTile = Tiles.AllInstructions;
-  }
-  get bdd(): string {
-    return this.bddTile = Tiles.BusinessDevelopment;
-  }
-  get isValuation(): boolean {
-    return this.valuationTile === Tiles.Valuations;
-  }
-  get isInstructions(): boolean {
-    return this.instructionsTile === Tiles.Instructions;
-  }
-  get isAllInstructions(): boolean {
-    return this.allInstructions === Tiles.AllInstructions;
-  }
-  get isBdd(): boolean {
-    return this.bddTile === Tiles.BusinessDevelopment;
-  }
-  get isPipeline(): boolean {
-    return this.pipelineTile === Tiles.Pipeline;
-  }
-  get isExchanges(): boolean {
-    return this.exchangesTile === Tiles.Exchanges;
-  }
+  // get valuations(): string {
+  //   return this.valuationTile = Tiles.Valuations;
+  // }
+  // get allInstructions(): string{
+  //   return this.allInstructionsTile = Tiles.AllInstructions;
+  // }
+  // get bdd(): string {
+  //   return this.bddTile = Tiles.BusinessDevelopment;
+  // }
+  // get isValuation(): boolean {
+  //   return this.valuationTile === Tiles.Valuations;
+  // }
+  // get isInstructions(): boolean {
+  //   return this.instructionsTile === Tiles.Instructions;
+  // }
+  // get isAllInstructions(): boolean {
+  //   return this.allInstructions === Tiles.AllInstructions;
+  // }
+  // get isBdd(): boolean {
+  //   return this.bddTile === Tiles.BusinessDevelopment;
+  // }
+  // get isPipeline(): boolean {
+  //   return this.pipelineTile === Tiles.Pipeline;
+  // }
+  // get isExchanges(): boolean {
+  //   return this.exchangesTile === Tiles.Exchanges;
+  // }
 
   set selectedPeriod(val: string) {
     this._selectedPeriod = val;
--- src/app/dashboard/my-dashboard/my-dashboard.component.ts
+++ src/app/dashboard/my-dashboard/my-dashboard.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Dashboard, Tiles } from '../shared/dashboard';
-import {RoundingPipe} from '../../core/shared/rounding.pipe';
 @Component({
   selector: 'app-my-dashboard',
   templateUrl: './my-dashboard.component.html',
@@ -10,18 +9,53 @@ export class MyDashboardComponent implements OnInit {
 @Input() myDashboard: Dashboard;
 @Input() period: string;
 @Input() periodKey: string;
-@Input() valuationTile: Tiles;
-@Input() allInstructionsTile: Tiles;
-@Input() instructionsTile: Tiles;
-@Input() exchangesTile: Tiles;
-@Input() bddTile: Tiles;
-@Input() pipelineTile: Tiles;
-@Input() isValuation: boolean;
-@Input() isAllInstructions: boolean;
-@Input() isInstructions: boolean;
-@Input() isBdd: boolean;
-@Input() isExchanges: boolean;
-@Input() isPipeline: boolean;
+// @Input() valuationTile: Tiles;
+// @Input() allInstructionsTile: Tiles;
+// @Input() instructionsTile: Tiles;
+// @Input() exchangesTile: Tiles;
+// @Input() bddTile: Tiles;
+// @Input() pipelineTile: Tiles;
+// @Input() isValuation: boolean;
+// @Input() isAllInstructions: boolean;
+// @Input() isInstructions: boolean;
+// @Input() isBdd: boolean;
+// @Input() isExchanges: boolean;
+// @Input() isPipeline: boolean;
+
+valuationTile: Tiles = Tiles.Valuations;
+bddTile: Tiles = Tiles.BusinessDevelopment;
+allInstructionsTile: Tiles = Tiles.AllInstructions;
+exchangesTile = Tiles.Exchanges;
+pipelineTile = Tiles.Pipeline;
+instructionsTile = Tiles.Instructions;
+
+get valuations(): string {
+ return this.valuationTile = Tiles.Valuations;
+}
+get allInstructions(): string{
+ return this.allInstructionsTile = Tiles.AllInstructions;
+}
+get bdd(): string {
+ return this.bddTile = Tiles.BusinessDevelopment;
+}
+get isValuation(): boolean {
+ return this.valuationTile === Tiles.Valuations;
+}
+get isInstructions(): boolean {
+ return this.instructionsTile === Tiles.Instructions;
+}
+get isAllInstructions(): boolean {
+ return this.allInstructions === Tiles.AllInstructions;
+}
+get isBdd(): boolean {
+ return this.bddTile === Tiles.BusinessDevelopment;
+}
+get isPipeline(): boolean {
+ return this.pipelineTile === Tiles.Pipeline;
+}
+get isExchanges(): boolean {
+ return this.exchangesTile === Tiles.Exchanges;
+}
 
   constructor() { }
 
