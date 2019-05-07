06a80815f43a22fc855ff1ee0e72ca9538e83e0e
dngAlexA <aagyapong@dng.co.uk>
2019-05-07 08:52:43 +0100

    Refactor instructions and bdd component

--- src/app/dashboard/dashboard-list/instructions-and-business-development/instructions-and-business-development.component.ts
+++ src/app/dashboard/dashboard-list/instructions-and-business-development/instructions-and-business-development.component.ts
@@ -19,81 +19,49 @@ priceRangeLet: number[];
 filteredInstructions: Instruction[] = [];
 isFakeLoadingVisible = false;
 
-get searchTerm(): string {
-  return this._searchTerm;
-}
-set searchTerm(val: string) {
-  this._searchTerm = val;
- // this.filteredInstructions = this.searchTerm ? this.performFilter(this.searchTerm) : this.allInstructions;
-}
+  get searchTerm(): string {
+    return this._searchTerm;
+  }
+  set searchTerm(val: string) {
+    this._searchTerm = val;
+  }
 
-get minPrice() {
-  return this._minPrice;
-}
-set minPrice(val: number) {
-  this._minPrice = val;
- // this.filteredInstructions = this.minPrice > 0 ? this.performMinPriceFilter(this.minPrice) : this.allInstructions;
-}
+  get minPrice() {
+    return this._minPrice;
+  }
+  set minPrice(val: number) {
+    this._minPrice = val;
+  }
 
-get maxPrice() {
-  return this._maxPrice;
-}
-set maxPrice(val: number) {
-  this._maxPrice = val;
+  get maxPrice() {
+    return this._maxPrice;
+  }
+  set maxPrice(val: number) {
+    this._maxPrice = val;
+  }
 
- // this.filteredInstructions = this.maxPrice > 0 ? this.performMaxPriceFilter(this.maxPrice) : this.allInstructions;
-}
-
-get bedroomValues() {
-  return MinBedrooms;
-}
+  get bedroomValues() {
+    return MinBedrooms;
+  }
   constructor(private sharedService: SharedService) {
     this.priceRangeSale = this.sharedService.priceRangeSale();
     this.priceRangeLet = this.sharedService.priceRangeLet();
   }
 
   ngOnInit() {
-
   }
 
-ngOnChanges() {
-  if (this.allInstructions) {
-    this.filteredInstructions = this.allInstructions;
-  }
-}
-
-  performFilter(filterBy: string): Instruction[] {
-  filterBy = filterBy.toLocaleLowerCase();
-  this.fakeLoading();
-  if (this.filteredInstructions.length) {
-    return this.filteredInstructions.filter((instruction: Instruction) =>
-    instruction.propertyAddress.toLocaleLowerCase().indexOf(filterBy) !== -1);
-  }
-  return this.allInstructions.filter((instruction: Instruction) =>
-  instruction.propertyAddress.toLocaleLowerCase().indexOf(filterBy) !== -1);
-  }
-
-  performMaxPriceFilter(filterBy: number): Instruction[] {
-    this.fakeLoading();
-    if (this.filteredInstructions.length) {
-      return this.filteredInstructions.filter(instruction => instruction.salesValue < filterBy || instruction.salesValue == filterBy);
+  ngOnChanges() {
+    if (this.allInstructions) {
+      this.filteredInstructions = this.allInstructions;
     }
-    return this.allInstructions.filter(instruction => instruction.salesValue < filterBy || instruction.salesValue == filterBy);
-  }
-
-  performMinPriceFilter(filterBy: number): Instruction[] {
-    this.fakeLoading();
-    if (this.filteredInstructions) {
-      return this.filteredInstructions.filter(instruction => instruction.salesValue > filterBy || instruction.salesValue == filterBy );
-     }
-   return this.allInstructions.filter(instruction => instruction.salesValue > filterBy || instruction.salesValue == filterBy );
   }
 
   searchInstructions(maxPriceFilter?: number, minPriceFilter?: number, searchTermFilter?: string): Instruction[] {
     this.fakeLoading();
     if (minPriceFilter == 0 && maxPriceFilter == 0 && !searchTermFilter) {
-     return this.allInstructions;
-   }
+    return this.allInstructions;
+  }
     searchTermFilter = searchTermFilter.toLocaleLowerCase();
     return this.filteredInstructions = this.allInstructions.filter(instruction =>
       (instruction.salesValue >= minPriceFilter || minPriceFilter == 0) &&
