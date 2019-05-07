cdf92f861825410ac981b7d57ab6d9d03315ed7b
dngAlexA <aagyapong@dng.co.uk>
2019-05-07 14:00:09 +0100

    Add constants for the authService

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -11,10 +11,11 @@ import { ConfirmModalComponent } from '../confirm-modal/confirm-modal.component'
 })
 export class AuthService {
   private user: UserForAuthentication;
-  endpointUrl = AppConstants.endpointUrl;
   private adalConfig = {
-    tenant: 'ed781348-2f1d-4f1e-bbf8-137da318df39',
-    clientId: '03d5d394-2418-42fa-a345-556b8d7ffcdb',
+    tenant: AppConstants.tenant,
+    clientId: AppConstants.clientId,
+    // tenant: 'ed781348-2f1d-4f1e-bbf8-137da318df39',
+    // clientId: '03d5d394-2418-42fa-a345-556b8d7ffcdb',
     // redirectUri: 'https://stagedouglasandgordon-wedge.azurewebsites.net/auth-callback',
     // postLogoutRedirectUri: 'https://stagedouglasandgordon-wedge.azurewebsites.net',
     redirectUri: 'http://localhost:4200/auth-callback',
--- src/app/core/shared/app-constants.ts
+++ src/app/core/shared/app-constants.ts
@@ -7,8 +7,12 @@ export class AppConstants {
   public static get clientId(): string { return'03d5d394-2418-42fa-a345-556b8d7ffcdb'; }
   public static get redirectUri(): string { return 'https://stagedouglasandgordon-wedge.azurewebsites.net/auth-callback'; }
   public static get postLogoutRedirectUri(): string { return 'https://stagedouglasandgordon-wedge.azurewebsites.net'; }
-  public static get endpointUrl(): string { return 'https://dandg-api-wedge.azurewebsites.net'; }
+  public static get azureEndpointUrl(): string { return 'https://dandg-api-wedge.azurewebsites.net'; }
+  // public static get redirectUri(): string { return 'http://localhost:4200/auth-callback'; }
+  // public static get postLogoutRedirectUri(): string { return  'http://localhost:4200'; }
+   public static get localEndpointUrl(): string { return '\'http://localhost:57211\': \'67f9a9a1-d8de-45bc-af20-43e1e18ccba5\''; }
   // public static get leaderboardBaseUrl(): string { return 'http://localhost:57211/v10/staffmembers/leaderboard'; }
   // public static get baseUrl(): string { return'http://localhost:57211/v10/staffMembers'; }
   // public static get baseContactGroupUrl(): string { return'http://localhost:57211/v10/contactGroups'; }
+
 }
