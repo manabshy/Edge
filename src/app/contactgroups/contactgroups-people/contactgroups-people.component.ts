01ebb4beda4d5ac59978e4b33e50bbf4175baa3a
dngAlexA <aagyapong@dng.co.uk>
2019-05-17 11:45:50 +0100

    Add contact group details

--- src/app/contactgroups/contactgroups-people/contactgroups-people.component.ts
+++ src/app/contactgroups/contactgroups-people/contactgroups-people.component.ts
@@ -3,6 +3,7 @@ import { ContactGroupsService } from '../shared/contact-groups.service';
 import { ActivatedRoute } from '@angular/router';
 import { Person } from 'src/app/core/models/person';
 import { ContactGroup } from '../shared/contact-group';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-contactgroups-people',
@@ -16,7 +17,8 @@ export class ContactgroupsPeopleComponent implements OnInit {
   contactGroupId: number;
   contactPeople: Person[];
   contactGroupDetails: ContactGroup;
-  constructor(private contactGroupService: ContactGroupsService, private route: ActivatedRoute) { }
+  contactGroupDetailsForm: FormGroup;
+  constructor(private contactGroupService: ContactGroupsService, private fb: FormBuilder, private route: ActivatedRoute) { }
 
   ngOnInit() {
      this.route.params.subscribe(params => {
@@ -24,25 +26,42 @@ export class ContactgroupsPeopleComponent implements OnInit {
        this.groupPersonId = +params['groupPersonId'] || 0;
        this.personId = +params['personId'] || 0;
       });
+      this.contactGroupDetailsForm = this.fb.group({
+       salutation: [''],
+       addressee: [''],
+       comments: ['']
+      });
      this.getContactGroupById(this.contactGroupId);
   }
 
   getContactGroupById(contactGroupId: number) {
     this.contactGroupService.getContactGroupbyId(contactGroupId).subscribe(data => {
       this.contactGroupDetails = data;
+      this.populateFormDetails(data);
       console.log('contact group people', this.contactGroupDetails);
     });
   }
+  populateFormDetails(contactGroup: ContactGroup) {
+    if (this.contactGroupDetailsForm) {
+      this.contactGroupDetailsForm.reset();
+    }
+      this.contactGroupDetails = contactGroup;
+      this.contactGroupDetailsForm.patchValue({
+        salutation: contactGroup.salutation,
+        addressee : contactGroup.addressee,
+        comments: contactGroup.comments
+      });
+  }
 
   removePerson(event, id: number) {
-    event.preventDefault(); 
-    event.stopPropagation(); 
+    event.preventDefault();
+    event.stopPropagation();
     console.log('id of person to be removed is:', id);
   }
 
   showHideMarkPrefs(event, i) {
-    event.preventDefault(); 
-    event.stopPropagation(); 
-    this.isCollapsed[i] = !this.isCollapsed[i]
+    event.preventDefault();
+    event.stopPropagation();
+    this.isCollapsed[i] = !this.isCollapsed[i];
   }
 }
