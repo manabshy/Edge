25c24800aad1635db5657d9e91a19092157426ac
dngElia <efulchignoni@dng.co.uk>
2019-06-04 17:32:23 +0100

    fix validation

--- src/app/contactgroups/contactgroups-detail-edit/contactgroups-detail-edit.component.ts
+++ src/app/contactgroups/contactgroups-detail-edit/contactgroups-detail-edit.component.ts
@@ -59,7 +59,8 @@ export class ContactgroupsDetailEditComponent implements OnInit {
     },
     'emailAddress': {
       'email': {
-        required: ' Email is required.'
+        required: ' Email is required.',
+        pattern: 'Email is not valid.'
       },
     },
     'email': {
@@ -142,7 +143,7 @@ export class ContactgroupsDetailEditComponent implements OnInit {
     } else {
       this.getPersonDetails(id);
     }
-    this.personForm.valueChanges.pipe(debounceTime(1000))
+    this.personForm.valueChanges.pipe(debounceTime(500))
       .subscribe((data) => {
         this.postCode.setValue(this.sharedService.formatPostCode(data.address.postCode), { emitEvent: false });
         this.logValidationErrors(this.personForm, false);
@@ -159,6 +160,9 @@ export class ContactgroupsDetailEditComponent implements OnInit {
     Object.keys(group.controls).forEach((key: string) => {
       const control = group.get(key);
       const messages = this.validationMessages[key];
+      if(control.valid) {
+        this.formErrors[key] = '';
+      }
       if (control && !control.valid && (fakeTouched || control.touched)) {
         this.formErrors[key] = '';
         for (const errorKey in control.errors) {
