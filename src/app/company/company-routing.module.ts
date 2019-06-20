f9971d2a83dc138aebfa6a4d7496a0f02566c5b7
dngAlexA <aagyapong@dng.co.uk>
2019-06-20 17:20:41 +0100

    Company centre (WIP)

--- src/app/company/company-routing.module.ts
+++ src/app/company/company-routing.module.ts
@@ -2,27 +2,27 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuardService } from '../core/services/auth-guard.service';
 import { CompanyComponent } from './company.component';
+import { CompanyDetailComponent } from './company-detail/company-detail.component';
+import { CompanyEditComponent } from './company-edit/company-edit.component';
+import { CanDeactivateGuard } from '../core/shared/can-deactivate.guard';
 
 const routes: Routes = [
   { path: '', canActivate: [AuthGuardService], children: [
     { path: 'company-centre',
     children: [
-      { path: '', component: CompanyComponent }
+      { path: '', component: CompanyComponent },
       // { path: 'company', children: [
       //   { path: '', component: ContactgroupsCompanyEditComponent }
       // ] },
-    //   { path: 'detail/:personId',
-    //   // children: [
-    //   //   { path: '', component: ContactgroupsDetailComponent },
-    //   //   { path: 'people/:contactGroupId', component: ContactgroupsPeopleComponent, canDeactivate: [CanDeactivateGuard] },
-    //   //   {path: 'edit', component: ContactgroupsDetailEditComponent, canDeactivate: [CanDeactivateGuard]},
-    //   //   // {path: 'edit:/id', component: ContactgroupsDetailEditComponent}
-    //   // ] },
-    // }
+      { path: 'detail/:id',
+      children: [
+        { path: '', component: CompanyDetailComponent },
+        {path: 'edit', component: CompanyEditComponent, canDeactivate: [CanDeactivateGuard]},
+      ] }
     ]
-  },
-  ] }
-];
+  }
+
+]}]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
