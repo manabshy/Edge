b4cac5d27e527b6d1cc44538be36e47c5bf028a5
dngAlexA <aagyapong@dng.co.uk>
2019-06-21 15:38:10 +0100

    Company edit (Initial)

--- src/app/company/company-edit/company-edit.component.ts
+++ src/app/company/company-edit/company-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ContactGroupsService } from 'src/app/contactgroups/shared/contact-groups.service';
+import { SharedService } from 'src/app/core/services/shared.service';
+import { AppConstants } from 'src/app/core/shared/app-constants';
 
 @Component({
   selector: 'app-company-edit',
@@ -6,10 +10,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./company-edit.component.scss']
 })
 export class CompanyEditComponent implements OnInit {
+  isSubmitting: boolean;
 
-  constructor() { }
+  companyForm: FormGroup;
+
+  constructor( private contactGroupService: ContactGroupsService,
+              private fb: FormBuilder,
+              private sharedService: SharedService
+            ) { }
 
   ngOnInit() {
+    this.setupCompanyForm();
   }
 
+  private setupCompanyForm() {
+    this.companyForm = this.fb.group({
+      companyName: [''],
+      companyType: [''],
+      signers: [''],
+      fullAddress: [''],
+      address: this.fb.group({
+        addressLines: ['', { validators: Validators.maxLength(500), updateOn: 'blur' }],
+        countryId: 0,
+        postCode: ['', { validators: [Validators.minLength(5), Validators.maxLength(8)], updateOn: 'blur' }],
+      }),
+      contactDetails: this.fb.group({
+        telephone: ['', { validators: [Validators.required, Validators.minLength(7), Validators.maxLength(16), Validators.pattern(/^\+?[ \d]+$/g)], updateOn: 'blur' }],
+        fax: [''],
+        email: ['', { validators: [Validators.required, Validators.pattern(AppConstants.emailPattern)], updateOn: 'blur' }],
+        website: [''],
+      })
+    });
+  }
+
+  canDeactivate(): boolean {
+    if (this.companyForm.dirty  && !this.isSubmitting) {
+      return false;
+    }
+    return true;
+  }
+  cancel() {
+    this.sharedService.back();
+  }
 }
