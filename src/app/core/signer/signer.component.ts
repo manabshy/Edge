a1ee2314328ef6d58663ecba529e4f7f66a4760e
dngElia <efulchignoni@dng.co.uk>
2019-06-26 16:11:07 +0100

    signer search WIP

--- src/app/core/signer/signer.component.ts
+++ src/app/core/signer/signer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input, ViewChild, ElementRef } from '@angular/core';
 import { ContactGroupsService } from 'src/app/contactgroups/shared/contact-groups.service';
 import { ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
@@ -14,12 +14,14 @@ import { debounceTime } from 'rxjs/operators';
 export class SignerComponent implements OnInit {
   @Output() selectedSigner = new EventEmitter<Signer>();
   @Input() existingSigner: Signer;
+  @ViewChild('searchTermInput') searchTermInput: ElementRef; 
   signerFinderForm: FormGroup;
   selectedSignerDetails: Signer;
   signers: Signer[];
   isLoading: boolean;
   isMessageVisible: boolean;
   isHintVisible: boolean;
+  searchTermBK: string = '';
   get signerNames(): FormControl {
     return <FormControl> this.signerFinderForm.get('searchTerm');
   }
@@ -30,7 +32,7 @@ export class SignerComponent implements OnInit {
       searchTerm: [''],
     });
     this.displayExistingSigners();
-    this.signerFinderForm.valueChanges.pipe(debounceTime(400)).subscribe(data => {
+    this.signerFinderForm.valueChanges.subscribe(data => {
       console.log('characters entered', data.searchTerm);
       this.signersAutocomplete(data.searchTerm);
     });
@@ -74,16 +76,28 @@ export class SignerComponent implements OnInit {
       const names = this.selectedSignerDetails.contactNames;
       const namesWithCompany = this.selectedSignerDetails.contactNames + ' (' + this.selectedSignerDetails.companyName + ')';
       this.selectedSignerDetails.companyName ? displayName = namesWithCompany : displayName = names;
+      this.searchTermBK = this.signerFinderForm.get('searchTerm').value;
       this.signerFinderForm.get('searchTerm').setValue(displayName);
       this.selectedSigner.emit(this.selectedSignerDetails);
+      this.searchTermInput.nativeElement.scrollIntoView();
     }
     console.log('selected signer ', this.selectedSignerDetails.contactNames);
     console.log('selected  signer company name', this.selectedSignerDetails.companyName);
     console.log('selected signer id', id);
   }
+
+  initSearch() {
+    this.selectedSignerDetails = null;
+    this.signerFinderForm.get('searchTerm').setValue(this.searchTermBK);
+  }
+
+  resetSearch() {
+    this.signerFinderForm.reset();
+  }
+
   onKeyup() {
     AppUtils.searchTerm = this.signerFinderForm.value.searchTerm;
-
+    
     if (this.signerFinderForm.value.searchTerm && this.signerFinderForm.value.searchTerm.length > 2) {
       this.isHintVisible = false;
     } else {
