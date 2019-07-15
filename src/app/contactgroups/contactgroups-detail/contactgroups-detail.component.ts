954ceba9c53b631cee71b3dcb2154b41c8c66934
dngElia <efulchignoni@dng.co.uk>
2019-07-15 15:44:20 +0100

    fix

--- src/app/contactgroups/contactgroups-detail/contactgroups-detail.component.ts
+++ src/app/contactgroups/contactgroups-detail/contactgroups-detail.component.ts
@@ -42,8 +42,10 @@ export class ContactgroupsDetailComponent implements OnInit {
   init() {
     // this.getContactGroupById(this.contactGroupId);
     // this.listInfo = this.sharedService.dropdownListInfo;
-    this.sharedService.getDropdownListInfo().subscribe(data=> this.listInfo = data);
-    this.warnings = this.listInfo.result.personWarningStatuses;
+    this.sharedService.getDropdownListInfo().subscribe(data=> {
+      this.listInfo = data
+      this.warnings = this.listInfo.result.personWarningStatuses;
+    });
     this.getSearchedPersonDetails(this.personId);
     this.getSearchedPersonContactGroups(this.personId);
     this.getSearchedPersonSummaryInfo(this.personId);
