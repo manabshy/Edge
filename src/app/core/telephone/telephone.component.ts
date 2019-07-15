abaee835a387a69ad7c9cc4809221e218c3dbe64
dngElia <efulchignoni@dng.co.uk>
2019-07-15 14:08:56 +0100

    stop on people modal

--- src/app/core/telephone/telephone.component.ts
+++ src/app/core/telephone/telephone.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Subject } from 'rxjs';
 import { BsModalService } from 'ngx-bootstrap/modal/';
 import { TelephoneModalComponent } from '../telephone-modal/telephone-modal.component';
+import { ConfirmModalComponent } from '../confirm-modal/confirm-modal.component';
 
 @Component({
   selector: 'app-telephone',
@@ -12,6 +13,7 @@ export class TelephoneComponent implements OnInit {
   @Input() number: string;
   @Input() searchTerm: string;
   @Input() sms: boolean;
+  @Input() warning: any;
 
   constructor(private modalService: BsModalService) { }
 
@@ -20,19 +22,54 @@ export class TelephoneComponent implements OnInit {
 
   callOrText() {
     if(this.sms) {
-      const subject = new Subject<string>();
-      const modal = this.modalService.show(TelephoneModalComponent);
-      modal.content.subject = subject;
-      subject.asObservable().subscribe(res => {
-        if(res === 'call') {
-          this.call();
-        } else {
-          alert('SMS');
-        }
-      });
+      if(this.warning) {
+        this.showWarning().subscribe(res =>{
+          if(res) {
+            this.callOrTextChoice();
+          }
+        });
+      } else {
+        this.callOrTextChoice();
+      }
     } else {
-      this.call();
+      if(this.warning) {
+        this.showWarning().subscribe(res =>{
+          if(res) {
+            this.call();
+          }
+        });
+      } else {
+        this.call();
+      }
     }
+  }
+
+  showWarning() {
+    const subject = new Subject<boolean>();
+    const initialState = {
+      danger: this.warning,
+      title: this.warning.value,
+      actions: ['Leave', 'Proceed']
+    };
+    const modal = this.modalService.show(ConfirmModalComponent, {ignoreBackdropClick: true, initialState});
+    modal.content.subject = subject;
+    return subject.asObservable();
+  }
+
+  callOrTextChoice() {
+    const subject = new Subject<string>();
+    const initialState = {
+      warning: this.warning
+    }
+    const modal = this.modalService.show(TelephoneModalComponent, {initialState: initialState});
+    modal.content.subject = subject;
+    subject.asObservable().subscribe(res => {
+      if(res === 'call') {
+        this.call();
+      } else {
+        alert('SMS');
+      }
+    });
   }
 
   call() {
