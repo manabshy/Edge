24d13a4b361b4dae59517e336ba3c7f7740d9736
dngElia <efulchignoni@dng.co.uk>
2019-07-16 17:15:56 +0100

    note WIP

--- src/app/core/note-modal/note-modal.component.ts
+++ src/app/core/note-modal/note-modal.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 import { Subject } from 'rxjs';
 import { Person } from '../models/person';
+import { TargetLocator } from 'selenium-webdriver';
 
 @Component({
   selector: 'app-note-modal',
@@ -13,11 +15,30 @@ export class NoteModalComponent implements OnInit {
   subject: Subject<boolean>;
   step2: boolean = false;
   selectedPerson: Person;
-
-  constructor(public bsModalRef: BsModalRef) { }
+  noteForm: FormGroup;
+  shortcuts = {
+    'LVM': 'left voice mail',
+    'LVMTCB': 'left voice mail to call back',
+    'SW': 'spoke with',
+    'FDT': 'foreign dial tone',
+    'SMS\'D': 'sent an SMS',
+    'RO': 'rang off/out',
+    'STVM': 'straight to voice mail',
+    'EA/OA': 'external/enemy agent or other agent',
+    'UO': 'under offer',
+    'NNW': 'number not working'
+  };
+  shortcutsAdded: any[] = [];
+  public keepOriginalOrder = (a) => a.key;
+  constructor(public bsModalRef: BsModalRef, private fb: FormBuilder) { }
 
   ngOnInit() {
     this.selectedPerson = this.data.person || null;
+
+    this.noteForm = this.fb.group({
+      isImportant: false,
+      text: ['']
+    });
   }
 
   select(person: Person) {
@@ -25,6 +46,21 @@ export class NoteModalComponent implements OnInit {
       this.selectedPerson = person
     }
     this.step2 = true;
+  }
+
+  consumeShortcut(shortcut: string) {
+    const index = this.shortcutsAdded.indexOf(shortcut);
+    const text = '';
+    if(index >= 0) {
+      this.shortcutsAdded.splice(index,1);
+    } else {
+      this.shortcutsAdded.push(shortcut);
+    }
+    this.shortcutsAdded.forEach(x=>{
+      text += x + ', ';
+    })
+    text = text.replace(/,\s*$/, "");
+    this.noteForm.get('text').setValue(text);
   }
 
   action(value: boolean) {
