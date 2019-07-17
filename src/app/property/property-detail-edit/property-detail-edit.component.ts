72e4fe11ee64cb353f995c406aae55834d6f1afa
dngElia <efulchignoni@dng.co.uk>
2019-07-17 12:05:06 +0100

    show region area and subarea

--- src/app/property/property-detail-edit/property-detail-edit.component.ts
+++ src/app/property/property-detail-edit/property-detail-edit.component.ts
@@ -23,6 +23,11 @@ export class PropertyDetailEditComponent implements OnInit {
   propertyTypes: any;
   allPropertyStyles: any;
   propertyStyles: InfoDetail[];
+  regions: InfoDetail[];
+  allAreas: InfoDetail[];
+  areas: InfoDetail[];
+  allSubAreas: InfoDetail[];
+  subAreas: InfoDetail[];
 
   constructor(private route: ActivatedRoute,
               private propertyService: PropertyService,
@@ -36,6 +41,9 @@ export class PropertyDetailEditComponent implements OnInit {
       this.listInfo = data;
       this.propertyTypes = this.listInfo.result.propertyTypes;
       this.allPropertyStyles = this.listInfo.result.propertyStyles;
+      this.regions = this.listInfo.result.regions;
+      this.allAreas = this.listInfo.result.areas;
+      this.allSubAreas = this.listInfo.result.subAreas;
     });
     this.route.params.subscribe(params => {
       this.propertyId = +params['id'] || 0;
@@ -50,9 +58,14 @@ export class PropertyDetailEditComponent implements OnInit {
     this.propertyService.getProperty(propertyId).subscribe(data => {
       this.propertyDetails = data;
       this.onSelectType(data.propertyTypeId);
+      this.onSelectRegion(data.regionId);
+      this.onSelectArea(data.areaId);
       this.propertyForm.patchValue({
         propertyTypeId: data.propertyTypeId,
-        propertyStyleId: data.propertyStyleId
+        propertyStyleId: data.propertyStyleId,
+        regionId: data.regionId,
+        areaId: data.areaId,
+        subAreaId: data.subAreaId,
       });
       console.log(this.propertyDetails);
     });
@@ -63,11 +76,20 @@ export class PropertyDetailEditComponent implements OnInit {
     this.propertyStyles = this.allPropertyStyles.filter((x:InfoDetail)=>x.parentId==propertyTypeId);
     console.log('selected styles', this.propertyStyles );
   }
+  onSelectRegion(regionId) {
+    this.areas = this.allAreas.filter((x:InfoDetail)=>x.parentId==regionId);
+  }
+  onSelectArea(areaId) {
+    this.subAreas = this.allSubAreas.filter((x:InfoDetail)=>x.parentId==areaId);
+  }
   setupEditForm() {
     this.propertyForm = this.fb.group({
       propertyTypeId: [''],
       propertyStyleId: [''],
       fullAddress: [''],
+      regionId: 0,
+      areaId: 0,
+      subAreaId: 0,
       address: this.fb.group({
         addressLines: ['', {validators: Validators.maxLength(500)}],
         countryId: 0,
