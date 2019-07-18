468a191d071ecd9721cfab86c6d2d274091600db
dngAlexA <aagyapong@dng.co.uk>
2019-07-18 17:15:29 +0100

    Add a base component for unscribing from subscriptions

--- src/app/contactgroups/contactgroups-detail-notes/contactgroups-detail-notes.component.ts
+++ src/app/contactgroups/contactgroups-detail-notes/contactgroups-detail-notes.component.ts
@@ -4,19 +4,23 @@ import { ActivatedRoute } from '@angular/router';
 import { SharedService } from 'src/app/core/services/shared.service';
 import { ContactGroupsService } from '../shared/contact-groups.service';
 import { PersonNote } from '../shared/contact-group';
+import { BaseComponent } from 'src/app/core/models/base-component';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-contactgroups-detail-notes',
   templateUrl: './contactgroups-detail-notes.component.html',
   styleUrls: ['./contactgroups-detail-notes.component.scss']
 })
-export class ContactgroupsDetailNotesComponent implements OnInit {
+export class ContactgroupsDetailNotesComponent extends BaseComponent implements OnInit  {
   person: Person;
   personId: number;
   personNotes: PersonNote[];
   contactGroupId: number;
   contactGroupNotes: PersonNote[];
-  constructor(private contactGroupService: ContactGroupsService, private route: ActivatedRoute, private sharedService: SharedService) { }
+  constructor(private contactGroupService: ContactGroupsService,
+              private route: ActivatedRoute,
+              private sharedService: SharedService) {super(); }
   // ngOnInit() {
   //   // let personParams = this.route.snapshot.queryParamMap.get('person');
   //   // if(personParams){
@@ -38,22 +42,22 @@ export class ContactgroupsDetailNotesComponent implements OnInit {
       this.personId = +params['personId'] || 0;
     });
 
-    if(this.contactGroupId) {
-      console.log('contact group id',this.contactGroupId);
+    if (this.contactGroupId) {
+      console.log('contact group id', this.contactGroupId);
       this.getContactGroupNotes(this.contactGroupId);
-    } else if(this.personId){
+    } else if (this.personId) {
       console.log('person id', this.personId);
       this.getPersonNotes(this.personId);
     }
   }
 
-  getPersonNotes(personId: number){
-    this.contactGroupService.getPersonNotes(personId).subscribe(data => {
+  getPersonNotes(personId: number) {
+    this.contactGroupService.getPersonNotes(personId).pipe(takeUntil(this.ngUnsubscribe)).subscribe(data => {
       this.personNotes = data;
     });
   }
-  getContactGroupNotes(personId: number){
-    this.contactGroupService.getPersonNotes(personId).subscribe(data => {
+  getContactGroupNotes(personId: number) {
+    this.contactGroupService.getPersonNotes(personId).pipe(takeUntil(this.ngUnsubscribe)).subscribe(data => {
       this.contactGroupNotes = data;
     });
   }
@@ -62,8 +66,8 @@ export class ContactgroupsDetailNotesComponent implements OnInit {
     event.stopPropagation();
     const data = {
       personId: this.personId,
-    }
-    console.log('for notes',data);
+    };
+    console.log('for notes', data);
     this.sharedService.addNote(data);
   }
 }
