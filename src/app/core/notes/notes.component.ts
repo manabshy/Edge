eac7e5ab6c60d0ba15b24ecb1764e9a19fd73d1c
dngAlexA <aagyapong@dng.co.uk>
2019-07-18 11:57:25 +0100

    small refactoring

--- src/app/core/notes/notes.component.ts
+++ src/app/core/notes/notes.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit, Input, OnChanges } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { ContactGroupsService } from 'src/app/contactgroups/shared/contact-groups.service';
 import { PersonNote, ContactGroupsNote } from 'src/app/contactgroups/shared/contact-group';
 import { SharedService } from 'src/app/core/services/shared.service';
 
@@ -9,20 +7,17 @@ import { SharedService } from 'src/app/core/services/shared.service';
   templateUrl: './notes.component.html',
   styleUrls: ['./notes.component.scss']
 })
-export class NotesComponent implements OnInit, OnChanges {
+export class NotesComponent implements OnInit {
   @Input() dataNote: any;
   @Input() personNotes: PersonNote[];
   @Input() contactGroupNotes: ContactGroupsNote[];
   notes: ContactGroupsNote[];
 
-  constructor(private contactGroupService: ContactGroupsService, private route: ActivatedRoute, private sharedService: SharedService) { }
+  constructor(private sharedService: SharedService) { }
 
   ngOnInit() {
   }
 
-  ngOnChanges(){
-    this.notes = this.contactGroupNotes;
-  }
 
  addNote(dataNote) {
    if (this.dataNote) {
