94acb3f496387aa1e7b98d6d09f93ca21d275d72
dngElia <efulchignoni@dng.co.uk>
2019-08-01 15:21:33 +0100

    new SMS layout

--- src/app/core/sms-modal/sms-modal.component.ts
+++ src/app/core/sms-modal/sms-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Renderer2 } from '@angular/core';
 import { Subject } from 'rxjs';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 import { sample } from 'rxjs/operators';
@@ -14,19 +14,28 @@ export class SmsModalComponent implements OnInit {
   @Input() number;
   @Input() actions;
   smsMaxLength = 700;
-  smsForm: FormGroup
+  smsForm: FormGroup;
+  isNumberEditable: boolean = false;
   subject: Subject<string>;
 
   get smsLength() {
     return +this.smsForm.get('text').value.length;
   }
 
-  constructor(public bsModalRef: BsModalRef, private fb: FormBuilder) { }
+  constructor(public bsModalRef: BsModalRef, private fb: FormBuilder, private renderer: Renderer2) { }
 
   ngOnInit() {
     this.smsForm = this.fb.group({
       text: ['', [Validators.required, Validators.maxLength(700)]]
     });
+  }
+
+  toggleNumberEdit() {
+    event.preventDefault();
+    this.isNumberEditable = !this.isNumberEditable;
+    setTimeout(()=>{
+      this.renderer.selectRootElement('#userNumber').focus();
+    })
   }
 
   action(value: string) {
