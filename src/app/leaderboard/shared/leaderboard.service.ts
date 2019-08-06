08d6a66358a1c0628e62d034c8c51bc7db833e8e
dngAlexA <aagyapong@dng.co.uk>
2019-08-06 16:08:18 +0100

    Add viewings completed and managed tenancies to service

--- src/app/leaderboard/shared/leaderboard.service.ts
+++ src/app/leaderboard/shared/leaderboard.service.ts
@@ -13,18 +13,26 @@ export class LeaderboardService {
 
   constructor(private http: HttpClient) {}
 
-  getStaffMemberPipeline(role: string): Observable<Leaderboard[]> {
+  getStaffMemberPipeline(role: string): Observable<Leaderboard[] | any> {
     const url = `${AppConstants.leaderboardBaseUrl}/pipeline?role=${role}`;
     return this.http.get<LeaderboardResult>(url)
     .pipe(map(response => response.result));
   }
 
-  getStaffMemberExchanges(role: string, period: any): Observable<Leaderboard[]> {
+  getStaffMemberExchanges(role: string, period: any): Observable<Leaderboard[] | any> {
     return this.getLeaderboard(role, period, 'exchanges');
   }
 
-  getStaffMemberInstructions(role: string, period: any, params?: any): Observable<Leaderboard[]> {
+  getStaffMemberInstructions(role: string, period: any, params?: any): Observable<Leaderboard[] | any> {
     return this.getLeaderboard(role, period, 'instructions', params);
+  }
+  
+  getStaffMemberViewingsCompleted(role: string, period: any, params?: any): Observable<Leaderboard[] | any> {
+    return this.getLeaderboard(role, period, 'viewings', params);
+  }
+
+  getStaffMemberManagedTenancies(role: string, period: any, params?: any): Observable<Leaderboard[] | any> {
+    return this.getLeaderboard(role, period, 'propertyLettings', params);
   }
 
   private getLeaderboard(role: string, period: any, endPoint: string, params?: any) {
