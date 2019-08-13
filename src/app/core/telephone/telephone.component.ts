9521489553c3e8cb2819b41c88578c7db6a3eaeb
dngElia <efulchignoni@dng.co.uk>
2019-08-13 16:57:51 +0100

    fix phone back

--- src/app/core/telephone/telephone.component.ts
+++ src/app/core/telephone/telephone.component.ts
@@ -102,7 +102,7 @@ export class TelephoneComponent implements OnInit {
 
   call() {
     if (window.innerWidth < 576) {
-      window.open('wtai://wp/mc;' + this.number);
+      window.open('tel:' + this.number);
     } else {
       const tapiInfo: TapiRequestInfo = {
         officeId: 10,
