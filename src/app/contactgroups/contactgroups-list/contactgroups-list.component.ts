9e7f7da46a305e9ad3d3707b3d1e06aa71512030
dngAlexA <aagyapong@dng.co.uk>
2019-09-20 15:49:06 +0100

    Add Send SMS to contact centre

--- src/app/contactgroups/contactgroups-list/contactgroups-list.component.ts
+++ src/app/contactgroups/contactgroups-list/contactgroups-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { ContactGroupAutoCompleteResult } from '../shared/contact-group';
 import { AppUtils } from 'src/app/core/shared/utils';
+import { SharedService } from 'src/app/core/services/shared.service';
 
 @Component({
   selector: 'app-contactgroups-list',
@@ -11,8 +12,9 @@ export class ContactgroupsListComponent implements OnInit, OnChanges {
   @Input() originalContactGroups: ContactGroupAutoCompleteResult[];
   @Input() searchTerm: string;
   contactGroups: ContactGroupAutoCompleteResult[];
+  contactPhoneNumbers = [];
 
-  constructor() { }
+  constructor(private sharedService: SharedService) { }
 
   ngOnInit() {
   }
@@ -20,7 +22,26 @@ export class ContactgroupsListComponent implements OnInit, OnChanges {
   ngOnChanges() {
    if (this.originalContactGroups) {
       this.contactGroups = this.originalContactGroups.slice(0, 10);
+      this.setSmsFlag();
    }
+    // remove duplicate phone numbers
+    // this.contactPhoneNumbers = [...new Map(this.output.map(item => [item.personId, item])).values()];
+  }
+
+  setSmsFlag() {
+    let sendSMS: boolean;
+    let result;
+    if (this.originalContactGroups) {
+      this.originalContactGroups.forEach(c => {
+        this.sharedService.isUKMobile(c.phoneNumbers[0]) ? sendSMS = true : sendSMS = false;
+        result = {
+          personId: c.personId,
+          sendSMS: sendSMS,
+          number: c.phoneNumbers[0]
+        };
+        this.contactPhoneNumbers.push(result);
+      });
+    }
   }
 
   onScrollDown() {
