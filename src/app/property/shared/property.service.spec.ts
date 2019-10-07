6bc1eca73ebe8ea294b3b8d07a326f1b70786dc4
dngAlexA <aagyapong@dng.co.uk>
2019-10-07 16:29:33 +0100

    Refactor to group test by method

--- src/app/property/shared/property.service.spec.ts
+++ src/app/property/shared/property.service.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController, TestRequest  } from '@angular/common/http/testing';
 
 import { PropertyService } from './property.service';
-import { HttpClientTestingModule, HttpTestingController, TestRequest  } from '@angular/common/http/testing';
 import { Property } from './property';
 import { MockProperty } from './test-helper';
 
@@ -9,7 +9,7 @@ fdescribe('PropertyService', () => {
   let httpTestingController: HttpTestingController;
   let service: PropertyService;
   const baseUrl = `https://dandg-api-wedge-dev.azurewebsites.net/v10/properties`;
-  let property = <Property> <unknown>MockProperty;
+  const property = <Property> <unknown>MockProperty;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -29,25 +29,45 @@ fdescribe('PropertyService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should make one api call and return a matching property', () => {
-    // let response: any;
-    // service.getProperty(property.propertyId).subscribe(res => {response = res; console.log('result', res)});
-    service.getProperty(property.propertyId).subscribe();
+  describe('#getProperty', () => {
+    it('should make one api call and return a matching property', () => {
+      // let response: Property;
+      // service.getProperty(property.propertyId).subscribe((res: Property) => {response = res; console.log('result', res)});
+      const url = `${baseUrl}/${property.propertyId}?includeInfo=false&includePhoto=false`;
+      service.getProperty(property.propertyId).subscribe();
 
-    const req: TestRequest = httpTestingController.expectOne(`${baseUrl}/${property.propertyId}?includeInfo=false&includePhoto=false`);
+      const req: TestRequest = httpTestingController.expectOne(url);
 
-    req.flush(property);
-    expect(req.request.method).toEqual('GET');
-    // expect(response).toEqual(property);
+      req.flush(property);
+      expect(req.request.method).toEqual('GET');
+    });
   });
 
-  it('should add and return a new property', () => {
-    service.addProperty(property).subscribe();
+  describe('#AddProperty', () => {
 
-    const req: TestRequest = httpTestingController.expectOne(`${baseUrl}`);
+    it('should add and return a new property', () => {
+      service.addProperty(property).subscribe();
 
-    req.flush(property);
-    expect(req.request.method).toEqual('POST');
+      const req: TestRequest = httpTestingController.expectOne(`${baseUrl}`);
+
+      req.flush(property);
+      expect(req.request.method).toEqual('POST');
+    });
+
   });
 
+  // describe('#getPotentialDuplicateProperties', () => {
+  //   it('return a full match for full address', () => {
+  //     // let response: any;
+  //     // service.getProperty(property.propertyId).subscribe(res => {response = res; console.log('result', res)});
+  //     service.getPotentialDuplicateProperties(property.address).subscribe();
+
+  //     const req: TestRequest = httpTestingController.expectOne(`${baseUrl}/duplicates`);
+
+  //     req.flush(property);
+  //     expect(req.request.method).toEqual('GET');
+  //     // expect(response).toEqual(property);
+  //   });
+
+  // });
 });
