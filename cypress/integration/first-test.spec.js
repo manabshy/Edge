cbc54d4fcefe62ac4fe39a4d16e4f09e98eabc09
dngAlexA <aagyapong@dng.co.uk>
2019-10-09 17:01:40 +0100

    e2e test setup (WIP)

--- cypress/integration/first-test.spec.js
+++ cypress/integration/first-test.spec.js
@@ -1,9 +1,16 @@
+/// <reference types="Cypress" />
 describe('My first test', () => {
+  beforeEach(()=>{
+    cy.visit('/login')
+    cy.loggedInAs('user')
+  })
 
-  it('should visit login page', () => {
+  it('greets with message', () => {
+    cy.contains('p', ' Sign in with your work email');
+  })
+
+  it('go to property centre', () => {
     cy.visit('/property-centre')
-
-    cy.get('button').click();
   })
 
 })
