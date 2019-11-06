5d95409cecd6f9653052fa3896aac8a5c7d58a78
Mansoor Malik <mmalik@dng.co.uk>
2019-11-06 11:18:55 +0000

    https://github.com/douglasandgordon/Wedge/issues/136

--- src/app/leads/lead.component.ts
+++ src/app/leads/lead.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { LeadsService } from './shared/leads.service';
+import { StaffMemberService } from '../core/services/staff-member.service';
+import { StaffMember } from '../core/models/staff-member';
+import { getLocaleDayNames } from '@angular/common';
 
 @Component({
   selector: 'app-lead',
@@ -7,9 +11,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeadComponent implements OnInit {
 
+  
+
   constructor() { }
 
   ngOnInit() {
+    
+  }
+
+  
   }
 
 }
--- src/app/leads/shared/leads.service.ts
+++ src/app/leads/shared/leads.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Lead } from './lead';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { tap, map } from 'rxjs/operators';
 import { AppConstants } from 'src/app/core/shared/app-constants';
 
 @Injectable({
@@ -11,30 +11,37 @@ import { AppConstants } from 'src/app/core/shared/app-constants';
 export class LeadsService {
 
   constructor(private http: HttpClient) { }
-  
-  
+
+
   addLead(lead: Lead): Observable<any> {
       const url = `${AppConstants.baseLeadsUrl}`;
-    
-      return this.http.post<Lead>(url, lead).pipe(tap(data => console.log('result', data)));
+
+      return this.http.post<Lead>(url, lead).pipe(
+        tap(data => console.log('result', JSON.stringify(data))));
   }
 
   updateLead(lead: Lead): Observable<any> {
-    const url = `${AppConstants.baseLeadsUrl}/${leadId}`;
+    const url = `${AppConstants.baseLeadsUrl}/${lead.leadId}`;
 
-    return this.http.put<Lead>(url, lead).pipe(tap(data => console.log('result', data)));
+    return this.http.put<Lead>(url, lead).pipe(
+      map(response => response),
+      tap(data => console.log('Updated Lead details here...', JSON.stringify(data))));
   }
 
   getLead(leadId: number): Observable<any>  {
     const url = `${AppConstants.baseLeadsUrl}/${leadId}`;
-    
-    return this.http.get<Lead>(url).pipe(tap(data => console.log('result', data)));
+
+    return this.http.get<any>(url).pipe(
+      map(response => response.result),
+      tap(data => console.log('result', JSON.stringify(data))));
   }
 
   getLeads(staffMemberId: number): Observable<any>  {
     const url = `${AppConstants.baseLeadsUrl}/owner/${staffMemberId}`;
-    
-    return this.http.get<Lead>(url).pipe(tap(data => console.log('result', data)));
+
+    return this.http.get<any>(url).pipe(
+      map(response => response.result),
+      tap(data => console.log('result', JSON.stringify(data))));
   }
 }
 
