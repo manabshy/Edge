7b93eb81a375caaea6d43301fb19a79b21bb2855
dngAlexA <aagyapong@dng.co.uk>
2019-11-07 15:10:06 +0000

    Extract house number from building name

--- src/app/core/address/address.component.ts
+++ src/app/core/address/address.component.ts
@@ -53,16 +53,16 @@ export class AddressComponent implements OnInit, OnChanges {
   }
 
   constructor(private sharedService: SharedService,
-              private addressService: AddressService,
-              private infoService: InfoService,
-              private storage: StorageMap,
-              private fb: FormBuilder,
-              private renderer: Renderer2,
-             ) { }
+    private addressService: AddressService,
+    private infoService: InfoService,
+    private storage: StorageMap,
+    private fb: FormBuilder,
+    private renderer: Renderer2,
+  ) { }
 
   ngOnInit() {
     this.init();
-   
+
   }
 
   ngOnChanges() {
@@ -80,7 +80,7 @@ export class AddressComponent implements OnInit, OnChanges {
 
     this.addressForm = this.fb.group({
       fullAddress: ['', Validators.required],
-      addressLines: ['', {validators: Validators.maxLength(500)}],
+      addressLines: ['', { validators: Validators.maxLength(500) }],
       countryId: this.defaultCountryCode,
       addressLine2: [''],
       flatNumber: [''],
@@ -88,7 +88,7 @@ export class AddressComponent implements OnInit, OnChanges {
       houseBuildingName: [''],
       streetName: [''],
       town: [''],
-      postCode: ['', {validators: [Validators.minLength(5), Validators.maxLength(8), Validators.pattern(AppConstants.postCodePattern)]}],
+      postCode: ['', { validators: [Validators.minLength(5), Validators.maxLength(8), Validators.pattern(AppConstants.postCodePattern)] }],
     });
     if (this.companyDetails || this.personDetails || this.propertyDetails) {
       this.populateAddressForm(this.personDetails, this.companyDetails, this.propertyDetails);
@@ -96,14 +96,14 @@ export class AddressComponent implements OnInit, OnChanges {
 
     Object.keys(this.addressForm.controls).forEach(key => {
       this.addressForm.get(key).valueChanges.pipe(debounceTime(500))
-      .subscribe((data) => {
-        if (key === "postCode") {
-          this.postCode.setValue(this.sharedService.formatPostCode(data), { emitEvent: false });
-        }
-        if (key !== "fullAddress") {
-          this.emitAddress();
-        }
-      });
+        .subscribe((data) => {
+          if (key === 'postCode') {
+            this.postCode.setValue(this.sharedService.formatPostCode(data), { emitEvent: false });
+          }
+          if (key !== 'fullAddress') {
+            this.emitAddress();
+          }
+        });
     });
   }
 
@@ -123,11 +123,11 @@ export class AddressComponent implements OnInit, OnChanges {
     this.enterAddressManually = true;
     this.foundAddress = null;
     setTimeout(() => {
-     if (!(this.propertyDetails || this.isNewProperty)) {
+      if (!(this.propertyDetails || this.isNewProperty)) {
         this.renderer.selectRootElement('#addressLines').focus();
-     } else {
-      this.renderer.selectRootElement('#flatNumber').focus();
-     }
+      } else {
+        this.renderer.selectRootElement('#flatNumber').focus();
+      }
     });
   }
 
@@ -175,16 +175,20 @@ export class AddressComponent implements OnInit, OnChanges {
             countryId: this.defaultCountryCode
           });
         } else {
-          let buildingInfo = retrievedAddress.BuildingName.split(' ');
-          let numberRegex = /\d/;
-          if(numberRegex.test(buildingInfo[0])){
+          const buildingInfo = retrievedAddress.BuildingName.split(' ');
+          const lastItem = buildingInfo.length - 1;
+          let buildingNumber: string;
+          if (this.hasNumber(buildingInfo[lastItem])) {
+            buildingNumber = buildingInfo[lastItem];
+            retrievedAddress.BuildingName = buildingInfo.slice(1, buildingInfo.length - 1).join(' ');
+          }
+          if (this.hasNumber(buildingInfo[0])) {
             retrievedAddress.BuildingNumber = buildingInfo[0];
             retrievedAddress.BuildingName = buildingInfo.slice(1, buildingInfo.length).join(' ');
           }
-
           this.addressForm.patchValue({
             flatNumber: retrievedAddress.SubBuilding.replace(/flat/gi, '').trim(),
-            houseNumber: retrievedAddress.BuildingNumber,
+            houseNumber: retrievedAddress.BuildingNumber || buildingNumber,
             houseBuildingName: retrievedAddress.BuildingName,
             streetName: retrievedAddress.Street,
             town: retrievedAddress.City,
@@ -192,9 +196,9 @@ export class AddressComponent implements OnInit, OnChanges {
           });
         }
 
-       if (this.isNewProperty || this.propertyDetails) {
+        if (this.isNewProperty || this.propertyDetails) {
           this.propertyDetails = this.addressForm.value;
-       }
+        }
         setTimeout(() => {
           let element = 'addressLines';
           if (this.propertyDetails) {
@@ -204,19 +208,22 @@ export class AddressComponent implements OnInit, OnChanges {
         });
       });
     }
-    console.log('form', this.addressForm)
+    console.log('form', this.addressForm);
   }
 
+  private hasNumber(number: string) {
+    return /\d/.test(number);
+  }
   private emitAddress() {
     const addressData = this.addressForm.value;
     let address;
     if (addressData) {
-        if (!(this.isNewProperty || this.propertyDetails)) {
-          address = {
-            addressLines: addressData.addressLines,
-            postCode: addressData.postCode,
-            countryId: addressData.countryId
-          };
+      if (!(this.isNewProperty || this.propertyDetails)) {
+        address = {
+          addressLines: addressData.addressLines,
+          postCode: addressData.postCode,
+          countryId: addressData.countryId
+        };
         if (!address.addressLines) {
           address = {
             addressLines: '',
@@ -254,10 +261,10 @@ export class AddressComponent implements OnInit, OnChanges {
             person.address.postCode = person.address.postCode.trim();
           }
           this.addressForm.patchValue({
-              addressLines: person.address.addressLines,
-              postCode: person.address.postCode,
-              countryId: person.address.countryId,
-              country: person.address.country,
+            addressLines: person.address.addressLines,
+            postCode: person.address.postCode,
+            countryId: person.address.countryId,
+            country: person.address.country,
           });
         }
         break;
@@ -276,17 +283,17 @@ export class AddressComponent implements OnInit, OnChanges {
         }
         break;
       case !!this.propertyDetails:
-      this.propertyDetails = property;
-      this.addressForm.patchValue({
-        addressLine2: property.address.addressLine2,
-        flatNumber: property.address.flatNumber,
-        houseNumber: property.address.houseNumber,
-        houseBuildingName: property.address.houseBuildingName,
-        streetName: property.address.streetName,
-        town: property.address.town,
-        postCode: property.address.postCode.trim(),
-        countryId: this.defaultCountryCode
-    });
+        this.propertyDetails = property;
+        this.addressForm.patchValue({
+          addressLine2: property.address.addressLine2,
+          flatNumber: property.address.flatNumber,
+          houseNumber: property.address.houseNumber,
+          houseBuildingName: property.address.houseBuildingName,
+          streetName: property.address.streetName,
+          town: property.address.town,
+          postCode: property.address.postCode.trim(),
+          countryId: this.defaultCountryCode
+        });
     }
   }
 }
