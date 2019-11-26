585ff3bcc9847d46d9b47d2ebe3330c8727e9283
Mansoor Malik <mmalik@dng.co.uk>
2019-11-26 23:08:57 +0000

    https://github.com/douglasandgordon/Wedge/issues/187

---
+++ src/app/leads/lead-assignment-modal/lead-assignment-modal/lead-assignment-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lead-assignment-modal',
+  templateUrl: './lead-assignment-modal.component.html',
+  styleUrls: ['./lead-assignment-modal.component.scss']
+})
+export class LeadAssignmentModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
