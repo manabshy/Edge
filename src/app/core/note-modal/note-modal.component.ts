1e14ddb0a80e957a863f61449162ea0200fe251b
dngElia <efulchignoni@dng.co.uk>
2019-11-28 10:40:11 +0000

    add note from list

--- src/app/core/note-modal/note-modal.component.ts
+++ src/app/core/note-modal/note-modal.component.ts
@@ -1,11 +1,8 @@
-import { Component, OnInit, Input, Renderer2 } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit, Input} from '@angular/core';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 import { Subject } from 'rxjs';
 import { Person } from '../models/person';
-import { ContactGroupsService } from 'src/app/contactgroups/shared/contact-groups.service';
-import { ContactGroup, ContactNote } from 'src/app/contactgroups/shared/contact-group';
-import { ToastrService } from 'ngx-toastr';
+import { ContactGroup } from 'src/app/contactgroups/shared/contact-group';
 
 @Component({
   selector: 'app-note-modal',
@@ -19,34 +16,12 @@ export class NoteModalComponent implements OnInit {
   selectedPerson: Person;
   selectedContactGroup: ContactGroup;
   isPersonNote: boolean;
-  personNote: ContactNote;
-  contactGroupNote: ContactNote;
-  noteForm: FormGroup;
-  shortcuts = {
-    'LVM': 'Left voice mail',
-    'LVMTCB': 'Left voice mail to call back',
-    'SW': 'Spoke with',
-    'FDT': 'Foreign dial tone',
-    'SMS\'D': 'Sent an SMS',
-    'RO': 'Rang off/out',
-    'STVM': 'Straight to voice mail',
-    'EA/OA': 'External/enemy agent or other agent',
-    'UO': 'Under offer',
-    'NNW': 'Number not working'
-  };
-  public keepOriginalOrder = (a) => a.key;
 
-  constructor(public bsModalRef: BsModalRef, private fb: FormBuilder, private contactGroupService: ContactGroupsService, private toastr: ToastrService, private renderer: Renderer2) { }
+  constructor(public bsModalRef: BsModalRef) { }
 
   ngOnInit() {
     this.selectedPerson = this.data.person || null;
     this.selectedContactGroup = this.data.group || null;
-
-    this.noteForm = this.fb.group({
-      isImportant: false,
-      isPinned: false,
-      text: ['']
-    });
   }
 
   select(person: Person) {
@@ -65,75 +40,9 @@ export class NoteModalComponent implements OnInit {
     this.step2 = true;
   }
 
-  consumeShortcut(shortcut: string) {
-    const textControl = this.noteForm.get('text');
-    let textValue = textControl.value;
-    if(textValue.includes(shortcut)) {
-      if(textValue.includes(shortcut + ', ')){
-        textValue = textValue.replace(shortcut + ', ','');
-      } else {
-        textValue = textValue.replace(shortcut,'');
-      }
-    } else {
-      textValue = textValue.slice(0, 0) + shortcut + ', ' + textValue.slice(0);
-    }
-    textValue = textValue.replace(/,\s*$/, '');
-    textControl.setValue(textValue.trimEnd() + ' ');
-    this.renderer.selectRootElement('#note').focus();
-  }
-
   action(value: boolean) {
-    console.log(value);
-   if (value) {
-      this.saveNote();
-   }
     this.bsModalRef.hide();
     this.subject.next(value);
     this.subject.complete();
-  }
-
-  saveNote() {
-    console.log(this.data);
-    this.isPersonNote || this.data.isPersonNote || this.data.personId ? this.addPersonNote() : this.addContactNote();
-    // if (this.noteForm.valid) {
-    //   if (this.noteForm.dirty) {
-    //     this.isPersonNote || this.data.isPersonNote || this.data.personId ? this.addPersonNote() : this.addContactNote();
-    //   }
-    // }
-  }
-
-  private addPersonNote() {
-    const note = { ...this.personNote, ...this.noteForm.value };
-    if (note && this.selectedPerson) {
-      note.personId = this.selectedPerson.personId;
-      this.contactGroupService.addPersonNote(note).subscribe(data => {
-        if (data) {
-          this.contactGroupService.notesChanged(data);
-          this.toastr.success('Note successfully added');
-        }
-      });
-    } else if (note && this.data.personId) {
-      note.personId = this.data.personId;
-      this.contactGroupService.addPersonNote(note).subscribe(data => {
-        if (data) {
-         this.contactGroupService.notesChanged(data);
-          this.toastr.success('Note successfully added');
-        }
-        console.log('added person note with id', data);
-      });
-    }
-  }
-  private addContactNote() {
-    const note = { ...this.contactGroupNote, ...this.noteForm.value };
-    if (note && this.selectedContactGroup) {
-      note.contactGroupId = this.selectedContactGroup.contactGroupId;
-      this.contactGroupService.addContactGroupNote(note).subscribe(data => {
-        if (data) {
-        this.contactGroupService.notesChanged(data);
-          this.toastr.success('Note successfully added');
-        }
-        console.log('added  contact group note', data);
-      });
-    }
   }
 }
