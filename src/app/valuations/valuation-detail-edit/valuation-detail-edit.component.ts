4af4d6892f1814dcc865a2272ee37c7edf52c415
dngAlexA <aagyapong@dng.co.uk>
2020-01-07 09:47:30 +0000

    Setup ValuationForm (Initial)

--- src/app/valuations/valuation-detail-edit/valuation-detail-edit.component.ts
+++ src/app/valuations/valuation-detail-edit/valuation-detail-edit.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Property } from 'src/app/property/shared/property';
 import { Signer } from 'src/app/contactgroups/shared/contact-group';
+import { ValuationService } from '../shared/valuation.service';
+import { ActivatedRoute } from '@angular/router';
+import { Valuation } from '../shared/valuation';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-valuation-detail-edit',
@@ -19,17 +23,52 @@ export class ValuationDetailEditComponent implements OnInit {
       fullName: 'Bill Doe'
     }
 
-  ]
+  ];
 
-  showCalendar: boolean = false;
+  showCalendar = false;
+  valuationId: number;
+  valuation: Valuation;
+  lastKnownOwner: Signer;
+  valuationForm: FormGroup;
 
-  constructor() { }
+  constructor(private valuationService: ValuationService, private route: ActivatedRoute, private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.setupForm();
+    this.valuationId = +this.route.snapshot.paramMap.get('id');
+    if (this.valuationId) {
+      this.getValuation(this.valuationId);
+    }
+  }
+
+  setupForm() {
+    this.valuationForm = this.fb.group({
+      outside: [null],
+      parking: [null],
+      features: [null]
+    });
   }
 
   selectStaffMember(staffMember: any) {
     this.showCalendar = true;
+  }
+
+  getValuation(id: number) {
+    this.valuationService.getValuation(id).subscribe((data => {
+      if (data) {
+        this.valuation = data;
+        this.populateForm(data);
+      }
+    }));
+  }
+
+  populateForm(valuation: Valuation) {
+    if (this.valuationForm) {
+      this.valuationForm.reset();
+    }
+    if (valuation) {
+
+    }
   }
 
   getSelectedProperty(property: Property) {
@@ -37,8 +76,10 @@ export class ValuationDetailEditComponent implements OnInit {
       console.log('selected property', property);
     }
   }
-  getSelectedOwner(owner: Signer){
+
+  getSelectedOwner(owner: Signer) {
     if (owner) {
+      this.lastKnownOwner = owner;
       console.log('selected owner', owner);
     }
   }
