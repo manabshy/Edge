8e2c008bfb025c91399bdec71926c74435b5fc8d
dngElia <efulchignoni@dng.co.uk>
2020-01-27 13:43:05 +0000

    remove last known owner

--- src/app/property/property-detail-edit/property-detail-edit.component.ts
+++ src/app/property/property-detail-edit/property-detail-edit.component.ts
@@ -208,6 +208,7 @@ export class PropertyDetailEditComponent implements OnInit {
   }
 
   getSelectedOwner(owner: Signer) {
+    console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',owner);
     if (this.lastKnownOwner !== owner) {
       this.propertyForm.markAsDirty();
     } else {
@@ -293,7 +294,7 @@ export class PropertyDetailEditComponent implements OnInit {
     const property = { ...this.propertyDetails, ...this.propertyForm.value };
     if (this.propertyDetails) {
       propertyAddress = { ...this.propertyDetails.address, ...this.propertyAddress };
-      this.lastKnownOwner ? property.lastKnownOwner = this.lastKnownOwner : property.lastKnownOwner = this.propertyDetails.lastKnownOwner;
+      property.lastKnownOwner = this.lastKnownOwner;
       property.address = propertyAddress;
     } else {
       property.lastKnownOwner = this.lastKnownOwner;
