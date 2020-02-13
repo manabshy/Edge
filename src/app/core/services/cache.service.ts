5cfc51c4d4db7ba78c3630be895c09ec0fd91ddb
dngAlexA <aagyapong@dng.co.uk>
2020-02-13 11:43:07 +0000

    Add cache service

---
+++ src/app/core/services/cache.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { StorageMap } from '@ngx-pwa/local-storage';
+import { HttpClient } from '@angular/common/http';
+import { CacheStatus, ResultData } from 'src/app/shared/result-data';
+import { InfoService } from './info.service';
+import { OfficeService } from './office.service';
+import { StaffMemberService } from './staff-member.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CacheService {
+
+  constructor(private infoService: InfoService,
+    private officeService: OfficeService,
+    private staffMemberService: StaffMemberService,
+    private storage: StorageMap) { }
+
+  invalidateCache(response?: ResultData | any) {
+    let cache: CacheStatus;
+    if (response) {
+      cache = response.cacheStatus;
+      console.log('cache status from response', cache);
+
+      this.storage.get('cacheStatus').subscribe((result: CacheStatus) => {
+        console.log('existing cache status here..', result);
+        if (result.info !== cache.info) {
+          this.storage.delete('info').subscribe();
+          this.infoService.getDropdownListInfo().subscribe();
+        }
+        if (result.offices !== cache.offices) {
+          this.storage.delete('offices').subscribe();
+          this.officeService.getOffices().subscribe();
+        }
+        if (result.staffMembers !== cache.staffMembers) {
+          this.storage.delete('allstaffmembers').subscribe();
+          this.staffMemberService.getAllStaffMembers().subscribe();
+        }
+      });
+      // switch (true) {
+      //   case !!cache.info:
+      //     this.invalidateInfoCache(cache.info);
+      //     break;
+      //   case !!cache.offices:
+      //     this.invalidateOfficesCache(cache.offices)
+      //     break;
+      //   case !!cache.staffMembers:
+      //     this.invalidateStaffMembersCache(cache.staffMembers)
+      //     break;
+      // }
+    }
+  }
+
+}
