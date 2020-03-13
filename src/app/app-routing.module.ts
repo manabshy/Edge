dfbf45a3efa90d63c29bcedfbab43c133eb2772e
dngAlexA <aagyapong@dng.co.uk>
2020-03-13 10:20:48 +0000

    Remove unused routes from appRoutingModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,26 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import { PropertyDetailsLettingComponent } from './property-details-letting/property-details-letting.component';
-import { PropertyDetailsSaleComponent } from './property-details-sale/property-details-sale.component';
-import { LeadEditComponent } from './leads/lead-edit/lead-edit.component';
-import { LeadRegisterComponent } from './leads/lead-register/lead-register.component';
-import { ApplicantRegisterComponent } from './applicant-register/applicant-register.component';
-import { ApplicantViewLettingComponent } from './applicant-view-letting/applicant-view-letting.component';
-import { ApplicantViewSaleComponent } from './applicant-view-sale/applicant-view-sale.component';
-import { SendEdetailsComponent } from './send-edetails/send-edetails.component';
 import { AuthGuardService } from './core/services/auth-guard.service';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { SelectiveStrategyService } from './core/services/selective-strategy.service';
 import { ImpersonateMemberComponent } from './impersonate-member/impersonate-member.component';
 
 const routes: Routes = [
-  { path: 'property-details-letting', component: PropertyDetailsLettingComponent, canActivate: [AuthGuardService] },
-  { path: 'property-details-sale', component: PropertyDetailsSaleComponent, canActivate: [AuthGuardService] },
-  { path: 'applicant-register', component: ApplicantRegisterComponent, canActivate: [AuthGuardService] },
-  { path: 'applicant-view-letting', component: ApplicantViewLettingComponent, canActivate: [AuthGuardService] },
-  { path: 'applicant-view-sale', component: ApplicantViewSaleComponent, canActivate: [AuthGuardService] },
-  { path: 'send-edetails', component: SendEdetailsComponent, canActivate: [AuthGuardService] },
   { path: 'impersonate-member', component: ImpersonateMemberComponent, canActivate: [AuthGuardService] },
   {
     path: 'leads-register',
