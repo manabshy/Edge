f675c58c3d88113970553d6e7743aa3bf60182a7
dngAlexA <aagyapong@dng.co.uk>
2020-04-08 13:13:55 +0100

    Add validation to note form

--- src/app/shared/note-form/note-form.component.ts
+++ src/app/shared/note-form/note-form.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, FormControl } from '@angular/forms';
 import { ContactNote, ContactGroup } from 'src/app/contactgroups/shared/contact-group';
 import { Person } from '../models/person';
 import { ContactGroupsService } from 'src/app/contactgroups/shared/contact-groups.service';
 import { ToastrService } from 'ngx-toastr';
+import { FormErrors, ValidationMessages } from 'src/app/core/shared/app-constants';
+import { SharedService } from 'src/app/core/services/shared.service';
+import { WedgeValidators } from '../wedge-validators';
 
 @Component({
   selector: 'app-note-form',
@@ -19,15 +22,21 @@ export class NoteFormComponent implements OnInit {
   @Input() selectedContactGroup: ContactGroup;
   @Input() isPersonNote: boolean;
   @Input() data;
-  @Input() isCancelVisible: boolean = false;
+  @Input() isCancelVisible = false;
   @Output() actionEmit = new EventEmitter<boolean>();
-  @ViewChild('note', {static: true}) noteComponent: ElementRef;
+  @ViewChild('note', { static: true }) noteComponent: ElementRef;
   shortcuts = {
     'Left Message': 'Left message',
     'SMS': 'Sent an SMS',
     'Emailed': 'Emailed'
   };
+
+  get textControl() {
+    return this.noteForm.get('text') as FormControl;
+  }
+
   public keepOriginalOrder = (a) => a.key;
+  formErrors = FormErrors;
 
   constructor(private fb: FormBuilder,
     private contactGroupService: ContactGroupsService,
@@ -35,21 +44,21 @@ export class NoteFormComponent implements OnInit {
 
   ngOnInit() {
     this.formInit();
+    this.noteForm.valueChanges.subscribe(() => this.logValidationErrors(this.noteForm, false));
   }
 
   formInit() {
     this.noteForm = this.fb.group({
       isImportant: false,
       isPinned: false,
-      text: ['']
+      text: ['', [Validators.required, Validators.minLength(1)]]
     });
-    this.noteForm.updateValueAndValidity();
   }
 
 
   consumeShortcut(shortcut: string) {
-    const textControl = this.noteForm.get('text');
-    let textValue = textControl.value || '';
+    this.noteForm.markAsDirty();
+    let textValue = this.textControl.value || '';
     if (textValue.includes(shortcut)) {
       if (textValue.includes(shortcut + ', ')) {
         textValue = textValue.replace(shortcut + ', ', '');
@@ -59,13 +68,25 @@ export class NoteFormComponent implements OnInit {
     } else {
       textValue = textValue.slice(0, 0) + shortcut + ', ' + textValue.slice(0);
     }
+
     textValue = textValue.replace(/,\s*$/, '');
-    textControl.setValue(textValue.replace(/,\s*$/, '') + ' ');
+    this.textControl.setValue(textValue.replace(/,\s*$/, '') + ' ');
+    this.setNoWhiteSpaceValidator(this.textControl);
     this.noteComponent.nativeElement.focus();
   }
 
+  setNoWhiteSpaceValidator(control: AbstractControl) {
+    if (control.value.trim().length === 0) {
+      control.setValidators(WedgeValidators.noWhitespaceValidator);
+      control.updateValueAndValidity();
+    } else {
+      control.clearValidators();
+      control.updateValueAndValidity();
+    }
+  }
+
   ctrlEnterSubmit(e) {
-    if (e.ctrlKey && e.keyCode == 13) {
+    if (e.ctrlKey && e.keyCode === 13) {
       this.action(true);
     }
   }
@@ -79,14 +100,57 @@ export class NoteFormComponent implements OnInit {
     this.actionEmit.emit(value);
   }
 
+
+  /* Duplication of logValidationErrors() and scrollToFirstInvalidField() due to cyclic dependency.
+     This component is used in the shared service so we can't
+     inject the shared service here.
+  */
+
+  logValidationErrors(group: FormGroup = this.noteForm, fakeTouched: boolean, scrollToError = false) {
+    Object.keys(group.controls).forEach((key: string) => {
+      const control = group.get(key);
+      const messages = ValidationMessages[key];
+      if (control.valid) {
+        FormErrors[key] = '';
+      }
+      if (control && !control.valid && (fakeTouched || control.dirty)) {
+        FormErrors[key] = '';
+        console.log('errors in prop edit', control.errors)
+        for (const errorKey in control.errors) {
+          if (errorKey) {
+            FormErrors[key] += messages[errorKey] + '\n';
+          }
+        }
+      }
+      if (control instanceof FormGroup) {
+        this.logValidationErrors(control, fakeTouched);
+      }
+    });
+    if (scrollToError) {
+      this.scrollToFirstInvalidField();
+    }
+  }
+
+  scrollToFirstInvalidField() {
+    const invalidFields = document.getElementsByClassName('is-invalid');
+    if (invalidFields.length) {
+      setTimeout(() => {
+        if (invalidFields[0]) {
+          invalidFields[0].scrollIntoView({ block: 'center' });
+        }
+      });
+    }
+  }
+
   saveNote() {
     console.log(this.data);
-    this.isPersonNote || this.data.isPersonNote || this.data.personId ? this.addPersonNote() : this.addContactNote();
-    // if (this.noteForm.valid) {
-    //   if (this.noteForm.dirty) {
-    //     this.isPersonNote || this.data.isPersonNote || this.data.personId ? this.addPersonNote() : this.addContactNote();
-    //   }
-    // }
+    this.logValidationErrors(this.noteForm, true, true);
+    if (this.noteForm.valid) {
+      if (this.noteForm.dirty) {
+        console.log('note form is valid and dirty', this.noteForm.value);
+        this.isPersonNote || this.data.isPersonNote || this.data.personId ? this.addPersonNote() : this.addContactNote();
+      }
+    }
   }
 
   private addPersonNote() {
@@ -106,21 +170,21 @@ export class NoteFormComponent implements OnInit {
       });
     }
   }
+
   private addContactNote() {
-    const note = { ...this.contactGroupNote, ...this.noteForm.value };
+    const note = { ...this.contactGroupNote, ...this.noteForm.value } as ContactNote;
     if (note && this.selectedContactGroup) {
       note.contactGroupId = this.selectedContactGroup.contactGroupId;
     } else if (note && this.data.group.contactGroupId) {
       note.contactGroupId = this.data.group.contactGroupId;
     }
-    if (note) {
+    if (note && note.text) {
       this.contactGroupService.addContactGroupNote(note).subscribe(data => {
         if (data) {
           this.formReset();
           this.contactGroupService.notesChanged(data);
           this.toastr.success('Note successfully added');
         }
-        console.log('added  contact group note', data);
       });
     }
   }
