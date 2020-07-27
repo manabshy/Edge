cac1af76ec16b60bc4d38a641f1a47dcb1c53952
dngAlexA <aagyapong@dng.co.uk>
2020-07-27 12:26:02 +0100

    Change api endpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://dandg-api-dev-wedge.azurewebsites.net/v10',
+  baseUrl: 'https://dandg-api-wedge-dev.azurewebsites.net/v10',
   baseRedirectUri: 'https://dandg-edge-dev.azurewebsites.net',
   endpointUrl: 'https://dandg-api-wedge-dev.azurewebsites.net'
 };
