b99bd59c95e7f1ee371a497ab0dd5dff7491b759
dngAlexA <aagyapong@dng.co.uk>
2020-07-28 16:06:25 +0100

    Add createdBy and badgeId to TeamMemberPoint interface

--- src/app/client-services/admin-panel-details/admin-panel-details.component.spec.ts
+++ src/app/client-services/admin-panel-details/admin-panel-details.component.spec.ts
@@ -21,24 +21,41 @@ fdescribe('AdminPanelDetailsComponent', () => {
   let component: AdminPanelDetailsComponent;
   let fixture: ComponentFixture<AdminPanelDetailsComponent>;
   let boardService: CsBoardService;
-  const memberPoints: any[] = [
+  const memberPoints: TeamMemberPoint[] = [
     {
-      date: '21/06/2020',
-      reason: 'Inbound Valuation',
-      points: 200
+      badgeId: 1,
+      createdBy: 'System',
+      dateTime: new Date('2020-07-23T16:00:22.847'),
+      pointId: 91,
+      pointTypeId: 9,
+      points: 500,
+      reason: 'Golden Stag',
+      staffMemberId: 2430
     },
     {
-      date: '21/06/2020',
-      reason: 'Outbound Valuation',
-      points: 250
+      badgeId: null,
+      createdBy: 'System',
+      dateTime: new Date('2020-07-27T14:38:55.373'),
+      pointId: 296,
+      pointTypeId: 2,
+      points: 20,
+      reason: '60 Abbeville Road, London SW4 9NE',
+      staffMemberId: 2430
     }
   ];
 
+  const newPoint = { pointTypeId: 9, points: 100, reason: 'Test', animate: false, createdBy: 'Rebecca Davies' } as unknown as TeamMemberPoint;
   const teamMembers: TeamMember[] = [
     {
-      staffMemberId: 1,
-      name: 'Melissa D\'Angelo',
-      photoUrl: 'assets/images/leaf_rake.png',
+      staffMemberId: 2430,
+      name: 'Shanil Khagram',
+      photoUrl: 'https://dandglive.blob.core.windows.net/staffmembers-public%5Cs-2430%5Cstaff_2430_90e73db2af0c4b1f97c4f4266a89c82f_Shanil+Khagram.png',
+      points: memberPoints
+    },
+    {
+      staffMemberId: 2622,
+      name: 'Rebecca Davies',
+      photoUrl: 'https://dandglive.blob.core.windows.net/staffmembers-public%5Cs-2622%5Cstaff_2622_4ddb3f08db664ad99c0d060942f9f917_REBECCA+DAVIES+150px+x+150px+Grey.jpg',
       points: memberPoints
     }
   ];
@@ -120,6 +137,20 @@ fdescribe('AdminPanelDetailsComponent', () => {
 
     tick();
 
+  }));
+
+  it('should update team member points with an added point', fakeAsync(() => {
+    spyOn(boardService, 'getCsTeamMemberDetails').and.returnValue(of(teamMembers[0].points));
+    spyOn(boardService, 'addPoint').and.returnValue(of(true));
+
+    component.ngOnInit();
+    component.memberDetails$.subscribe(points => {
+      component.teamMemberPoints = points;
+    });
+    component.updateTeamMemberPoints(newPoint);
+    tick();
+
+    expect(component.teamMemberPoints.length).toBe(3);
   }));
 
 });
