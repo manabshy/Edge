53fe772e78b62826400e8f1af993f851c057e17c
dngAlexA <aagyapong@dng.co.uk>
2020-08-07 15:15:42 +0100

    Add configs loader service

---
+++ src/app/configs-loader.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpBackend } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ConfigsLoaderService {
+  private httpClient: HttpClient;
+  private configs: Configs;
+  constructor(handler: HttpBackend) {
+    this.httpClient = new HttpClient(handler);
+  }
+  get ApiEndpoint() {
+    return this.configs.apiEndpoint;
+  }
+
+  get AppEndpoint() {
+    return this.configs.appEndpoint;
+  }
+
+  get ConfigsLoadedFrom() {
+    return this.configs.configsLoadedFrom;
+  }
+
+  public async loadConfigs(): Promise<any> {
+    return this.httpClient.get('assets/configs.json').pipe(settings => settings)
+      .toPromise()
+      .then(settings => {
+        this.configs = settings as Configs;
+        console.log('configs here to be replaced', this.configs);
+      });
+  }
+}
+
+
+export interface Configs {
+  apiEndpoint: string;
+  appEndpoint: string;
+  configsLoadedFrom: string;
+}
