7a9f9a55b009ee2b5b1624170eeb6af487b7b090
dngAlexA <aagyapong@dng.co.uk>
2020-08-10 10:40:09 +0100

    Add redirectUri to env variables for testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -82,7 +82,7 @@ export class AppComponent extends BaseComponent implements OnInit, AfterViewChec
   }
 
   ngOnInit() {
-    console.log('ApiEndpoint in ngonit', environment.baseUrl, 'appEndpoint', environment.baseRedirectUri);
+    console.log('BASEURL in ngonit', environment.baseUrl, 'REDIRECTURI', environment.baseRedirectUri);
     this.toastr.overlayContainer = this.toastContainer;
     console.log('instance initiliased');
     if (this.isLoggedIn) {
@@ -176,7 +176,7 @@ export class AppComponent extends BaseComponent implements OnInit, AfterViewChec
       environment.baseUrl = this.configLoaderService.ApiEndpoint;
       environment.endpointUrl = this.configLoaderService.ApiEndpoint;
       environment.baseRedirectUri = this.configLoaderService.AppEndpoint;
-      console.log('ApiEndpoint in if state', this.configLoaderService.ApiEndpoint, 'appEndpoint', this.configLoaderService.AppEndpoint);
+      console.log('ApiEndpoint', this.configLoaderService.ApiEndpoint, 'appEndpoint', this.configLoaderService.AppEndpoint);
     }
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   baseUrl: '',
-  baseRedirectUri: '',
+  baseRedirectUri: 'https://dandg-edge-dev.azurewebsites.net',
   endpointUrl: ''
 };
