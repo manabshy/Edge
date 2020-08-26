c684786979245b16a3820262e2d0580612c9591f
dngAlexA <aagyapong@dng.co.uk>
2020-08-26 15:53:29 +0100

    Update cypress and add tsconfig.json for cypress

--- cypress/integration/property-center.spec.ts
+++ cypress/integration/property-center.spec.ts
@@ -1,22 +1,22 @@
-/// <reference types="Cypress" />
+// /// <reference types="Cypress" />
 describe('Property center should', () => {
   beforeEach(() => {
-  })
+  });
 
   it('show login page', () => {
-    cy.visit('/login')
+    cy.visit('/login');
     cy.contains('p', ' Sign in with your work email');
-  })
+  });
 
   it('search for properties using a search term', () => {
-    cy.visit('/property-centre')
+    cy.visit('/property-centre');
     cy.get('[type="search"]')
       .type('road')
       .should('have.value', 'road');
     cy.get('.btn-secondary')
       .should('have.text', 'Search')
-      .click()
-    cy.get('.list-group-item').should('be.visible')
-  })
+      .click();
+    cy.get('.list-group-item').should('be.visible');
+  });
 
-})
+});
