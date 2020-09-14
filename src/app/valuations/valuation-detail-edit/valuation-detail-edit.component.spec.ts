45e425f01ff2746de3074f5cd43c18690e525580
dngAlexA <aagyapong@dng.co.uk>
2020-09-14 16:52:10 +0100

    Add seperate describe blocks for valuation edit component and template tests

--- src/app/valuations/valuation-detail-edit/valuation-detail-edit.component.spec.ts
+++ src/app/valuations/valuation-detail-edit/valuation-detail-edit.component.spec.ts
@@ -29,17 +29,17 @@ import { SignerComponent } from 'src/app/shared/signer/signer.component';
 import { BreadcrumbComponent } from 'src/app/shared/breadcrumb/breadcrumb.component';
 import { HttpClient } from '@angular/common/http';
 import { NgSelectModule } from '@ng-select/ng-select';
-import {MockVals, mockAllValuers } from 'src/testing/fixture-data/valuations-data';
+import { MockVals, mockAllValuers } from 'src/testing/fixture-data/valuations-data';
 import { Valuation, Valuer, ValuationStatusEnum } from '../shared/valuation';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
 import { MockDropdownListInfo } from 'src/testing/fixture-data/dropdown-list-data.json';
 
 let component: ValuationDetailEditComponent;
 let fixture: ComponentFixture<ValuationDetailEditComponent>;
-let activatedRoute: ActivatedRouteStub;
 let propertyService;
 // let Location :Location
-const property = {'propertyId': 68847,
+const property = {
+  'propertyId': 68847,
   'bedrooms': 1,
   'bathrooms': null,
   'receptions': 1,
@@ -50,21 +50,23 @@ const property = {'propertyId': 68847,
   'outsideSpace': null,
   'propertyFeature': null,
   'valuers': [
-    {'staffMemberId': 2467, 'firstName': 'Georgia', 'lastName': 'Jakstys', 'fullName': 'Georgia Jakstys', emailAddress: null, hasReminder: false, exchangeGUID: ''},
-    {'staffMemberId': 2530, 'firstName': 'Charlotte', 'lastName': 'Sweeney', 'fullName': 'Charlotte Sweeney', emailAddress: null, hasReminder: false, exchangeGUID: ''}]
+    { 'staffMemberId': 2467, 'firstName': 'Georgia', 'lastName': 'Jakstys', 'fullName': 'Georgia Jakstys', emailAddress: null, hasReminder: false, exchangeGUID: '' },
+    { 'staffMemberId': 2530, 'firstName': 'Charlotte', 'lastName': 'Sweeney', 'fullName': 'Charlotte Sweeney', emailAddress: null, hasReminder: false, exchangeGUID: '' }]
 } as Property;
-const owner = {'contactGroupId': 239920,
-'contactNames': 'Mrs Fatima Saada',
-'companyName': null,
-'phoneNumber': null,
-'emailAddress': null};
+const owner = {
+  'contactGroupId': 239920,
+  'contactNames': 'Mrs Fatima Saada',
+  'companyName': null,
+  'phoneNumber': null,
+  'emailAddress': null
+};
 
 const mockVals = MockVals;
-const tenures = [ {id: 1, value: 'Freehold'},
-                {id: 2, value: 'Share of Freehold'},
-                {id: 3, value: 'Leasehold'}];
+const tenures = [{ id: 1, value: 'Freehold' },
+{ id: 2, value: 'Share of Freehold' },
+{ id: 3, value: 'Leasehold' }];
 
-describe('ValuationDetailEditComponent', () => {
+fdescribe('ValuationDetailEditComponent', () => {
   const routerSpy = createRouterSpy();
   const storageMapSpy = createStorageMapSpy();
   beforeEach(async(() => {
@@ -148,196 +150,194 @@ describe('ValuationDetailEditComponent', () => {
     // let service = new PropertyService(null)
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-  it('should contain a title', () => {
-    const titleElem = fixture.debugElement.query(By.css( 'h4'));
-    expect(titleElem.nativeElement.textContent).toBe('Valuation');
-  });
-  it('should have a Actions button', () => {
-    const buttons = fixture.debugElement.queryAll(By.css('button'));
-    const actionsButton = buttons[0].nativeElement;
-    expect(actionsButton.textContent).toBe(' Actions ');
-  });
-  it('should NOT have a Actions button if its newValuation', () => {
-    component.isNewValuation = true;
-    fixture.detectChanges();
-    const buttons = fixture.debugElement.queryAll(By.css('button'));
-    const actionsButton = buttons[0].nativeElement;
-    expect(actionsButton.textContent).not.toBe(' Actions ');
-  });
+  describe('Template', () => {
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+    it('should contain a title', () => {
+      const titleElem = fixture.debugElement.query(By.css('h4'));
+      expect(titleElem.nativeElement.textContent).toBe('Valuation');
+    });
+    it('should have a Actions button', () => {
+      const buttons = fixture.debugElement.queryAll(By.css('button'));
+      const actionsButton = buttons[0].nativeElement;
+      expect(actionsButton.textContent).toBe(' Actions ');
+    });
+    it('should NOT have a Actions button if its newValuation', () => {
+      component.isNewValuation = true;
+      fixture.detectChanges();
+      const buttons = fixture.debugElement.queryAll(By.css('button'));
+      const actionsButton = buttons[0].nativeElement;
+      expect(actionsButton.textContent).not.toBe(' Actions ');
+    });
 
-  it('should create valuation form', () => {
-    component.ngOnInit();
-    expect(component.valuationForm).toBeTruthy();
-  });
-  // xit('should have 2 buttons on the screen if isNewValuation is true', ()=>{
-  //   //test it from other components?
-  // })
-  // xit('should have property selected if isNewValuation is not set', ()=>{
-  //   //test it from other components?
-  // })
+    it('should create valuation form', () => {
+      component.ngOnInit();
+      expect(component.valuationForm).toBeTruthy();
+    });
 
-  it('should display form if property and owner selected', () => {
-    showForm(component);
-    fixture.detectChanges();
-    expect(fixture.debugElement.queryAll(By.css('fieldset')).length).toBeGreaterThanOrEqual(4);
+    it('should display form if property and owner selected', () => {
+      showForm(component);
+      fixture.detectChanges();
+      expect(fixture.debugElement.queryAll(By.css('fieldset')).length).toBeGreaterThanOrEqual(4);
 
-  });
-  it('should fill the form if calling populateform', () => {
-    showForm(component);
-    fixture.detectChanges();
-    // component.valuation=mockVals[0] as unknown as Valuation
-    component.populateForm(component.valuation);
-    fixture.detectChanges();
+    });
+    it('should fill the form if calling populateform', () => {
+      showForm(component);
+      fixture.detectChanges();
+      // component.valuation=mockVals[0] as unknown as Valuation
+      component.populateForm(component.valuation);
+      fixture.detectChanges();
 
-    expect(component.valuationForm.value.reason).toBe(mockVals[0].reason);
-    expect(component.valuationForm.value.bathrooms).toBe(mockVals[0].bathrooms);
-    expect(component.valuationForm.value.lettingsValuer).toBe(mockVals[0].lettingsValuer);
-    expect(component.valuationForm.value.timeFrame).toBe(mockVals[0].timeFrame);
-    expect(component.valuationForm.value.generalNotes).toBe(mockVals[0].generalNotes);
-  });
+      expect(component.valuationForm.value.reason).toBe(mockVals[0].reason);
+      expect(component.valuationForm.value.bathrooms).toBe(mockVals[0].bathrooms);
+      expect(component.valuationForm.value.lettingsValuer).toBe(mockVals[0].lettingsValuer);
+      expect(component.valuationForm.value.timeFrame).toBe(mockVals[0].timeFrame);
+      expect(component.valuationForm.value.generalNotes).toBe(mockVals[0].generalNotes);
+    });
 
-  it('should populate tenures', () => {
-    showForm(component);
-    component.tenures = tenures;
-    fixture.detectChanges();
+    it('should populate tenures', () => {
+      showForm(component);
+      component.tenures = tenures;
+      fixture.detectChanges();
 
-    const tenureId = fixture.debugElement.queryAll(By.css('#tenureId option'));
-    expect(tenureId.length).toBeGreaterThanOrEqual(2);
-  });
+      const tenureId = fixture.debugElement.queryAll(By.css('#tenureId option'));
+      expect(tenureId.length).toBeGreaterThanOrEqual(2);
+    });
 
-  it('should display lease expiry if leasehold selected', () => {
-    showForm(component);
-    component.populateForm(component.valuation);
-    fixture.detectChanges();
+    it('should display lease expiry if leasehold selected', () => {
+      showForm(component);
+      component.populateForm(component.valuation);
+      fixture.detectChanges();
 
-    component.valuationForm.patchValue({tenureId: 3});
-    component.onTenureChange(3);
-    fixture.detectChanges();
-    const expiryInput = fixture.debugElement.query(By.css('input[name="approxLeaseExpiryDate"]'));
-    expect(expiryInput).toBeTruthy();
-  });
-  it('should NOT display lease expiry if freehold selected', () => {
-    showForm(component);
-    component.populateForm(component.valuation);
-    fixture.detectChanges();
+      component.valuationForm.patchValue({ tenureId: 3 });
+      component.onTenureChange(3);
+      fixture.detectChanges();
+      const expiryInput = fixture.debugElement.query(By.css('input[name="approxLeaseExpiryDate"]'));
+      expect(expiryInput).toBeTruthy();
+    });
+    it('should NOT display lease expiry if freehold selected', () => {
+      showForm(component);
+      component.populateForm(component.valuation);
+      fixture.detectChanges();
 
-    component.valuationForm.patchValue({tenureId: 2});
-    component.onTenureChange(2);
-    fixture.detectChanges();
-    const expiryInput = fixture.debugElement.query(By.css('input[name="approxLeaseExpiryDate"]'));
+      component.valuationForm.patchValue({ tenureId: 2 });
+      component.onTenureChange(2);
+      fixture.detectChanges();
+      const expiryInput = fixture.debugElement.query(By.css('input[name="approxLeaseExpiryDate"]'));
 
-    expect(expiryInput).toBeFalsy();
-  });
-  it('should show lettings valuer dropdown only if \'lettings\' were clicked', () => {
-    showForm(component);
-    component.populateForm(component.valuation);
-    fixture.detectChanges();
-    clickOn('#valuationLettings');
-    expect(component.isLettingsOnly).toBeTruthy();
-  });
+      expect(expiryInput).toBeFalsy();
+    });
+    it('should show lettings valuer dropdown only if \'lettings\' were clicked', () => {
+      showForm(component);
+      component.populateForm(component.valuation);
+      fixture.detectChanges();
+      clickOn('#valuationLettings');
+      expect(component.isLettingsOnly).toBeTruthy();
+    });
 
-  it('should show sales valuer dropdown only if \'sales\' were clicked', () => {
-    showForm(component);
-    component.populateForm(component.valuation);
-    fixture.detectChanges();
-    clickOn('#valuationSales');
-    expect(component.isSalesOnly).toBeTruthy();
-  });
-  it('should show sales and lettings valuer dropdowns only if \'both\' were clicked', () => {
-    showForm(component);
-    component.populateForm(component.valuation);
-    fixture.detectChanges();
-    clickOn('#valuationBoth');
-    expect(component.isSalesAndLettings).toBeTruthy();
-  });
+    it('should show sales valuer dropdown only if \'sales\' were clicked', () => {
+      showForm(component);
+      component.populateForm(component.valuation);
+      fixture.detectChanges();
+      clickOn('#valuationSales');
+      expect(component.isSalesOnly).toBeTruthy();
+    });
+    it('should show sales and lettings valuer dropdowns only if \'both\' were clicked', () => {
+      showForm(component);
+      component.populateForm(component.valuation);
+      fixture.detectChanges();
+      clickOn('#valuationBoth');
+      expect(component.isSalesAndLettings).toBeTruthy();
+    });
 
-  it('should display error if reason are not set', () => {
-    showForm(component);
-    fixture.detectChanges();
-    clickOn('button[type="submit"]'); // save
-    fixture.detectChanges();
-    const error = fixture.debugElement.query(By.css('#reason + div'));
-    expect(error.nativeElement.textContent.length).toBeGreaterThanOrEqual(5);
-  });
-  it('should display error if timeframe are not set', () => {
-    showForm(component);
-    fixture.detectChanges();
-    clickOn('button[type="submit"]'); // save
-    fixture.detectChanges();
-    const error = fixture.debugElement.query(By.css('#timeFrame + div'));
-    expect(error.nativeElement.textContent.length).toBeGreaterThanOrEqual(5);
-  });
-  it('should display error if generalNotes are not set', () => {
-    showForm(component);
-    fixture.detectChanges();
-    clickOn('button[type="submit"]'); // save
-    fixture.detectChanges();
-    const error = fixture.debugElement.query(By.css('#generalNotes + div'));
-    expect(error.nativeElement.textContent.length).toBeGreaterThanOrEqual(5);
-  });
+    it('should display error if reason are not set', () => {
+      showForm(component);
+      fixture.detectChanges();
+      clickOn('button[type="submit"]'); // save
+      fixture.detectChanges();
+      const error = fixture.debugElement.query(By.css('#reason + div'));
+      expect(error.nativeElement.textContent.length).toBeGreaterThanOrEqual(5);
+    });
+    it('should display error if timeframe are not set', () => {
+      showForm(component);
+      fixture.detectChanges();
+      clickOn('button[type="submit"]'); // save
+      fixture.detectChanges();
+      const error = fixture.debugElement.query(By.css('#timeFrame + div'));
+      expect(error.nativeElement.textContent.length).toBeGreaterThanOrEqual(5);
+    });
+    it('should display error if generalNotes are not set', () => {
+      showForm(component);
+      fixture.detectChanges();
+      clickOn('button[type="submit"]'); // save
+      fixture.detectChanges();
+      const error = fixture.debugElement.query(By.css('#generalNotes + div'));
+      expect(error.nativeElement.textContent.length).toBeGreaterThanOrEqual(5);
+    });
 
-  it('should be in error if valuers are not selected', () => {
-    showForm(component);
-    fixture.detectChanges();
-    clickOn('#valuationBoth');
-    component.toggleValuerType();
-    fixture.detectChanges();
-    clickOn('button[type="submit"]'); // save
-    fixture.detectChanges();
-    expect(component.valuationForm.controls.salesValuerId.errors.required).toBeTruthy();
-    expect(component.valuationForm.controls.lettingsValuerId.errors.required).toBeTruthy();
-  });
-  it('should show an error if valuers are selected but no availability selected', () => {
-    showForm(component);
-    component.populateForm(component.valuation);
-    component.valuationForm.patchValue({valuationDate: null}); // make form invalid
-    fixture.detectChanges();
-    clickOn('#valuationBoth');
-    component.toggleValuerType();
-    fixture.detectChanges();
-    clickOn('button[type="submit"]'); // save
-    fixture.detectChanges();
-    const warning = fixture.debugElement.query(By.css('.form-group.alert.alert-warning'));
-    expect(warning.nativeElement.textContent.length).toBeGreaterThanOrEqual(5);
-  });
+    it('should be in error if valuers are not selected', () => {
+      showForm(component);
+      fixture.detectChanges();
+      clickOn('#valuationBoth');
+      component.toggleValuerType();
+      fixture.detectChanges();
+      clickOn('button[type="submit"]'); // save
+      fixture.detectChanges();
+      expect(component.valuationForm.controls.salesValuerId.errors.required).toBeTruthy();
+      expect(component.valuationForm.controls.lettingsValuerId.errors.required).toBeTruthy();
+    });
+    it('should show an error if valuers are selected but no availability selected', () => {
+      showForm(component);
+      component.populateForm(component.valuation);
+      component.valuationForm.patchValue({ valuationDate: null }); // make form invalid
+      fixture.detectChanges();
+      clickOn('#valuationBoth');
+      component.toggleValuerType();
+      fixture.detectChanges();
+      clickOn('button[type="submit"]'); // save
+      fixture.detectChanges();
+      const warning = fixture.debugElement.query(By.css('.form-group.alert.alert-warning'));
+      expect(warning.nativeElement.textContent.length).toBeGreaterThanOrEqual(5);
+    });
 
-  it('should show an error if lease expiry is present but not selected', () => {
-    showForm(component);
-    component.tenures = tenures;
-    component.populateForm(component.valuation);
-    fixture.detectChanges();
+    it('should show an error if lease expiry is present but not selected', () => {
+      showForm(component);
+      component.tenures = tenures;
+      component.populateForm(component.valuation);
+      fixture.detectChanges();
 
-    component.valuationForm.patchValue({valuationDate: null}); // make form invalid
-    component.valuationForm.patchValue({tenureId: 3});
-    fixture.detectChanges();
-    component.onTenureChange(3);
-    const expiryInput = fixture.debugElement.query(By.css('input[name="approxLeaseExpiryDate"]'));
-    console.log(expiryInput);
-    clickOn('button[type="submit"]'); // save
-    fixture.detectChanges();
+      component.valuationForm.patchValue({ valuationDate: null }); // make form invalid
+      component.valuationForm.patchValue({ tenureId: 3 });
+      fixture.detectChanges();
+      component.onTenureChange(3);
+      const expiryInput = fixture.debugElement.query(By.css('input[name="approxLeaseExpiryDate"]'));
+      console.log(expiryInput);
+      clickOn('button[type="submit"]'); // save
+      fixture.detectChanges();
 
-    const error = fixture.debugElement.query(By.css('#approxLeaseExpiryDate + div'));
-    console.log(error);
-    expect(error.nativeElement.textContent.length).toBeGreaterThanOrEqual(5);
-  });
+      const error = fixture.debugElement.query(By.css('#approxLeaseExpiryDate + div'));
+      console.log(error);
+      expect(error.nativeElement.textContent.length).toBeGreaterThanOrEqual(5);
+    });
 
 
-  /**services starts here */
-  it('should call getproperty if property id is set', fakeAsync(() => {
-    spyOn(propertyService, 'getProperty').and.returnValue(of(property));
-    component.propertyId = 5;
-    fixture.detectChanges();
-    component.getPropertyDetails();
-    fixture.detectChanges();
-    tick();
-    expect(propertyService.getProperty).toHaveBeenCalledTimes(1);
-    // expect(service.getProperty).toHaveBeenCalledTimes(1)
-    expect(component.property).toEqual(property);
-  }));
+  });
 
+  describe('Component', () => {
+    /**services starts here */
+    it('should call getproperty if property id is set', fakeAsync(() => {
+      spyOn(propertyService, 'getProperty').and.returnValue(of(property));
+      component.propertyId = 5;
+      fixture.detectChanges();
+      component.getPropertyDetails();
+      fixture.detectChanges();
+      tick();
+      expect(propertyService.getProperty).toHaveBeenCalledTimes(1);
+      expect(component.property).toEqual(property);
+    }));
+
+  });
 });
 
 function showForm(component) {
