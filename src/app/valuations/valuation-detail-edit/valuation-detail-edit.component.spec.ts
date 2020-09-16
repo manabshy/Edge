7d04aad6a995a616e4bfade0c47d20b4a84e82d7
dngAlexA <aagyapong@dng.co.uk>
2020-09-16 16:14:36 +0100

    Test: Small refactoring

--- src/app/valuations/valuation-detail-edit/valuation-detail-edit.component.spec.ts
+++ src/app/valuations/valuation-detail-edit/valuation-detail-edit.component.spec.ts
@@ -1,39 +1,25 @@
 import { async, ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
 
 import { ValuationDetailEditComponent } from './valuation-detail-edit.component';
-import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { ReactiveFormsModule, FormsModule, FormBuilder } from '@angular/forms';
-import { SharedService } from 'src/app/core/services/shared.service';
-import { SharedServiceStub } from 'src/testing/shared-service-stub';
-import { Router, ActivatedRoute } from '@angular/router';
-import { ActivatedRouteStub } from 'src/testing/activated-route-stub';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { ValuationService } from '../shared/valuation.service';
-import { ContactGroupsService } from 'src/app/contactgroups/shared/contact-groups.service';
-import { StaffMemberService } from 'src/app/core/services/staff-member.service';
 import { ToastrService, ToastrModule } from 'ngx-toastr';
-import { StorageMap } from '@ngx-pwa/local-storage';
-import { InfoService } from 'src/app/core/services/info.service';
-import { BsModalService, BsModalRef, ModalDirective, ModalModule } from 'ngx-bootstrap/modal';
+import { BsModalService, BsModalRef, ModalModule } from 'ngx-bootstrap/modal';
 import { createStorageMapSpy } from 'src/testing/test-spies';
 import { of } from 'rxjs';
 import { PropertyService } from 'src/app/property/shared/property.service';
 import { By } from '@angular/platform-browser';
-import { BaseProperty } from 'src/app/shared/models/base-property';
 import { Property } from 'src/app/property/shared/property';
-import { BsDatepickerModule, MonthPickerComponent } from 'ngx-bootstrap/datepicker';
-import { PropertyFinderComponent } from 'src/app/shared/property-finder/property-finder.component';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 // import { MockComponent, MockedComponent, MockRender } from 'ng-mocks';
-import { SignerComponent } from 'src/app/shared/signer/signer.component';
-import { BreadcrumbComponent } from 'src/app/shared/breadcrumb/breadcrumb.component';
-import { HttpClient } from '@angular/common/http';
 import { NgSelectModule } from '@ng-select/ng-select';
-import { MockVals, mockAllValuers, MockValuations } from 'src/testing/fixture-data/valuations-data';
-import { Valuation, Valuer, ValuationStatusEnum } from '../shared/valuation';
+import { MockVals, mockAllValuers } from 'src/testing/fixture-data/valuations-data';
+import { Valuation, Valuer } from '../shared/valuation';
 import { ButtonsModule } from 'ngx-bootstrap/buttons';
 import { MockDropdownListInfo } from 'src/testing/fixture-data/dropdown-list-data.json';
-import { BasicStaffMember } from 'src/app/dashboard/shared/dashboard';
 import { BaseStaffMember } from 'src/app/shared/models/base-staff-member';
 
 let component: ValuationDetailEditComponent;
@@ -54,8 +40,14 @@ const property = {
   'outsideSpace': null,
   'propertyFeature': null,
   'valuers': [
-    { 'staffMemberId': 2467, 'firstName': 'Georgia', 'lastName': 'Jakstys', 'fullName': 'Georgia Jakstys', emailAddress: null, hasReminder: false, exchangeGUID: '' },
-    { 'staffMemberId': 2530, 'firstName': 'Charlotte', 'lastName': 'Sweeney', 'fullName': 'Charlotte Sweeney', emailAddress: null, hasReminder: false, exchangeGUID: '' }]
+    {
+      'staffMemberId': 2467, 'firstName': 'Georgia', 'lastName': 'Jakstys', 'fullName': 'Georgia Jakstys',
+      emailAddress: null, hasReminder: false, exchangeGUID: ''
+    },
+    {
+      'staffMemberId': 2530, 'firstName': 'Charlotte', 'lastName': 'Sweeney', 'fullName': 'Charlotte Sweeney',
+      emailAddress: null, hasReminder: false, exchangeGUID: ''
+    }]
 } as Property;
 const owner = {
   'contactGroupId': 239920,
@@ -71,7 +63,6 @@ const tenures = [{ id: 1, value: 'Freehold' },
 { id: 3, value: 'Leasehold' }];
 
 fdescribe('ValuationDetailEditComponent', () => {
-  const routerSpy = createRouterSpy();
   const storageMapSpy = createStorageMapSpy();
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -92,48 +83,10 @@ fdescribe('ValuationDetailEditComponent', () => {
         BsDatepickerModule.forRoot()
       ],
       providers: [
-        // { provide: ValuationService, useValue: {} },
         BsModalService,
         BsModalRef,
         ToastrService,
         PropertyService
-
-        // {provide: PropertyService, useValue:{
-        //   getProperty: ()=>of(property),
-        //   setAddedProperty :()=>{}
-        // }}
-        // { provide: ContactGroupsService, useValue: {} },
-        // { provide: SharedService, useValue: SharedServiceStub },
-        // { provide: StaffMemberService, useValue: {} },
-        // { provide: ToastrService, useValue: {} },
-        // { provide: StorageMap, useValue: storageMapSpy },
-        // { provide: InfoService, useValue: {} },
-        // { provide: Router, useValue: routerSpy },
-        // { provide: ActivatedRoute, useValue: activatedRoute },
-        // {
-        //   provide: ActivatedRoute, useValue: {
-        //     snapshot: {
-        //       paramMap: {
-        //         get: () => 1
-        //       },
-        //       queryParamMap: {
-        //         get: (key: string) => {
-        //           switch (key) {
-        //             case 'propertyId':
-        //               return 2;
-        //             case 'lastKnownOwnerId':
-        //               return 12;
-        //             case 'isNewValuation':
-        //               return true;
-        //           }
-        //         }
-        //       }
-        //     },
-        //     params: of({
-        //       id: 2,
-        //     })
-        //   }
-        // },
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
@@ -141,19 +94,13 @@ fdescribe('ValuationDetailEditComponent', () => {
   }));
 
   beforeEach(() => {
-    // activatedRoute = new ActivatedRouteStub();
     fixture = TestBed.createComponent(ValuationDetailEditComponent);
     component = fixture.componentInstance;
     component.isNewValuation = true;
-    // TestBed.inject(ValuationService);
-    // TestBed.inject(PropertyService);
-    // TestBed.inject(ContactGroupsService);
     storageMapSpy.get.and.returnValue(of(MockDropdownListInfo));
     fixture.detectChanges();
     propertyService = TestBed.inject(PropertyService);
     valuationService = TestBed.inject(ValuationService);
-
-    // let service = new PropertyService(null)
   });
 
   describe('Template', () => {
@@ -346,7 +293,6 @@ fdescribe('ValuationDetailEditComponent', () => {
     }));
 
     it('should get valuation', fakeAsync(() => {
-      const valuation = mockVals[0] as unknown as Valuation;
       spyOn(valuationService, 'getValuation').and.returnValue(of(valuation));
 
       component.getValuation(1234);
@@ -360,7 +306,6 @@ fdescribe('ValuationDetailEditComponent', () => {
     // utility functions
     it('should correctly calculate weekly rent given the monthly rent', () => {
       const expectedRent = 923.08;
-
       const actualRent = component.calculateWeeklyRent(4000);
 
       expect(+actualRent).toEqual(expectedRent);
@@ -368,21 +313,18 @@ fdescribe('ValuationDetailEditComponent', () => {
 
     it('should correctly calculate monthly rent given the weekly rent', () => {
       const expectedRent = 4333.33;
-
       const actualRent = component.calculateMonthlyRent(1000);
 
       expect(+actualRent).toEqual(expectedRent);
     });
 
     it('should correctly set valuation type for a lettings valuation', () => {
-
       component.setValuationType(valuation);
 
       expect(component.isLettingsOnly).toEqual(true);
     });
 
     it('should correctly set valuation type for a lettings only valuation', () => {
-
       component.setValuationType(valuation);
 
       expect(component.isLettingsOnly).toEqual(true);
@@ -423,38 +365,5 @@ function clickOn(button) {
   fixture.detectChanges();
 }
 
-class Page {
-  // getter properties wait to query the DOM until called.
-  get buttons() { return this.queryAll<HTMLButtonElement>('button'); }
-  get saveBtn() { return this.buttons[0]; }
-  get cancelBtn() { return this.buttons[1]; }
-  get nameDisplay() { return this.query<HTMLElement>('span'); }
-  get nameInput() { return this.query<HTMLInputElement>('input'); }
 
-  gotoListSpy: jasmine.Spy;
-  navigateSpy: jasmine.Spy;
 
-  constructor(fixtureLocal: ComponentFixture<ValuationDetailEditComponent>) {
-
-    // get the navigate spy from the injected router spy object
-    const routerSpy = <any>fixtureLocal.debugElement.injector.get(Router);
-    this.navigateSpy = routerSpy.navigate;
-
-    // spy on component's `gotoList()` method
-    const componentLocal = fixtureLocal.componentInstance;
-    // this.gotoListSpy = spyOn(componentLocal, 'gotoList').and.callThrough();
-  }
-
-  //// query helpers ////
-  private query<T>(selector: string): T {
-    return fixture.nativeElement.querySelector(selector);
-  }
-
-  private queryAll<T>(selector: string): T[] {
-    return fixture.nativeElement.querySelectorAll(selector);
-  }
-}
-
-function createRouterSpy() {
-  return jasmine.createSpyObj('Router', ['navigate']);
-}
