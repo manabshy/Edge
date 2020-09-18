577f3254a4d2bb0f71d63410e734ce51051ced7f
dngAlexA <aagyapong@dng.co.uk>
2020-09-18 15:10:38 +0100

    Cypress Test: Add new property from property center

---
+++ cypress/integration/property/property-center.spec.ts
@@ -0,0 +1,59 @@
+describe('Property center should', () => {
+  const newPropertyId = 0;
+  beforeEach(() => {
+    cy.visit('/property-centre');
+  });
+
+  // it('show login page', () => {
+  //   cy.visit('/login');
+  //   cy.contains('p', ' Sign in with your work email');
+  // });
+
+  it('search for properties using a search term', () => {
+    cy.get('[type="search"]')
+      .type('king')
+      .should('have.value', 'king');
+    cy.get('.btn-secondary')
+      .should('have.text', 'Search')
+      .click();
+    cy.get('.list-group-item').should('be.visible');
+  });
+
+  it('navigate to create new property from the property center ', () => {
+
+    cy.get('#addNewProperty').click();
+    cy.visit(`/property-centre/detail/${newPropertyId}/edit?isNewProperty=true`);
+  });
+
+  it('add new property ', () => {
+    const propertyType = '1'; // House
+    const propertyStyle = '1'; // Semi-Detached
+    const addressLine2 = 'Test address line 4';
+    const area = '7'; // Battersea
+    const subArea = '2'; // Other areas(Battersea)
+    const postCode = 'sw11 1af';
+
+    cy.get('#addNewProperty').click();
+    cy.visit(`/property-centre/detail/${newPropertyId}/edit?isNewProperty=true`);
+    cy.get('#fullAddress').type(postCode);
+    cy.get('#searchAddress').click();
+    cy.get('app-subnav-item').click({ multiple: true });
+    cy.wait(1000);
+    cy.get('app-subnav').get('app-subnav-item').click({ multiple: true, force: true });
+    cy.wait(1000);
+    // cy.get('.off-canvas') .invoke('attr', 'style', 'display: block')
+    // .should('have.attr', 'style', 'display: block');
+    // cy.get('#flatNumber').type('300');
+    // cy.get('#office').click({multiple:true}).type('2')
+    // cy.get('#houseNumber').type('300');
+    cy.get('#addressLine2').type(addressLine2);
+    cy.get('#propertyTypeId').select(propertyType)
+      .get('#propertyStyleId').select(propertyStyle);
+    cy.get('#area').select(area)
+      .get('#subArea').select(subArea);
+    cy.get('#saveProperty').click();
+    cy.get('#confirm').click();
+
+  });
+
+});
--- cypress/integration/valuations/valuation-edit.spec.ts
+++ cypress/integration/valuations/valuation-edit.spec.ts
@@ -3,9 +3,13 @@ describe('Valuation edit should', () => {
   beforeEach(() => { });
 
   it('update the details of an existing valuation', () => {
-    cy.visit('/valuations-register/detail/162269/edit');
-    cy.get('#reason').clear().type('Cypress testing here');
+    const valuationId = '162269';
+    const reasonText = 'Cypress testing here';
+
+    cy.visit(`/valuations-register/detail/${valuationId}/edit`);
+    cy.get('#reason').clear().type(reasonText);
     cy.get('#saveVal').click();
+
   });
 
   xit('add a new valuation', () => {
