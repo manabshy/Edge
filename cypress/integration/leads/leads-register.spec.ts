105844076c265323ef9e54f371de972e0a1d65cb
dngAlexA <aagyapong@dng.co.uk>
2020-09-21 12:22:43 +0100

    Cypress Test: Filter leads by office

--- cypress/integration/leads/leads-register.spec.ts
+++ cypress/integration/leads/leads-register.spec.ts
@@ -1,33 +1,39 @@
 describe('Leads register should', () => {
-  const newPropertyId = 0;
+  const postCode = 'sw11 6se';
+  const leadTypeId = '6'; // Vendor
+  const leadType = 'Vendor';
+
   beforeEach(() => {
     cy.visit('/leads-register');
   });
 
-  it('search for leads using a search term and lead type', () => {
-    const postCode = 'sw11 6se';
-    const leadTypeId = '6'; // Vendor
-    const leadType = 'Vendor';
+  xit('search for leads using a search term and lead type', () => {
+
 
     cy.get('[type="search"]')
       .type(postCode)
       .should('have.value', postCode);
-      cy.get('#leadType').select(leadTypeId);
-      cy.wait(2000);
+    cy.get('#leadType').select(leadTypeId);
+    cy.wait(2000);
     cy.get('#search')
       .should('have.text', 'Search')
       .click();
     cy.get('#leadTypeValue').should('have.text', leadType); // Assertion not needed;
   });
 
-  xit('filter leads by type', () => {
-    cy.get('[type="search"]')
-      .type('king')
-      .should('have.value', 'king');
-    cy.get('.btn-secondary')
-      .should('have.text', 'Search')
-      .click();
-    cy.get('.list-group-item').should('be.visible');
+  it('filter leads by office', () => {
+    // cy.get('[type="search"]')
+    //   .type('king')
+    //   .should('have.value', 'king');
+    cy.get('ng-select').then((selects) => {
+      const select = selects[0];
+      cy.wrap(select).click({ multiple: true })
+        .get('ng-dropdown-panel') // get the opened drop-down panel
+        .get('.ng-option') // Get all the options in drop-down
+        .then((items) => {
+          cy.wrap(items[0]).click({ multiple: false });
+        });
+    });
   });
 
   // it('navigate to create new property from the property center ', () => {
