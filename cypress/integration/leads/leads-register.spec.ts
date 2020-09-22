c4e349b2cb70e681301342b572206eafaaaaeb60
dngAlexA <aagyapong@dng.co.uk>
2020-09-22 14:41:57 +0100

    Cypress Test: Use all filters to search for leads

--- cypress/integration/leads/leads-register.spec.ts
+++ cypress/integration/leads/leads-register.spec.ts
@@ -1,15 +1,15 @@
 describe('Leads register should', () => {
-  const postCode = 'sw11 6se';
+  let postCode = 'sw11 6se';
   const leadTypeId = '6'; // Vendor
   const leadType = 'Vendor';
+  const dateFrom = '01/01/2020';
+  const dateTo = '21/09/2020';
 
   beforeEach(() => {
     cy.visit('/leads-register');
   });
 
-  xit('search for leads using a search term and lead type', () => {
-
-
+  it('search for leads using a search term and lead type', () => {
     cy.get('[type="search"]')
       .type(postCode)
       .should('have.value', postCode);
@@ -22,9 +22,6 @@ describe('Leads register should', () => {
   });
 
   it('filter leads by office', () => {
-    // cy.get('[type="search"]')
-    //   .type('king')
-    //   .should('have.value', 'king');
     cy.get('ng-select').then((selects) => {
       const select = selects[0];
       cy.wrap(select).click({ multiple: true })
@@ -34,6 +31,32 @@ describe('Leads register should', () => {
           cy.wrap(items[0]).click({ multiple: false });
         });
     });
+  });
+
+  it('use all filters to search for leads', () => {
+    postCode = 'sw12 8ag';
+    cy.get('[type="search"]')
+      .type(postCode)
+      .should('have.value', postCode);
+    cy.get('#leadType').select(leadTypeId);
+    cy.wait(2000);
+
+    cy.get('#dateFrom').type(dateFrom);
+    cy.get('#dateTo').type(dateTo);
+
+    cy.get('ng-select').then((selects) => {
+      const select = selects[0];
+      cy.wrap(select).click({ multiple: true })
+        .get('ng-dropdown-panel') // get the opened drop-down panel
+        .get('.ng-option') // Get all the options in drop-down
+        .then((items) => {
+          cy.wrap(items[0]).click({ multiple: false });
+        });
+    });
+
+    cy.get('#search')
+      .should('have.text', 'Search')
+      .click();
   });
 
   // it('navigate to create new property from the property center ', () => {
