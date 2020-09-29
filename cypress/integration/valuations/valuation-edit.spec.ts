a1b7bbb963978ac8b653e70df1c84682c7617da3
dngAlexA <aagyapong@dng.co.uk>
2020-09-29 13:14:38 +0100

    Cypress Test: Value a booked valuation

--- cypress/integration/valuations/valuation-edit.spec.ts
+++ cypress/integration/valuations/valuation-edit.spec.ts
@@ -1,9 +1,14 @@
 describe('Valuation edit should', () => {
+  let valuationId = '162278'; // 162282
+  const saleValue = 500000;
+  const shortLetWeeklyRent = 1000;
+  const longLetWeeklyRent = 900;
+  const monthlyRent = 5300;
 
   beforeEach(() => { });
 
   it('update the details of an existing valuation', () => {
-    const valuationId = '162269';
+    valuationId = '162269';
     const reasonText = 'Cypress testing here';
 
     cy.visit(`/valuations-register/detail/${valuationId}/edit`);
@@ -12,9 +17,27 @@ describe('Valuation edit should', () => {
 
   });
 
-  xit('add a new valuation', () => {
-    cy.visit('/valuations-register/detail/0/edit?isNewValuation=true');
-    cy.get('app-property-finder').click();
+  it('value a booked valuation', () => {
+    cy.visit(`/valuations-register/detail/${valuationId}/edit`);
+    cy.get('[data-cy=valuationValues]').then(() => {
+      cy.get('[data-cy=sellValue]').clear().type(saleValue.toString());
+      cy.get('[data-cy=shortLetValue]').clear().type(shortLetWeeklyRent.toString());
+      cy.get('[data-cy=longLetValue]').clear().type(longLetWeeklyRent.toString());
+
+      cy.get('#saveVal').click();
+      cy.get('[data-cy=sellValue]').should('have.value', saleValue.toString());
+    });
   });
 
+  it.only('instruct a valuation', () => {
+    cy.visit(`/valuations-register/detail/${valuationId}/edit`);
+    cy.get('[data-cy=valuationValues]').then(() => {
+      cy.get('[data-cy=sellValue]').clear().type(saleValue.toString());
+      cy.get('[data-cy=shortLetValue]').clear().type(shortLetWeeklyRent.toString());
+      cy.get('[data-cy=longLetValue]').clear().type(longLetWeeklyRent.toString());
+
+      cy.get('#saveVal').click();
+      cy.get('[data-cy=sellValue]').should('have.value', saleValue.toString());
+    });
+  });
 });
