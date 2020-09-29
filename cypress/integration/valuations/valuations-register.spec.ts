c1707a1e4b860894ae9d249ab71a03060cabb3c3
dngAlexA <aagyapong@dng.co.uk>
2020-09-29 11:05:45 +0100

    Create a new valuation from valuation register

--- cypress/integration/valuations/valuations-register.spec.ts
+++ cypress/integration/valuations/valuations-register.spec.ts
@@ -3,6 +3,10 @@ describe('Valuation register should', () => {
   const dateFrom = '01/01/2020';
   const dateTo = '21/09/2020';
   const status = 'Booked' // id: 2, value: 'Booked'
+  const reason = 'A good reason for e2e testing';
+  const timeFrame = 'Late summer';
+  const generalNotes = 'Notes for end to end testing';
+
   beforeEach(() => {
     cy.visit('/valuations-register');
   });
@@ -18,7 +22,39 @@ describe('Valuation register should', () => {
     cy.get('[data-cy=valStatusLabel]').first().should('have.text', status);
   });
 
-  xit('create new valuation ', () => {
+  it.only('create new valuation ', () => {
+
+    cy.get('[data-cy=newVal]').click();
+    cy.get('[data-cy=addProperty]').click();
+    cy.get('[data-cy=propSearchTerm]').type('road');
+
+    cy.get('[data-cy=searchProperty]').click();
+    cy.get('app-subnav').get('app-subnav-item').last().click();
+
+    cy.get('[data-cy=reason]').type(reason);
+    cy.get('[data-cy=timeFrame]').type(timeFrame);
+    cy.get('[data-cy=generalNotes]').type(generalNotes);
+
+    cy.get('[data-cy=valSales]').click();
+
+    cy.get('[data-cy=salesValuers]').then((select) => {
+      cy.wrap(select).click({ multiple: true })
+        .get('ng-dropdown-panel') // get the opened drop-down panel
+        .get('.ng-option') // Get all the options in drop-down
+        .then((items) => { cy.wrap(items[2]).click(); });
+    });
+    // cy.get('[data-cy=leaseLength]').clear().type('100');
+
+    cy.get('[data-cy=getvailability]').click();
+    cy.get('[data-cy=searchAvailability]').click();
+
+
+    cy.get('app-subnav').get('app-subnav-item').first().click();
+    cy.get('[data-cy=saveVal]').click();
+
+    cy.get('[data-cy=reason]').should('have.value', reason);
+    cy.get('[data-cy=timeFrame]').should('have.value', timeFrame);
+    cy.get('[data-cy=generalNotes]').should('have.value', generalNotes);
 
     // add new property from the property edit
   });
