d80068999ebfc6bae9c571a5136f75da84c022e5
dngAlexA <aagyapong@dng.co.uk>
2020-09-29 15:51:52 +0100

    Cypress Test: Instruct a sales valuation

--- cypress/integration/valuations/valuation-edit.spec.ts
+++ cypress/integration/valuations/valuation-edit.spec.ts
@@ -7,7 +7,7 @@ describe('Valuation edit should', () => {
 
   beforeEach(() => { });
 
-  it('update the details of an existing valuation', () => {
+  xit('update the details of an existing valuation', () => {
     valuationId = '162269';
     const reasonText = 'Cypress testing here';
 
@@ -17,7 +17,7 @@ describe('Valuation edit should', () => {
 
   });
 
-  it('value a booked valuation', () => {
+  xit('value a booked valuation', () => {
     cy.visit(`/valuations-register/detail/${valuationId}/edit`);
     cy.get('[data-cy=valuationValues]').then(() => {
       cy.get('[data-cy=sellValue]').clear().type(saleValue.toString());
@@ -29,15 +29,21 @@ describe('Valuation edit should', () => {
     });
   });
 
-  it.only('instruct a valuation', () => {
+  xit('instruct a lettings valuation', () => {
+     valuationId = '162282'; // 162282
     cy.visit(`/valuations-register/detail/${valuationId}/edit`);
-    cy.get('[data-cy=valuationValues]').then(() => {
-      cy.get('[data-cy=sellValue]').clear().type(saleValue.toString());
-      cy.get('[data-cy=shortLetValue]').clear().type(shortLetWeeklyRent.toString());
-      cy.get('[data-cy=longLetValue]').clear().type(longLetWeeklyRent.toString());
+    cy.get('[data-cy=startInstruction]').click();
+    cy.get('[data-cy=instructLet]').check({ force: true });
+    cy.get('[data-cy=soleAgencyLettings]').check({ force: true });
+    cy.get('[data-cy=saveInstruction]').click();
+  });
 
-      cy.get('#saveVal').click();
-      cy.get('[data-cy=sellValue]').should('have.value', saleValue.toString());
-    });
+  it.only('instruct a sales valuation', () => {
+     valuationId = '162282'; // 162282
+    cy.visit(`/valuations-register/detail/${valuationId}/edit`);
+    cy.get('[data-cy=startInstruction]').click();
+    cy.get('[data-cy=instructSale]').check({ force: true });
+    cy.get('[data-cy=soleAgencySales]').check({ force: true });
+    cy.get('[data-cy=saveInstruction]').click();
   });
 });
