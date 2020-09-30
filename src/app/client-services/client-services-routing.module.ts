15d1968b09723f2a4c8b6e46eb1d642f03fa227a
dngAlexA <aagyapong@dng.co.uk>
2020-09-30 12:13:00 +0100

    Add rules component to client services

--- src/app/client-services/client-services-routing.module.ts
+++ src/app/client-services/client-services-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminPanelListComponent } from './admin-panel-list/admin-panel-list.component';
 import { AdminPanelDetailsComponent } from './admin-panel-details/admin-panel-details.component';
+import { RulesComponent } from './shared/components/rules/rules.component';
 
 
 const routes: Routes = [
   { path: '', component: AdminPanelListComponent },
+  { path: 'rules', component: RulesComponent },
   { path: 'details/:id', component: AdminPanelDetailsComponent }
 ];
 
--- src/app/client-services/client-services.module.ts
+++ src/app/client-services/client-services.module.ts
@@ -5,11 +5,12 @@ import { ClientServicesRoutingModule } from './client-services-routing.module';
 import { AdminPanelListComponent } from './admin-panel-list/admin-panel-list.component';
 import { AdminPanelDetailsComponent } from './admin-panel-details/admin-panel-details.component';
 import { CoreModule } from '../core/core.module';
+import { RulesComponent } from './shared/components/rules/rules.component';
 
 
 
 @NgModule({
-  declarations: [AdminPanelListComponent, AdminPanelDetailsComponent],
+  declarations: [AdminPanelListComponent, AdminPanelDetailsComponent, RulesComponent],
   imports: [
     CoreModule,
     CommonModule,
