32a73e2ec832f9074060bbfef09a452eae750e64
dngAlexA <aagyapong@dng.co.uk>
2020-10-01 12:30:51 +0100

    Catch and show API error messages

--- src/app/client-services/admin-panel-list/admin-panel-list.component.ts
+++ src/app/client-services/admin-panel-list/admin-panel-list.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CsBoardService } from '../shared/services/cs-board.service';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { EMPTY, Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { TeamMember } from '../shared/models/team-member';
+import { SharedService } from 'src/app/core/services/shared.service';
 
 @Component({
   selector: 'app-admin-panel-list',
@@ -12,10 +13,9 @@ import { TeamMember } from '../shared/models/team-member';
 export class AdminPanelListComponent implements OnInit {
   members$ = new Observable<TeamMember[]>();
 
-  constructor(private boardService: CsBoardService) { }
+  constructor(private boardService: CsBoardService, private sharedService: SharedService) { }
 
   ngOnInit(): void {
-    this.members$ = this.boardService.getCsBoard();
+    this.members$ = this.boardService.getCsBoard().pipe(catchError((err) => { this.sharedService.showError(err); return EMPTY }));
   }
-
 }
