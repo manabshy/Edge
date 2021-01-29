0dec043ef8d973fef9c95f5a56a4cf86087e06e6
dngAlexA <aagyapong@dng.co.uk>
2021-01-29 14:06:08 +0000

    Change Confirm modal to use p-dialog (Initial)

--- src/app/core/shared/can-deactivate.guard.ts
+++ src/app/core/shared/can-deactivate.guard.ts
@@ -22,19 +22,20 @@ export class CanDeactivateGuard implements CanDeactivate<CanComponentDeactivate>
     currentRoute: ActivatedRouteSnapshot,
     currentState: RouterStateSnapshot,
     nextState?: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
-      AppUtils.prevRoute = AppUtils.prevRouteBU;
+    AppUtils.prevRoute = AppUtils.prevRouteBU;
 
-      if(!component.canDeactivate()) {
-        AppUtils.deactivateRoute = AppUtils.prevRoute;
-        const subject = new Subject<boolean>();
-        const initialState = {
-          title: 'If you leave your current changes will be lost',
-          actions: ['Stay', 'Leave']
-        };
-        const modal = this.modalService.show(ConfirmModalComponent, {ignoreBackdropClick: true, initialState});
-        modal.content.subject = subject;
-        return subject.asObservable();
-      }
-      return true;
+    if (!component.canDeactivate()) {
+      AppUtils.deactivateRoute = AppUtils.prevRoute;
+      const subject = new Subject<boolean>();
+      const initialState = {
+        title: 'If you leave your current changes will be lost',
+        actions: ['Stay', 'Leave'],
+        showModal: true
+      };
+      const modal = this.modalService.show(ConfirmModalComponent, { ignoreBackdropClick: true, initialState });
+      modal.content.subject = subject;
+      return subject.asObservable();
+    }
+    return true;
   }
 }
