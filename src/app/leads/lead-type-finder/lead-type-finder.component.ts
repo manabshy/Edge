e7bdd612c1746f88574be0173804ce89818e8145
dngAlexA <aagyapong@dng.co.uk>
2021-02-18 14:27:45 +0000

    Pass lead type id list to component

--- src/app/leads/lead-type-finder/lead-type-finder.component.ts
+++ src/app/leads/lead-type-finder/lead-type-finder.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { StorageMap } from '@ngx-pwa/local-storage';
 import { map, tap } from 'rxjs/operators';
 import { DropdownListInfo, InfoDetail, InfoService } from 'src/app/core/services/info.service';
+import { AppUtils } from 'src/app/core/shared/utils';
 
 @Component({
   selector: 'app-lead-type-finder',
@@ -11,6 +12,7 @@ import { DropdownListInfo, InfoDetail, InfoService } from 'src/app/core/services
 })
 export class LeadTypeFinderComponent implements OnInit, OnChanges {
   @Input() leadTypeId: number;
+  @Input() leadTypeIds: any;
   @Input() fullWidth: boolean;
   @Input() readOnly: boolean;
   @Input() isMultiSelect = false;
@@ -44,6 +46,14 @@ export class LeadTypeFinderComponent implements OnInit, OnChanges {
       } else {
         this.leadTypeFinderForm.get('leadTypeId').setValue(null);
       }
+      if (this.leadTypeIds) {
+        console.log('lead type ids in finder', this.leadTypeIds);
+        this.leadTypeFinderForm.patchValue({ leadTypeIds: this.leadTypeIds });
+      } else {
+        this.leadTypeFinderForm.get('leadTypeId').setValue(null);
+        console.log('here for null type ids');
+
+      }
     }
   }
 
@@ -51,6 +61,8 @@ export class LeadTypeFinderComponent implements OnInit, OnChanges {
     this.storage.get('info').subscribe((data: DropdownListInfo) => {
       if (data) {
         this.leadTypes = data.leadTypes;
+        console.log('lead yptes', this.leadTypes);
+
       } else {
         this.infoService.getDropdownListInfo()
           .pipe((map(response => response),
