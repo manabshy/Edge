b264087d18bb919fe777f27773e40a5cf82ede2a
dngAlexA <aagyapong@dng.co.uk>
2021-03-24 11:51:40 +0000

    Remove code from component in preparation for component removal

--- src/app/leads/lead.component.ts
+++ src/app/leads/lead.component.ts
@@ -15,96 +15,12 @@ const PAGE_SIZE = 20;
 })
 export class LeadComponent implements OnInit {
 
-  currentStaffMember: StaffMember;
-  leads: Lead[];
-  staffMembers: StaffMember[];
-  page = 1;
-  bottomReached = false;
-  leadSearchInfo: LeadSearchInfo;
-  searchTerm = '';
 
   constructor(private leadService: LeadsService,
     private sharedService: SharedService,
     private staffMemberService: StaffMemberService) { }
 
-  ngOnInit() {
-
-    // this.leadService.leadSearchTermChanges$.subscribe(newSearchTerm => this.searchTerm = newSearchTerm)
-
-    // Current Staff Member
-    this.staffMemberService.getCurrentStaffMember().subscribe(data => {
-      if (data) {
-        this.currentStaffMember = data;
-        this.bottomReached = false;
-
-        this.leadSearchInfo = {
-          page: this.page,
-          ownerId: this.currentStaffMember.staffMemberId,
-          personId: null,
-          leadTypeId: null,
-          officeId: null,
-          dateFrom: null,
-          dateTo: null,
-          includeClosedLeads: false,
-          includeUnassignedLeadsOnly: false,
-          leadSearchTerm: this.searchTerm ? this.searchTerm : ''
-        };
-        if (AppUtils.leadSearchInfo) {
-          console.log('from app utils', AppUtils.leadSearchInfo);
-          this.leadSearchInfo = AppUtils.leadSearchInfo;
-          // this.searchTerm = AppUtils.leadSearchInfo.leadSearchTerm;
-        }
-        // this.getLeads(this.leadSearchInfo);
-      }
-    });
-
-    // page changes here
-    this.leadService.pageChanges$.subscribe(leadSearchInfo => {
-      if (leadSearchInfo) {
-        this.page = leadSearchInfo.page;
-        // this.getLeads(leadSearchInfo);
-        // console.log('calling from here 2', this.leadSearchInfo.leadSearchTerm);
-      }
-    });
-
-    // this.getActiveStaffmembers(); Remove ASAP 22/02/21
-  }
-
-  getLeads(leadSearchInfo: LeadSearchInfo) {
-    console.log('get leads search info: ', leadSearchInfo);
-    this.leadService.getLeads(leadSearchInfo, PAGE_SIZE).subscribe(result => {
-
-      if (leadSearchInfo.page === 1) {
-        this.leads = result;
-      } else {
-        if (this.leads) {
-          this.leads = this.leads.concat(result);
-        } else {
-          this.leads = result;
-        }
-      }
-      this.setBottomReachedFlag(result);
-
-    }, () => {
-      this.leads = [];
-    });
-  }
-
-  private setBottomReachedFlag(result: any) {
-    if (result && (!result.length || result.length < +PAGE_SIZE)) {
-      this.bottomReached = true;
-    } else {
-      this.bottomReached = false;
-    }
-  }
-
-  getActiveStaffmembers() {
-    this.staffMemberService.getActiveStaffMembers().subscribe(result => {
-      this.staffMembers = result;
-    }, () => {
-      this.staffMembers = [];
-    });
-  }
+  ngOnInit() { }
 
 
 }
