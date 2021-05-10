f699bab26c17915036159885c8686a32f610fb59
dngAlexA <aagyapong@dng.co.uk>
2021-05-10 09:20:36 +0100

    Remove redundant code and unsubscribe from label subject

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, NavigationEnd, NavigationStart, Router } from '@angular/router';
-import { StorageMap } from '@ngx-pwa/local-storage';
-import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
-import { filter } from 'rxjs/operators';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { DynamicDialogRef } from 'primeng/dynamicdialog';
+import { filter, takeUntil } from 'rxjs/operators';
 import { AuthService } from '../core/services/auth.service';
 import { HeaderService } from '../core/services/header.service';
-import { StaffMemberService } from '../core/services/staff-member.service';
-import { ImpersonateMemberComponent } from '../impersonate-member/impersonate-member.component';
+import { BaseComponent } from '../shared/models/base-component';
 import { BaseStaffMember } from '../shared/models/base-staff-member';
 import { StaffMember } from '../shared/models/staff-member';
 
@@ -15,7 +13,7 @@ import { StaffMember } from '../shared/models/staff-member';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent extends BaseComponent implements OnInit {
   currentStaffMember: StaffMember;
   navTitle: string;
   headerLabel: string;
@@ -25,18 +23,15 @@ export class HeaderComponent implements OnInit {
   isImpersonating = false;
 
   constructor(public authService: AuthService,
-    private storage: StorageMap,
-    private staffMemberService: StaffMemberService,
-    private dialogService: DialogService,
     private headerService: HeaderService,
     private route: ActivatedRoute,
     private router: Router) {
+    super();
     this.setRouteTitle();
   }
 
   private setRouteTitle() {
     this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(() => {
-      // this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(() => {
       const childRoute = this.getChildRoute(this.route);
       childRoute.data.subscribe((route) => {
         console.log('title and route', route);
@@ -46,102 +41,17 @@ export class HeaderComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.storage.get('impersonatedStaffMember').subscribe((staffMember: BaseStaffMember) => {
-      if (staffMember) {
-        this.impersonatedStaffMember = staffMember;
-        this.isImpersonating = true;
-        console.log('selected id:', staffMember);
-      }
-    });
-
-    this.storage.get('currentUser').subscribe((data: StaffMember) => {
-      if (data) {
-        this.currentStaffMember = data;
-      } else {
-        this.staffMemberService.getCurrentStaffMember().subscribe(res => this.currentStaffMember = res);
-      }
-      console.log('current user from storage in main menu....', this.currentStaffMember);
-    });
-
     this.getLabel();
-    console.log(this.isImpersonating, 'is impersonationg');
-
-  }
-
-  logIn() {
-    this.authService.login();
-  }
-
-  logOut() {
-    this.authService.logout();
   }
 
   getChildRoute(route: ActivatedRoute): ActivatedRoute {
     if (route.firstChild) {
       return this.getChildRoute(route.firstChild);
-    } else {
-      return route;
-    }
+    } else { return route; }
   }
 
   getLabel() {
-    this.headerService.label$.subscribe(label => {
-      this.headerLabel = label; console.log({ label });
-    });
+    this.headerService.label$.pipe(takeUntil(this.ngUnsubscribe)).subscribe(label => this.headerLabel = label);
   }
 
-  impersonate(stop = false) {
-    console.log({ stop });
-    if (stop) {
-      this.showImpersonation = false;
-      this.isImpersonating = false;
-      this.storage.delete('signature').subscribe();
-      localStorage.removeItem('impersonatedStaffMemberId');
-      this.storage.delete('impersonatedStaffMember').subscribe();
-      this.storage.get('originalUser').subscribe((data: StaffMember) => {
-        if (data) {
-          this.currentStaffMember = data;
-          this.storage.set('currentUser', data).subscribe(() => {
-            this.storage.delete('originalUser').subscribe();
-            location.reload();
-          });
-        }
-      });
-    } else {
-      this.showImpersonation = true;
-    }
-    // if (!stop) {
-    //   this.showImpersonation = true;
-    //   // this.ref = this.dialogService.open(ImpersonateMemberComponent,{styleClass: 'dialog dialog--medium', header:'Select Staff Member'})
-    // }
-  }
-
-  getSelectedStaffMember(member: BaseStaffMember) {
-    console.log({ member });
-    this.impersonatedStaffMember = member;
-  }
-
-  startImpersonation() {
-    this.storage.set('originalUser', this.currentStaffMember).subscribe();
-    this.storage.delete('currentUser').subscribe();
-    this.storage.delete('signature').subscribe();
-    this.isImpersonating = true;
-    this.showImpersonation = false;
-
-    // this.currentStaffMember.fullName = this.impersonatedStaffMember.fullName;
-    // this.currentStaffMember.photoUrl = null;
-    this.storage.set('impersonatedStaffMember', this.impersonatedStaffMember).subscribe();
-    localStorage.setItem('impersonatedStaffMemberId', JSON.stringify(this.impersonatedStaffMember.staffMemberId));
-    this.staffMemberService.impersonatedStaffMemberChanged(this.impersonatedStaffMember);
-    // this.getCurrentUser();
-
-  }
-
-  private getCurrentUser() {
-    this.staffMemberService.getCurrentStaffMember().subscribe(res => {
-      this.currentStaffMember = res;
-      console.log({ res }, 'new person');
-      location.reload();
-    });
-  }
 }
