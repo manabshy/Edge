11bb33a9740953b12a2347a1c1b117914f58e304
Ramazan ALP <ralp@dng.co.uk>
2021-06-01 17:02:50 +0100

    Add notes section stays unchanged on accessing previous leads

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,63 +1,93 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-import { HomeComponent } from './home/home.component';
-import { AuthGuardService } from './core/services/auth-guard.service';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { SelectiveStrategyService } from './core/services/selective-strategy.service';
-import { ImpersonateMemberComponent } from './impersonate-member/impersonate-member.component';
-import { MsalGuard } from '@azure/msal-angular';
-import { CalendarComponent } from './calendar-shared/calendar/calendar.component';
+import { HomeComponent } from "./home/home.component";
+import { AuthGuardService } from "./core/services/auth-guard.service";
+import { NotFoundComponent } from "./not-found/not-found.component";
+import { SelectiveStrategyService } from "./core/services/selective-strategy.service";
+import { ImpersonateMemberComponent } from "./impersonate-member/impersonate-member.component";
+import { MsalGuard } from "@azure/msal-angular";
+import { CalendarComponent } from "./calendar-shared/calendar/calendar.component";
 
 const routes: Routes = [
-  { path: 'impersonate-member', component: ImpersonateMemberComponent, canActivate: [MsalGuard] },
   {
-    path: 'leads-register',
-    data: { preload: true },
-    loadChildren: () => import('./leads/leads.module').then(m => m.LeadsModule),
+    path: "impersonate-member",
+    component: ImpersonateMemberComponent,
     canActivate: [MsalGuard],
   },
   {
-    path: 'valuations-register',
+    path: "leads-register",
     data: { preload: true },
-    loadChildren: () => import('./valuations/valuations.module').then(m => m.ValuationsModule),
-    canActivate: [MsalGuard]
+    loadChildren: () =>
+      import("./leads/leads.module").then((m) => m.LeadsModule),
+    canActivate: [MsalGuard],
   },
   {
-    path: 'property-centre',
+    path: "valuations-register",
     data: { preload: true },
-    loadChildren: () => import('./property/property.module').then(m => m.PropertyModule),
-    canActivate: [MsalGuard]
+    loadChildren: () =>
+      import("./valuations/valuations.module").then((m) => m.ValuationsModule),
+    canActivate: [MsalGuard],
   },
   {
-    path: 'contact-centre',
-    loadChildren: () => import('./contactgroups/contactgroups.module').then(m => m.ContactgroupsModule), canActivate: [MsalGuard]
+    path: "property-centre",
+    data: { preload: true },
+    loadChildren: () =>
+      import("./property/property.module").then((m) => m.PropertyModule),
+    canActivate: [MsalGuard],
   },
   {
-    path: 'company-centre',
-    loadChildren: () => import('./company/company.module').then(m => m.CompanyModule), canActivate: [MsalGuard]
+    path: "contact-centre",
+    loadChildren: () =>
+      import("./contactgroups/contactgroups.module").then(
+        (m) => m.ContactgroupsModule
+      ),
+    canActivate: [MsalGuard],
   },
   {
-    path: 'diary',
-    loadChildren: () => import('./diary/diary.module').then(m => m.DiaryModule), canActivate: [MsalGuard]
+    path: "company-centre",
+    loadChildren: () =>
+      import("./company/company.module").then((m) => m.CompanyModule),
+    canActivate: [MsalGuard],
   },
   {
-    path: 'admin-panel',
-    loadChildren: () => import('./client-services/client-services.module').then(m => m.ClientServicesModule), canActivate: [MsalGuard]
+    path: "diary",
+    loadChildren: () =>
+      import("./diary/diary.module").then((m) => m.DiaryModule),
+    canActivate: [MsalGuard],
   },
   {
-    path: 'dashboard',
-    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule), canActivate: [MsalGuard]
+    path: "admin-panel",
+    loadChildren: () =>
+      import("./client-services/client-services.module").then(
+        (m) => m.ClientServicesModule
+      ),
+    canActivate: [MsalGuard],
   },
-  { path: '', component: CalendarComponent, canActivate: [MsalGuard], data: { title: 'Calendar' } },
+  {
+    path: "dashboard",
+    loadChildren: () =>
+      import("./dashboard/dashboard.module").then((m) => m.DashboardModule),
+    canActivate: [MsalGuard],
+  },
+  {
+    path: "",
+    component: CalendarComponent,
+    canActivate: [MsalGuard],
+    data: { title: "Calendar" },
+  },
   // { path: '', component: HomeComponent, canActivate: [MsalGuard], data: { title: 'Calendar' } },
-  { path: '**', component: NotFoundComponent }
+  { path: "**", component: NotFoundComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled', relativeLinkResolution: 'legacy', onSameUrlNavigation: 'reload' })],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      scrollPositionRestoration: "enabled",
+      relativeLinkResolution: "legacy",
+      onSameUrlNavigation: "reload",
+    }),
+  ],
+  exports: [RouterModule],
 })
-
-export class AppRoutingModule { }
-
+export class AppRoutingModule {}
--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,24 +1,29 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { MyDashboardComponent } from './my-dashboard/my-dashboard.component';
-import { TeamDashboardComponent } from './team-dashboard/team-dashboard.component';
-import { DashboardComponent } from './dashboard.component';
-import { DashboardListComponent } from './dashboard-list/dashboard-list.component';
-import { HomeComponent } from '../home/home.component';
-import { AuthGuardService } from '../core/services/auth-guard.service';
-import { InstructionsAndBusinessDevelopmentComponent } from './dashboard-list/instructions-and-business-development/instructions-and-business-development.component';
-import { ExchangesAndPipelineComponent } from './dashboard-list/exchanges-and-pipeline/exchanges-and-pipeline.component';
-import { MsalGuard } from '@azure/msal-angular';
-import { OverviewComponent } from './overview/overview.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { MyDashboardComponent } from "./my-dashboard/my-dashboard.component";
+import { TeamDashboardComponent } from "./team-dashboard/team-dashboard.component";
+import { DashboardComponent } from "./dashboard.component";
+import { DashboardListComponent } from "./dashboard-list/dashboard-list.component";
+import { HomeComponent } from "../home/home.component";
+import { AuthGuardService } from "../core/services/auth-guard.service";
+import { InstructionsAndBusinessDevelopmentComponent } from "./dashboard-list/instructions-and-business-development/instructions-and-business-development.component";
+import { ExchangesAndPipelineComponent } from "./dashboard-list/exchanges-and-pipeline/exchanges-and-pipeline.component";
+import { MsalGuard } from "@azure/msal-angular";
+import { OverviewComponent } from "./overview/overview.component";
 
 const routes: Routes = [
-  { path: 'overview', component: OverviewComponent, canActivate: [MsalGuard], data: { title: 'Overview' } },
+  {
+    path: "overview",
+    component: OverviewComponent,
+    canActivate: [MsalGuard],
+    data: { title: "Overview" },
+  },
   // { path: 'list/:id', component: DashboardListComponent, canActivate: [MsalGuard] },
   // { path: 'instructionsAndBdd/:id', component: InstructionsAndBusinessDevelopmentComponent, canActivate: [MsalGuard] },
   // { path: 'exchangesAndPipeline/:id', component: ExchangesAndPipelineComponent, canActivate: [MsalGuard] },
 ];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class DashboardRoutingModule { }
+export class DashboardRoutingModule {}
