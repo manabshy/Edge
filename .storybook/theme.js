0c142f5f4422397922222e0f1c6eeefebd240ceb
Dave Beazer <dbeazer@dng.co.uk>
2021-09-21 12:32:49 +0100

    add Storybook

---
+++ .storybook/theme.js
@@ -0,0 +1,13 @@
+// .storybook/YourTheme.js
+
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  appBg: '#0a1a4a',
+  brandTitle: 'Douglas & Gordon storybook',
+  brandUrl: 'https://example.com',
+  brandImage: '/assets/dng_logo.png',
+  textColor: 'white',
+  textInverseColor: 'rgba(55,55,55,0.9)',
+});
