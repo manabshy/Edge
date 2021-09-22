b3069d3e2ec79b2e93b128d900fdd89ad607c563
Dave Beazer <dbeazer@dng.co.uk>
2021-09-22 10:32:16 +0100

    Broken out component and vendor modules separately

--- src/app/diary/diary.module.ts
+++ src/app/diary/diary.module.ts
@@ -1,30 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { DiaryRoutingModule } from './diary-routing.module';
 import { AddDiaryEventComponent } from './add-diary-event/add-diary-event.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { CoreModule } from '../core/core.module';
-import { CalendarComponent } from '../calendar-shared/calendar/calendar.component';
-import { CalendarHeaderComponent } from '../calendar-shared/calendar-header.component';
-import { CalendarModule } from 'angular-calendar';
-import { calendar } from 'ngx-bootstrap/chronos/moment/calendar';
-import { SharedModule } from '../shared/shared.module';
+import { ComponentsModule } from '../shared/components.module';
 
 @NgModule({
   declarations: [
     AddDiaryEventComponent
-    // CalendarComponent,
-    // CalendarHeaderComponent
   ],
   imports: [
     CommonModule,
-    SharedModule,
+    ComponentsModule,
     DiaryRoutingModule,
     ReactiveFormsModule,
-    // CoreModule,
-    // CalendarModule
   ],
-  // exports:[CalendarComponent]
 })
 export class DiaryModule { }
