bb301d907a378a4fec8f3a03277a0ce103b95fe3
Dave Beazer <dbeazer@dng.co.uk>
2021-10-21 08:17:32 +0100

    fix build

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,5 +1,9 @@
+import { version } from '../../package.json'
+
 export const environment = {
+  version,
   production: true,
+  hmr: false,
   baseUrl: 'https://dandg-api-wedge-test.azurewebsites.net/v10',
   baseRedirectUri: 'https://dandg-edge-test.azurewebsites.net',
   endpointUrl: 'https://dandg-api-wedge-test.azurewebsites.net'
