a4f16194f854179257b657ecb0ad881cdeb2fc21
Ramazan ALP <ralp@dng.co.uk>
2021-10-27 14:07:35 +0100

    creating new valuation

--- src/app/leads/lead-register/lead-register.component.ts
+++ src/app/leads/lead-register/lead-register.component.ts
@@ -1,31 +1,24 @@
-import { Component, OnInit, Input } from "@angular/core";
-import { LeadsService } from "../shared/leads.service";
-import { StaffMemberService } from "src/app/core/services/staff-member.service";
-import { Lead, LeadSearchInfo, ListingType } from "../shared/lead";
-import { StaffMember, Office } from "src/app/shared/models/staff-member";
-import { StorageMap } from "@ngx-pwa/local-storage";
-import {
-  DropdownListInfo,
-  InfoDetail,
-  InfoService,
-} from "src/app/core/services/info.service";
-import { FormGroup, FormBuilder, FormControl } from "@angular/forms";
-import {
-  SharedService,
-  WedgeError,
-} from "src/app/core/services/shared.service";
-import { AppUtils } from "src/app/core/shared/utils";
-import * as _ from "lodash";
-import { Router } from "@angular/router";
-import { MessageService } from "primeng/api";
-import { BsLocaleService } from "ngx-bootstrap/datepicker";
-import { addDays } from "date-fns";
+import { Component, OnInit, Input } from '@angular/core';
+import { LeadsService } from '../shared/leads.service';
+import { StaffMemberService } from 'src/app/core/services/staff-member.service';
+import { Lead, LeadSearchInfo, ListingType } from '../shared/lead';
+import { StaffMember, Office } from 'src/app/shared/models/staff-member';
+import { StorageMap } from '@ngx-pwa/local-storage';
+import { DropdownListInfo, InfoDetail, InfoService } from 'src/app/core/services/info.service';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
+import { SharedService, WedgeError } from 'src/app/core/services/shared.service';
+import { AppUtils } from 'src/app/core/shared/utils';
+import * as _ from 'lodash';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { BsLocaleService } from 'ngx-bootstrap/datepicker';
+import { addDays } from 'date-fns';
 
 const PAGE_SIZE = 40;
 @Component({
-  selector: "app-lead-register",
-  templateUrl: "../lead-register/lead-register.component.html",
-  styleUrls: ["../lead-register/lead-register.component.scss"],
+  selector: 'app-lead-register',
+  templateUrl: '../lead-register/lead-register.component.html',
+  styleUrls: ['../lead-register/lead-register.component.scss'],
 })
 export class LeadRegisterComponent implements OnInit {
   @Input() bottomReached: boolean;
@@ -44,7 +37,7 @@ export class LeadRegisterComponent implements OnInit {
   enableOwnerFilter = true;
   selectedLeadsForAssignment: Lead[] = [];
   isSelectAllChecked = false;
-  searchTerm = "";
+  searchTerm = '';
   errorMessage: WedgeError;
   info: string;
   isClosedIncluded: boolean;
@@ -52,7 +45,7 @@ export class LeadRegisterComponent implements OnInit {
   isAdvancedSearchVisible = false;
   showModal = false;
   newLeadOwnerId: number;
-  locale = "en-gb";
+  locale = 'en-gb';
   isLoading = false;
   queryResultCount: number;
   isClosedLeadFound = false;
@@ -60,29 +53,29 @@ export class LeadRegisterComponent implements OnInit {
   defaultListingType = ListingType.MyLeads;
 
   leadFilters: { name: string; value: number }[] = [
-    { value: 1, name: "My leads" },
-    { value: 2, name: "Other user leads" },
-    { value: 4, name: "Unassigned leads" },
+    { value: 1, name: 'My leads' },
+    { value: 2, name: 'Other user leads' },
+    { value: 4, name: 'Unassigned leads' },
   ];
 
   get officeIdsControl(): FormControl {
-    return this.leadRegisterForm?.get("officeIds") as FormControl;
+    return this.leadRegisterForm?.get('officeIds') as FormControl;
   }
 
   get leadTypeIdsControl(): FormControl {
-    return this.leadRegisterForm?.get("leadTypeIds") as FormControl;
+    return this.leadRegisterForm?.get('leadTypeIds') as FormControl;
   }
 
   get dateFromControl(): FormControl {
-    return this.leadRegisterForm?.get("dateFrom") as FormControl;
+    return this.leadRegisterForm?.get('dateFrom') as FormControl;
   }
 
   get dateToControl(): FormControl {
-    return this.leadRegisterForm?.get("dateTo") as FormControl;
+    return this.leadRegisterForm?.get('dateTo') as FormControl;
   }
 
   get listingTypeControl(): FormControl {
-    return this.leadRegisterForm?.get("listingType") as FormControl;
+    return this.leadRegisterForm?.get('listingType') as FormControl;
   }
 
   constructor(
@@ -94,7 +87,7 @@ export class LeadRegisterComponent implements OnInit {
     private messageService: MessageService,
     private localeService: BsLocaleService,
     private router: Router,
-    private fb: FormBuilder
+    private fb: FormBuilder,
   ) {
     this.localeService.use(this.locale);
   }
@@ -108,12 +101,12 @@ export class LeadRegisterComponent implements OnInit {
     // New search term
     this.leadService.leadSearchTermChanges$.subscribe((newSearchTerm) => {
       if (newSearchTerm) {
-        this.leadRegisterForm.get("leadSearchTerm").setValue(newSearchTerm);
+        this.leadRegisterForm.get('leadSearchTerm').setValue(newSearchTerm);
         this.leadSearchInfo = this.getSearchInfo(true);
       } else {
-        AppUtils.leadSearchTerm = "";
+        AppUtils.leadSearchTerm = '';
         this.leadRegisterForm.patchValue({
-          leadSearchTerm: "",
+          leadSearchTerm: '',
         });
       }
       this.PerformSearch();
@@ -126,20 +119,14 @@ export class LeadRegisterComponent implements OnInit {
   }
 
   getLeadsForCurrentUser() {
-    this.storage.get("currentUser").subscribe((data: StaffMember) => {
+    this.storage.get('currentUser').subscribe((data: StaffMember) => {
       if (data) {
         this.currentStaffMember = data;
         this.bottomReached = false;
-        const seeAllLeadsPermission = this.currentStaffMember.permissions.find(
-          (x) => x.permissionId === 69
-        );
-        seeAllLeadsPermission
-          ? (this.canSeeUnassignable = true)
-          : (this.canSeeUnassignable = false);
-        this.defaultListingType = this.canSeeUnassignable
-          ? ListingType.OtherUserLeads
-          : ListingType.MyLeads;
-        localStorage.setItem("listingType", this.defaultListingType.toString());
+        const seeAllLeadsPermission = this.currentStaffMember.permissions.find((x) => x.permissionId === 69);
+        seeAllLeadsPermission ? (this.canSeeUnassignable = true) : (this.canSeeUnassignable = false);
+        this.defaultListingType = this.canSeeUnassignable ? ListingType.OtherUserLeads : ListingType.MyLeads;
+        localStorage.setItem('listingType', this.defaultListingType.toString());
         this.page = 0;
         this.leadSearchInfo = {
           page: this.page,
@@ -152,7 +139,7 @@ export class LeadRegisterComponent implements OnInit {
           listingType: this.defaultListingType,
           includeClosedLeads: false,
           includeUnassignedLeadsOnly: false,
-          leadSearchTerm: this.searchTerm ? this.searchTerm : "",
+          leadSearchTerm: this.searchTerm ? this.searchTerm : '',
         };
         if (AppUtils.leadSearchInfo) {
           this.leadSearchInfo = AppUtils.leadSearchInfo;
@@ -168,17 +155,15 @@ export class LeadRegisterComponent implements OnInit {
 
   getInfoFromLocalStorage() {
     // Lead Types
-    this.storage.get("info").subscribe((data: DropdownListInfo) => {
+    this.storage.get('info').subscribe((data: DropdownListInfo) => {
       if (data) {
         this.leadTypes = data.leadTypes;
       } else {
-        this.infoService
-          .getDropdownListInfo()
-          .subscribe((res) => (this.leadTypes = res.leadTypes));
+        this.infoService.getDropdownListInfo().subscribe((res) => (this.leadTypes = res.leadTypes));
       }
     });
 
-    this.storage.get("activeStaffmembers").subscribe((data) => {
+    this.storage.get('activeStaffmembers').subscribe((data) => {
       if (data) {
         this.staffMembers = data as StaffMember[];
       }
@@ -188,14 +173,14 @@ export class LeadRegisterComponent implements OnInit {
   setupLeadRegisterForm() {
     this.leadRegisterForm = this.fb.group({
       ownerId: [],
-      officeIds: [""],
+      officeIds: [''],
       leadTypeIds: [],
       includeClosedLeads: [],
       dateFrom: [],
       dateTo: [],
       listingType: [this.defaultListingType],
       includeUnassignedLeadsOnly: [false],
-      leadSearchTerm: [""],
+      leadSearchTerm: [''],
     });
   }
 
@@ -204,8 +189,8 @@ export class LeadRegisterComponent implements OnInit {
     this.leadService.getLeads(leadSearchInfo, PAGE_SIZE).subscribe((result) => {
       if (result) {
         this.isLoading = false;
-
-        this.queryResultCount = result[0].leadsQueryCount;
+        this.queryResultCount = 0;
+        if (result.length > 0) this.queryResultCount = result[0].leadsQueryCount;
 
         if (leadSearchInfo.page === 1) {
           this.filteredLeads = result;
@@ -270,9 +255,7 @@ export class LeadRegisterComponent implements OnInit {
   }
 
   getSelectedLeads() {
-    this.selectedLeadsForAssignment = this.filteredLeads.filter(
-      (x) => x.isChecked
-    );
+    this.selectedLeadsForAssignment = this.filteredLeads.filter((x) => x.isChecked);
   }
 
   onOwnerChanged(event: any) {
@@ -286,7 +269,7 @@ export class LeadRegisterComponent implements OnInit {
       });
       this.leadSearchInfo = this.getSearchInfo(true);
     } else {
-      this.leadRegisterForm.patchValue({ leadSearchTerm: "" });
+      this.leadRegisterForm.patchValue({ leadSearchTerm: '' });
     }
   }
 
@@ -340,23 +323,21 @@ export class LeadRegisterComponent implements OnInit {
 
   assignLeadsToOwner() {
     if (this.newLeadOwnerId) {
-      this.leadService
-        .assignLeads(this.newLeadOwnerId, this.selectedLeadsForAssignment)
-        .subscribe((result) => {
-          if (result) {
-            this.messageService.add({
-              severity: "success",
-              summary: "Lead(s) successfully assigned!",
-              closable: false,
-              key: "assignmentMessage",
-            });
-            this.areLeadsAssignable = false;
-            this.selectedLeadsForAssignment = [];
-            this.staffMemberService.clearSelectedStaffMember(true);
-            this.replaceLeadsWithNewOwners(result);
-            this.showModal = false;
-          }
-        });
+      this.leadService.assignLeads(this.newLeadOwnerId, this.selectedLeadsForAssignment).subscribe((result) => {
+        if (result) {
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Lead(s) successfully assigned!',
+            closable: false,
+            key: 'assignmentMessage',
+          });
+          this.areLeadsAssignable = false;
+          this.selectedLeadsForAssignment = [];
+          this.staffMemberService.clearSelectedStaffMember(true);
+          this.replaceLeadsWithNewOwners(result);
+          this.showModal = false;
+        }
+      });
     }
   }
 
@@ -367,15 +348,12 @@ export class LeadRegisterComponent implements OnInit {
       let newInfo: LeadSearchInfo;
       if (this.isSubmitting) {
         newInfo = { ...this.leadSearchInfo, ...this.leadRegisterForm.value };
-        localStorage.setItem(
-          "listingType",
-          this.leadSearchInfo.listingType.toString()
-        );
+        localStorage.setItem('listingType', this.leadSearchInfo.listingType.toString());
       } else {
-        const listingType = +localStorage.getItem("listingType");
+        const listingType = +localStorage.getItem('listingType');
         newInfo = { ...this.leadSearchInfo, listingType };
         this.listingTypeControl.setValue(listingType);
-        console.log({ newInfo }, { listingType }, "not submitting");
+        console.log({ newInfo }, { listingType }, 'not submitting');
       }
       newInfo.startLeadId = lead.leadId;
       newInfo.ownerId = lead.ownerId;
@@ -387,14 +365,14 @@ export class LeadRegisterComponent implements OnInit {
         leadSearchInfo: this.info,
       };
       this.leadService.previousLeadQueryParam.next(queryParams);
-      this.router.navigate(["/leads-register/edit", lead.leadId], {
+      this.router.navigate(['/leads-register/edit', lead.leadId], {
         queryParams: queryParams,
       });
     }
   }
 
   getSelectedOfficeId(officeIds: number[]) {
-    this.leadRegisterForm.get("officeIds").setValue(officeIds);
+    this.leadRegisterForm.get('officeIds').setValue(officeIds);
   }
 
   getSelectedTypes(types: number[]) {
@@ -406,7 +384,7 @@ export class LeadRegisterComponent implements OnInit {
       this.leadSearchInfo = this.getSearchInfo(true);
     }
     const url = this.router.url;
-    const isLeadsRegister = url.endsWith("/leads-register");
+    const isLeadsRegister = url.endsWith('/leads-register');
 
     if (isLeadsRegister) {
       if (!this.bottomReached) {
@@ -423,9 +401,7 @@ export class LeadRegisterComponent implements OnInit {
       for (const lead of this.filteredLeads) {
         newLeads.forEach((x) => {
           if (x.leadId === lead.leadId) {
-            index = this.filteredLeads.findIndex(
-              (l) => l.leadId === lead.leadId
-            );
+            index = this.filteredLeads.findIndex((l) => l.leadId === lead.leadId);
             if (this.filteredLeads[index]) {
               this.filteredLeads[index] = x;
             }
@@ -440,24 +416,20 @@ export class LeadRegisterComponent implements OnInit {
   }
 
   clearDate(type: string) {
-    if (type === "to") {
+    if (type === 'to') {
       this.dateToControl.setValue(null);
-    } else if (type === "from") {
+    } else if (type === 'from') {
       this.dateFromControl.setValue(null);
     }
   }
 
   isLeadOutDated(lead: Lead, index): string {
-    if (
-      lead &&
-      lead.nextChaseDate &&
-      new Date(lead.nextChaseDate).getTime() < addDays(new Date(), -1).getTime()
-    ) {
+    if (lead && lead.nextChaseDate && new Date(lead.nextChaseDate).getTime() < addDays(new Date(), -1).getTime()) {
       if (index % 2 === 0) {
-        return "#F1AFA8";
+        return '#F1AFA8';
       }
-      return "#FAE1DF";
+      return '#FAE1DF';
     }
-    return "";
+    return '';
   }
 }
