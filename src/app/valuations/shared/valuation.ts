f1bb3234e1e44330ac5a4563558e1376bedfd5b0
Ramazan ALP <ralp@dng.co.uk>
2021-11-01 09:20:29 +0000

    valuations origin

--- src/app/valuations/shared/valuation.ts
+++ src/app/valuations/shared/valuation.ts
@@ -1,171 +1,172 @@
-import { StaffMember } from './../../shared/models/staff-member';
+import { StaffMember } from './../../shared/models/staff-member'
 
-import { DiaryEvent } from 'src/app/diary/shared/diary';
-import { BaseProperty } from 'src/app/shared/models/base-property';
-import { BaseRequestOption } from 'src/app/shared/models/base-request-option';
-import { BaseStaffMember } from 'src/app/shared/models/base-staff-member';
-import { Property } from 'src/app/property/shared/property';
-import { Office } from 'src/app/shared/models/staff-member';
-import { EdgeFile } from 'src/app/shared/models/edgeFile';
-import { BaseNote, Signer } from 'src/app/contact-groups/shared/contact-group';
+import { DiaryEvent } from 'src/app/diary/shared/diary'
+import { BaseProperty } from 'src/app/shared/models/base-property'
+import { BaseRequestOption } from 'src/app/shared/models/base-request-option'
+import { BaseStaffMember } from 'src/app/shared/models/base-staff-member'
+import { Property } from 'src/app/property/shared/property'
+import { Office } from 'src/app/shared/models/staff-member'
+import { EdgeFile } from 'src/app/shared/models/edgeFile'
+import { BaseNote, Signer } from 'src/app/contact-groups/shared/contact-group'
 
 export interface ValuationInfo {
-  reason?: string;
-  timeFrame?: string;
-  generalNotes?: string;
+  reason?: string
+  timeFrame?: string
+  generalNotes?: string
 }
 export interface Valuation extends ValuationInfo {
-  valuationEventId?: number;
-  valuationStatus?: number;
-  valuationStatusDescription?: string;
-  officeId?: number;
-  valuationStatusLabel?: string;
-  property?: Property;
-  salesValuer?: BaseStaffMember;
-  lettingsValuer?: BaseStaffMember;
-  valuationDate?: Date;
-  propertyOwner?: Signer;
-  diaryEvent?: DiaryEvent;
-  bedrooms?: number;
-  bathrooms?: number;
-  receptions?: number;
-  sqFt?: number;
-  tenureId?: number;
-  originId?: number;
-  approxLeaseExpiryDate?: Date;
-  outsideSpace?: any;
-  parking?: any;
-  propertyFeature?: number[];
-  otherFeatures?: number[];
-  diaryEventId?: number;
-  startDateTime?: Date;
-  endDateTime?: Date;
-  startTime?: string;
-  totalHours?: number;
-  suggestedAskingPrice?: number;
-  suggestedAskingRentLongLet?: number;
-  suggestedAskingRentShortLet?: number;
-  suggestedAskingRentLongLetMonthly?: number;
-  suggestedAskingRentShortLetMonthly?: number;
-  createdDate?: Date;
-  createdBy?: BaseStaffMember;
-  isActive?: boolean;
-  isExpired?: boolean;
-  section21StatusId?: number;
-  declarableInterest?: boolean;
-  meetingOwner?: boolean;
-  combinedValuationBooking?: ValuationBooking;
-  salesValuationBooking?: ValuationBooking;
-  lettingsValuationBooking?: ValuationBooking;
-  queryResultCount?: number;
-  isPowerOfAttorney?: boolean;
-  ccOwner?: boolean;
-  adminContact?: Signer;
-  valuationFiles?: ValuationFile[];
-  valuationType?: ValuationTypeEnum;
-  dateRequestSent?: Date;
-  cancellationReason?: string;
-  cancelledBy?: StaffMember;
-  cancelledDate?: Date;
-  cancellationTypeId?: number;
-  valuationContactNote?: valuationNote;
-  ownerConfirmed?: number;
-  deedLandRegFiles?: EdgeFile[];
-  leaseLandRegFiles?: EdgeFile[];
-  nameChangeRegFiles?: EdgeFile[];
-  userEnteredOwner?: string;
-  leaseExpiryDate?: Date;
-  complianceCheck?: ComplianceChecksStatus;
-  lockDate?: Date;
+  valuationEventId?: number
+  valuationStatus?: number
+  valuationStatusDescription?: string
+  officeId?: number
+  valuationStatusLabel?: string
+  property?: Property
+  salesValuer?: BaseStaffMember
+  lettingsValuer?: BaseStaffMember
+  valuationDate?: Date
+  propertyOwner?: Signer
+  diaryEvent?: DiaryEvent
+  bedrooms?: number
+  bathrooms?: number
+  receptions?: number
+  sqFt?: number
+  tenureId?: number
+  originId?: number
+  bookedBy?: BaseStaffMember
+  approxLeaseExpiryDate?: Date
+  outsideSpace?: any
+  parking?: any
+  propertyFeature?: number[]
+  otherFeatures?: number[]
+  diaryEventId?: number
+  startDateTime?: Date
+  endDateTime?: Date
+  startTime?: string
+  totalHours?: number
+  suggestedAskingPrice?: number
+  suggestedAskingRentLongLet?: number
+  suggestedAskingRentShortLet?: number
+  suggestedAskingRentLongLetMonthly?: number
+  suggestedAskingRentShortLetMonthly?: number
+  createdDate?: Date
+  createdBy?: BaseStaffMember
+  isActive?: boolean
+  isExpired?: boolean
+  section21StatusId?: number
+  declarableInterest?: boolean
+  meetingOwner?: boolean
+  combinedValuationBooking?: ValuationBooking
+  salesValuationBooking?: ValuationBooking
+  lettingsValuationBooking?: ValuationBooking
+  queryResultCount?: number
+  isPowerOfAttorney?: boolean
+  ccOwner?: boolean
+  adminContact?: Signer
+  valuationFiles?: ValuationFile[]
+  valuationType?: ValuationTypeEnum
+  dateRequestSent?: Date
+  cancellationReason?: string
+  cancelledBy?: StaffMember
+  cancelledDate?: Date
+  cancellationTypeId?: number
+  valuationContactNote?: valuationNote
+  ownerConfirmed?: number
+  deedLandRegFiles?: EdgeFile[]
+  leaseLandRegFiles?: EdgeFile[]
+  nameChangeRegFiles?: EdgeFile[]
+  userEnteredOwner?: string
+  leaseExpiryDate?: Date
+  complianceCheck?: ComplianceChecksStatus
+  lockDate?: Date
 }
 
 export interface ComplianceChecksStatus {
-  compliancePassedByFullName: string;
-  compliancePassedDate: string;
+  compliancePassedByFullName: string
+  compliancePassedDate: string
 }
 
 export interface ValuationPricingInfo {
-  suggestedAskingRentLongLetMonthly?: number;
-  suggestedAskingRentShortLetMonthly?: number;
+  suggestedAskingRentLongLetMonthly?: number
+  suggestedAskingRentShortLetMonthly?: number
 }
 
 export interface ValuationFile {
-  blobFileTypeId: number;
-  fileLastModified: Date;
-  fileUri: string;
+  blobFileTypeId: number
+  fileLastModified: Date
+  fileUri: string
 }
 
 export interface ValuationPropertyInfo {
-  propertyId: number;
-  bedrooms: number;
-  bathrooms: number;
-  receptions: number;
-  sqFt: number;
-  tenureId: number;
-  approxLeaseExpiryDate?: any;
-  parking?: any;
-  outsideSpace?: any;
-  propertyFeature?: any;
-  valuers: BaseStaffMember[];
+  propertyId: number
+  bedrooms: number
+  bathrooms: number
+  receptions: number
+  sqFt: number
+  tenureId: number
+  approxLeaseExpiryDate?: any
+  parking?: any
+  outsideSpace?: any
+  propertyFeature?: any
+  valuers: BaseStaffMember[]
 }
 
 export interface OfficeMember {
-  office: Office;
-  staffMembers: BaseStaffMember[];
+  office: Office
+  staffMembers: BaseStaffMember[]
 }
 
 export interface Valuer {
-  sales: OfficeMember[];
-  lettings: OfficeMember[];
+  sales: OfficeMember[]
+  lettings: OfficeMember[]
 }
 export interface ValuationStaffMembersCalanderEvents {
-  thisWeek: Date[];
-  nextWeek: Date[];
-  next2Weeks: Date[];
+  thisWeek: Date[]
+  nextWeek: Date[]
+  next2Weeks: Date[]
 }
 export interface CalendarAvailibility {
-  availableDates: ValuationStaffMembersCalanderEvents[];
+  availableDates: ValuationStaffMembersCalanderEvents[]
 }
 export interface ValuersAvailabilityOption extends BaseRequestOption {
-  fromDate?: string;
-  lettingsValuerId?: number;
-  salesValuerId?: number;
+  fromDate?: string
+  lettingsValuerId?: number
+  salesValuerId?: number
 }
 export interface ValuationRequestOption extends BaseRequestOption {
-  status?: number[];
-  date?: string;
-  valuerId?: number[];
-  officeId?: number[];
+  status?: number[]
+  date?: string
+  valuerId?: number[]
+  officeId?: number[]
 }
 
 export interface ValuationBooking {
-  valuationBookingId?: number;
-  diaryEventId?: number;
-  name?: string;
-  emailAddress?: string;
-  contactNumber?: string;
-  associationId?: number;
-  meetingOwner?: boolean;
-  startDateTime?: Date;
-  totalHours?: number;
+  valuationBookingId?: number
+  diaryEventId?: number
+  name?: string
+  emailAddress?: string
+  contactNumber?: string
+  associationId?: number
+  meetingOwner?: boolean
+  startDateTime?: Date
+  totalHours?: number
 }
 
 export interface valuationNote extends BaseNote {
-  dateLastUpdated?: Date;
-  lastUpdatedByFullName?: string;
-  contactGroupId?: number;
-  valuationEventId?: number;
+  dateLastUpdated?: Date
+  lastUpdatedByFullName?: string
+  contactGroupId?: number
+  valuationEventId?: number
 }
 
 export interface CancelValuation {
-  valuationEventId?: number;
-  typeId?: number;
-  reason?: string;
+  valuationEventId?: number
+  typeId?: number
+  reason?: string
 }
 
 export interface ValuationStatus {
-  id: number;
-  value: string;
+  id: number
+  value: string
 }
 
 export const ValuationStatuses = <ValuationStatus[]>[
@@ -173,8 +174,8 @@ export const ValuationStatuses = <ValuationStatus[]>[
   { id: 3, value: 'Valued' },
   { id: 4, value: 'Instructed' },
   { id: 5, value: 'Cancelled' },
-  { id: 6, value: 'Closed' },
-];
+  { id: 6, value: 'Closed' }
+]
 
 export enum ValuationStatusEnum {
   None = 0,
@@ -182,7 +183,7 @@ export enum ValuationStatusEnum {
   Valued = 3,
   Instructed = 4,
   Cancelled = 5,
-  Closed = 6,
+  Closed = 6
 }
 
 export enum ValuationCancellationReasons {
@@ -192,13 +193,13 @@ export enum ValuationCancellationReasons {
   Change_of_circumstance = 4,
   Agent_not_allowing_access = 5,
   Did_not_want_sales_or_lettings_val = 6,
-  Other = 7,
+  Other = 7
 }
 
 export enum ValuationTypeEnum {
   None = 0,
   Sales = 1,
-  Lettings = 2,
+  Lettings = 2
 }
 
 export enum ValuationActions {
@@ -208,19 +209,19 @@ export enum ValuationActions {
   lettingsTermsofBusiness = 3,
   landLordQuestionnaire = 4,
   vendorQuestionnaire = 5,
-  cancelValuation = 6,
+  cancelValuation = 6
 }
 
 export function getValuationStatuses() {
-  return Object.keys(ValuationStatusEnum).filter((type) => isNaN(<any>type));
+  return Object.keys(ValuationStatusEnum).filter((type) => isNaN(<any>type))
 }
 
 export enum SalesAgencyTypeEnum {
   Sole = 1,
-  Multi = 4,
+  Multi = 4
 }
 
 export enum LettingsAgencyTypeEnum {
   Sole = 1,
-  Multi = 2,
+  Multi = 2
 }
--- src/app/valuations/valuations.module.ts
+++ src/app/valuations/valuations.module.ts
@@ -1,16 +1,17 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { SharedModule } from "../shared/shared.module";
-import { CalendarSharedModule } from "../calendar-shared/calendar-shared.module";
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { SharedModule } from '../shared/shared.module'
+import { CalendarSharedModule } from '../calendar-shared/calendar-shared.module'
 
-import { ValuationsRoutingModule } from "./valuations-routing.module";
-import { ValuationsComponent } from "./valuations.component";
-import { ValuationListComponent } from "./valuation-list/valuation-list.component";
-import { ValuationDetailEditComponent } from "./valuation-detail-edit/valuation-detail-edit.component";
-import { ValuationsLandRegisterComponent } from "./valuation-land-register/valuation-land-register.component";
-import { CancelValuationComponent } from './cancel-valuation/cancel-valuation.component';
-import { TermsOfBusinessModule } from "./terms-of-business/terms-of-business.module";
-import { ComplianceModule } from "./compliance/compliance.module";
+import { ValuationsRoutingModule } from './valuations-routing.module'
+import { ValuationsComponent } from './valuations.component'
+import { ValuationListComponent } from './valuation-list/valuation-list.component'
+import { ValuationDetailEditComponent } from './valuation-detail-edit/valuation-detail-edit.component'
+import { ValuationsLandRegisterComponent } from './valuation-land-register/valuation-land-register.component'
+import { CancelValuationComponent } from './cancel-valuation/cancel-valuation.component'
+import { TermsOfBusinessModule } from './terms-of-business/terms-of-business.module'
+import { ComplianceModule } from './compliance/compliance.module'
+import { ValuationOriginComponent } from './valuation-origin/valuation-origin.component'
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { ComplianceModule } from "./compliance/compliance.module";
     ValuationDetailEditComponent,
     ValuationsLandRegisterComponent,
     CancelValuationComponent,
+    ValuationOriginComponent
   ],
   imports: [
     CommonModule,
