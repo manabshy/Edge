24aa3145688982bd008f9b05c87007be989f0bbb
Dave Beazer <dbeazer@dng.co.uk>
2021-11-03 15:10:11 +0000

    file tidy

--- src/app/shared/components/company-finder/company-finder.component.ts
+++ src/app/shared/components/company-finder/company-finder.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter, Input, OnDestroy } from '@angular/core'
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core'
 import { ContactGroupsService } from 'src/app/contact-groups/shared/contact-groups.service'
 import { Company, CompanyAutoCompleteResult } from 'src/app/contact-groups/shared/contact-group'
 import { distinctUntilChanged, switchMap, tap, catchError } from 'rxjs/operators'
@@ -8,22 +8,20 @@ import { Router } from '@angular/router'
 @Component({
   selector: 'app-company-finder',
   template: `
-    <div>
-      <app-pure-company-finder-shell
-        [searchResults]="foundCompanies"
-        [existingCompany]="existingCompany"
-        [hasBeenSearched]="hasBeenSearched"
-        [noSuggestions]="noSuggestions"
-        [suggestions]="suggestions"
-        [companyNameError]="companyNameError"
-        [existingIds]="existingIds"
-        (companyName)="companyName.emit($event)"
-        [canCreateNewCompany]="canCreateNewCompany"
-        (searchCompanyEmitter)="findCompany($event)"
-        (selectedCompanyDetails)="selectedCompanyDetails.emit($event)"
-        (onManualEntry)="isManualEntry($event)"
-      ></app-pure-company-finder-shell>
-    </div>
+    <app-pure-company-finder-shell
+      [searchResults]="foundCompanies"
+      [existingCompany]="existingCompany"
+      [hasBeenSearched]="hasBeenSearched"
+      [noSuggestions]="noSuggestions"
+      [suggestions]="suggestions"
+      [companyNameError]="companyNameError"
+      [existingIds]="existingIds"
+      (companyName)="companyName.emit($event)"
+      [canCreateNewCompany]="canCreateNewCompany"
+      (searchCompanyEmitter)="findCompany($event)"
+      (selectedCompanyDetails)="selectedCompanyDetails.emit($event)"
+      (onManualEntry)="isManualEntry($event)"
+    ></app-pure-company-finder-shell>
   `
 })
 export class CompanyFinderComponent implements OnInit {
