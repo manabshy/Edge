3477176bb19e7d64569734b0c9ae46eb32a22659
Ramazan ALP <ralp@dng.co.uk>
2021-11-22 17:12:58 +0000

    new property

--- src/app/valuations/valuation-detail-edit/valuation-detail-edit.component.ts
+++ src/app/valuations/valuation-detail-edit/valuation-detail-edit.component.ts
@@ -964,13 +964,13 @@ export class ValuationDetailEditComponent extends BaseComponent implements OnIni
 
   private setupListInfo(info: DropdownListInfo) {
     this.tenures = [{ id: 0, value: 'Not Known' }, ...info.tenures]
-    this.outsideSpaces = info.outsideSpaces
-    this.parkings = info.parkings
-    this.features = info.propertyFeatures
-    this.allOrigins = info.origins
+    this.outsideSpaces = [...info.outsideSpaces]
+    this.parkings = [...info.parkings]
+    this.features = [...info.propertyFeatures]
+    this.allOrigins = [...info.origins]
     this.allOriginTypes = info.originTypes.filter((x) => x.id == 12 || x.id == 13 || x.id == 14)
-    this.interestList = info.section21Statuses
-    this.associateTypes = info.associations
+    this.interestList = [...info.section21Statuses]
+    this.associateTypes = [...info.associations]
     this.propertyTypes = [{ id: 0, value: ' ' }, ...info.propertyTypes]
     this.allPropertyStyles = [{ id: 0, value: ' ' }, ...info.propertyStyles]
     this.propertyStyles = [{ id: 0, value: ' ' }, ...info.propertyStyles]
@@ -1797,11 +1797,11 @@ export class ValuationDetailEditComponent extends BaseComponent implements OnIni
 
   private getAddedProperty() {
     this.propertyService.newPropertyAdded$.pipe(takeUntil(this.ngUnsubscribe)).subscribe((newProperty) => {
-      if (newProperty) {
+      if (newProperty && newProperty.propertyId != this.property?.propertyId) {
+        this.getPropertyInformation(newProperty.propertyId)
         this.property = newProperty
         this.showProperty = false
         this.valuationForm.get('property').setValue(this.property)
-        this.getValuers(this.property.propertyId)
         // this.getContactGroup(newProperty.lastKnownOwner?.contactGroupId);
         this.getSelectedOwner(newProperty.lastKnownOwner)
       }
