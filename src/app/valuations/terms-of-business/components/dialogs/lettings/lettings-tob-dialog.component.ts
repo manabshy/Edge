8287285bbf5ef11a3f587c1a9d690d4a0f96a0f5
Dave Beazer <dbeazer@dng.co.uk>
2021-11-22 16:28:59 +0000

    4.0.0-30

--- src/app/valuations/terms-of-business/components/dialogs/lettings/lettings-tob-dialog.component.ts
+++ src/app/valuations/terms-of-business/components/dialogs/lettings/lettings-tob-dialog.component.ts
@@ -15,6 +15,10 @@ import { FileTypeEnum } from 'src/app/core/services/file.service'
       (onHide)="close()"
     >
       <div class="flex flex-col" style="width: 500px">
+        <ng-container>
+          <app-messages [message]="message"></app-messages>
+        </ng-container>
+
         <app-file-upload
           [uploadedFiles]="tmpFiles"
           (getFiles)="getFiles($event)"
@@ -23,7 +27,6 @@ import { FileTypeEnum } from 'src/app/core/services/file.service'
         ></app-file-upload>
 
         <form [formGroup]="form" class="my-4 ml-4">
-         
           <fieldset class="mb-2">
             <fieldset class="row">
               <label style="width: auto; margin-top: 10px">Will D&G be managing the property?</label>
@@ -39,18 +42,30 @@ import { FileTypeEnum } from 'src/app/core/services/file.service'
               </span>
             </span>
           </fieldset>
-         
+
           <fieldset class="mb-2 animated fadeIn" *ngIf="model.isManagement === false">
             <fieldset class="row">
               <label style="width: auto; margin-top: 10px">Will the Landlord be managing the property?</label>
             </fieldset>
             <span class="radio radio--inline">
               <span style="margin-right: 8px">
-                <input class="p-2" type="radio" id="isLandlordManagedYes" [value]="true" formControlName="isLandlordManaged" />
+                <input
+                  class="p-2"
+                  type="radio"
+                  id="isLandlordManagedYes"
+                  [value]="true"
+                  formControlName="isLandlordManaged"
+                />
                 <label for="isLandlordManagedYes" class="mb-1">Yes</label>
               </span>
               <span style="margin-right: 8px">
-                <input class="p-2" type="radio" id="isLandlordManagedNo" [value]="false" formControlName="isLandlordManaged" />
+                <input
+                  class="p-2"
+                  type="radio"
+                  id="isLandlordManagedNo"
+                  [value]="false"
+                  formControlName="isLandlordManaged"
+                />
                 <label for="isLandlordManagedNo" class="mb-1">No</label>
               </span>
             </span>
@@ -62,11 +77,23 @@ import { FileTypeEnum } from 'src/app/core/services/file.service'
             </fieldset>
             <span class="radio radio--inline">
               <span style="margin-right: 8px">
-                <input class="p-2" type="radio" id="zeroDepositAcceptedYes" [value]="true" formControlName="zeroDepositAccepted" />
+                <input
+                  class="p-2"
+                  type="radio"
+                  id="zeroDepositAcceptedYes"
+                  [value]="true"
+                  formControlName="zeroDepositAccepted"
+                />
                 <label for="zeroDepositAcceptedYes" class="mb-1">Yes</label>
               </span>
               <span style="margin-right: 8px">
-                <input class="p-2" type="radio" id="zeroDepositAcceptedNo" [value]="false" formControlName="zeroDepositAccepted" />
+                <input
+                  class="p-2"
+                  type="radio"
+                  id="zeroDepositAcceptedNo"
+                  [value]="false"
+                  formControlName="zeroDepositAccepted"
+                />
                 <label for="zeroDepositAcceptedNo" class="mb-1">No</label>
               </span>
             </span>
@@ -81,7 +108,7 @@ import { FileTypeEnum } from 'src/app/core/services/file.service'
               type="button"
               class="btn btn--positive"
               (click)="submit()"
-              [disabled]="!fileUploaded && !form.valid"
+              [disabled]="!fileUploaded || !form.valid"
               data-cy="uploadFiles"
             >
               Submit
@@ -100,9 +127,13 @@ export class LettingsToBDialogComponent implements OnInit, OnDestroy {
   form: FormGroup
   model: any = {}
   fileType = FileTypeEnum.ImageAndDocument
-  tmpFiles: File[]
-  fileUploaded: boolean = true
+  tmpFiles: File[] = []
+  fileUploaded: boolean = false
   formSub: Subscription
+  message: any = {
+    type: 'info',
+    text: ['Please upload Terms of Business and complete the form below']
+  }
 
   constructor(private fb: FormBuilder) {}
 
@@ -123,31 +154,86 @@ export class LettingsToBDialogComponent implements OnInit, OnDestroy {
 
     this.formSub = this.form.valueChanges.subscribe((data) => {
       this.model = { ...this.model, ...data }
+      this.updateFormValidators(data)
+      this.updateMessages()
     })
   }
 
   ngOnDestroy() {
+    this.clearDialog()
     this.formSub.unsubscribe()
   }
 
   public getFiles(files): void {
     this.tmpFiles = files
     this.fileUploaded = !!files ? true : false
+    this.updateMessages()
   }
 
   public submit() {
+    if (this.formIsValid()) {
+      const payload = this.buildPayload()
+      this.onSubmitTermsOfBusiness.emit(payload)
+      this.showDialog = false
+    }
+  }
+
+  public close() {
+    this.clearDialog()
+    this.updateMessages()
+    this.onSubmitTermsOfBusiness.emit(false)
+    this.showDialog = false
+  }
+
+  private updateFormValidators(formData) {
+    if (!formData.isManagement) {
+      this.form.controls['isLandlordManaged'].setValidators([Validators.required])
+    } else {
+      this.form.controls['isLandlordManaged'].clearValidators()
+    }
+  }
+
+  private formIsValid() {
+    if (!this.form.valid || !this.tmpFiles.length) return false
+    return true
+  }
+
+  private buildPayload() {
     const payload = {
       model: this.model,
       file: this.tmpFiles
     }
-    if (!this.form.valid || !this.tmpFiles.length) return
     payload.model.managedByTypeId = this.model.isManaged ? 1 : this.model.isLandlordManaged ? 2 : 0
-    this.onSubmitTermsOfBusiness.emit(payload)
-    this.showDialog = false
   }
 
-  public close() {
-    this.onSubmitTermsOfBusiness.emit(false)
-    this.showDialog = false
+  private clearDialog() {
+    this.model = {
+      isManagement: null,
+      isLandlordManaged: null,
+      zeroDepositAccepted: null,
+      managedByTypeId: null
+    }
+    this.tmpFiles = []
+    this.form.patchValue({
+      isManagement: null,
+      isLandlordManaged: null,
+      zeroDepositAccepted: null
+    })
+  }
+
+  private updateMessages() {
+    if (!this.form.valid && !this.tmpFiles.length) {
+      this.message.type = 'info'
+      this.message.text = ['Please upload Terms of Business and complete the form below']
+    } else if (this.form.valid && !this.tmpFiles.length) {
+      this.message.type = 'warn'
+      this.message.text = ['Please upload Terms of Business document']
+    } else if (!this.form.valid && this.tmpFiles.length) {
+      this.message.type = 'warn'
+      this.message.text = ['Please complete the form']
+    } else if (this.form.valid && this.tmpFiles.length) {
+      this.message.type = 'success'
+      this.message.text = ['Ready to upload']
+    }
   }
 }
