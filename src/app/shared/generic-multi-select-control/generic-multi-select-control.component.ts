209f8f72fddb191fdeda4e5677034935ac1816cb
Ramazan ALP <ralp@dng.co.uk>
2021-12-03 11:21:55 +0000

    multi-select options

--- src/app/shared/generic-multi-select-control/generic-multi-select-control.component.ts
+++ src/app/shared/generic-multi-select-control/generic-multi-select-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core'
 
 /***
  * @description a generic select control that displays a list of options and triggers a callback on change
@@ -11,31 +11,30 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-generic-multi-select-control',
   template: `
-  <form >
-  <fieldset>
-    <label>{{label}}</label>
-    <ng-container>
-      <p-multiSelect 
-        #multiSelect
-        [attr.data-cy]="cyProp"
-        [(ngModel)]="model"
-        [options]="options" 
-        optionLabel="value" 
-        optionValue="id"
-        placeholder="{{placeholder}}" 
-        (onChange)="selectionHasChanged($event)"
-        name="{{label}}"
-       >
-      </p-multiSelect>
-    </ng-container>
+    <form>
+      <fieldset>
+        <label>{{ label }}</label>
+        <ng-container>
+          <p-multiSelect
+            #multiSelect
+            [attr.data-cy]="cyProp"
+            [(ngModel)]="model"
+            [options]="options"
+            optionLabel="value"
+            optionValue="id"
+            placeholder="{{ placeholder }}"
+            (onChange)="selectionHasChanged($event)"
+            name="{{ label }}"
+            selectedItemsLabel="{0} items selected"
+          ></p-multiSelect>
+        </ng-container>
 
-    <p class="message message--negative">{{label}} is required</p>
-  </fieldset>
-</form>
- `
+        <p class="message message--negative">{{ label }} is required</p>
+      </fieldset>
+    </form>
+  `
 })
 export class GenericMultiSelectControlComponent {
-
   @Input() label: string
   @Input() placeholder: string
   @Input() options: any
@@ -43,11 +42,10 @@ export class GenericMultiSelectControlComponent {
   @Input() model: any
 
   @Output() onSelectionChange: EventEmitter<Array<any>> = new EventEmitter()
-  
-  constructor() { }
 
-  selectionHasChanged(vals){
+  constructor() {}
+
+  selectionHasChanged(vals) {
     this.onSelectionChange.emit(vals.value)
   }
-
 }
