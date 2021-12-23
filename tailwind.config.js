638afa2008e273a97dd2f71cfb270dac5ac5222b
Dave Beazer <dbeazer@dng.co.uk>
2021-12-23 09:17:54 +0000

    update tailwind config for tailwind 3.x

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,61 +1,58 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   important: true,
-  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
-  // purge: {
-  //   enabled: true,
-  //   content: ['./src/**/*.html'],
-  // },
-  darkMode: false, // or 'media' or 'class'
+  content: ['./src/**/*.{js,jsx,ts,tsx,html}'],
   theme: {
     extend: {
       transitionProperty: {
-        'spacing': 'margin, padding',
+        spacing: 'margin, padding'
       },
       colors: {
-        info: '#3498DB'
+        info: '#3498DB',
+        green: colors.emerald,
+        yellow: colors.amber,
+        purple: colors.violet
       },
       fontSize: {
-        'xs': '.65rem'
+        xs: '.65rem'
       },
       margin: {
-        '0.3': '0.15rem',
-       },
-       width:{
-        '124': '40rem',
-        '148': '48rem',
-        '180': '60rem'
-       },
-       minWidth: {
-        '0': '0',
+        0.3: '0.15rem'
+      },
+      width: {
+        124: '40rem',
+        148: '48rem',
+        180: '60rem'
+      },
+      minWidth: {
+        0: '0',
         '1/4': '25%',
         '1/2': '50%',
         '3/4': '75%',
-        'full': '100%',
-       },
-       height:{
-        '124': '30rem',
-        '148': '34rem',
-        '160': '40rem'
-       },
-       minHeight: {
-        '0': '0',
-        '1/5':'10vh',
+        full: '100%'
+      },
+      height: {
+        124: '30rem',
+        148: '34rem',
+        160: '40rem'
+      },
+      minHeight: {
+        0: '0',
+        '1/5': '10vh',
         '1/4': '25vh',
         '1/2': '50vh',
         '3/4': '75vh',
-        'full': '100vh',
-       },
+        full: '100vh'
+      },
       fontFamily: {
         // https://tailwindcss.com/docs/font-family#customizing
-        sans: [
-          'Poppins'
-        ],
-      },
-    },
+        sans: ['Poppins']
+      }
+    }
   },
-  variants: {
-    extend: {
-    },
+  corePlugins: {
+    preflight: false
   },
-  plugins: [],
+  plugins: []
 }
