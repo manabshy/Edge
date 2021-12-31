867c77000491016dea96603a3d4ee8c68bb6d3a3
Dave Beazer <dave@sideprojectco.com>
2021-12-31 12:24:59 +0000

    hide other rows. add chromatic token

--- src/app/gamification/rewards/rewards-shell/rewards-shell.component.ts
+++ src/app/gamification/rewards/rewards-shell/rewards-shell.component.ts
@@ -6,8 +6,10 @@ import { Component, Input, OnInit } from '@angular/core'
     <div class="">
       <app-rewards-toolbar></app-rewards-toolbar>
       <app-rewards-row [vm]="vm.daily"></app-rewards-row>
+      <!--
       <app-rewards-row [vm]="vm.weekly"></app-rewards-row>
       <app-rewards-row [vm]="vm.monthly"></app-rewards-row>
+      -->
     </div>
   `
 })
