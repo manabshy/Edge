dbda377a29c637170574481de9654586d995eddb
Cem Ergen <cergen@dng.co.uk>
2022-01-12 14:39:29 +0000

    test baseUrl changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
   hmr: false,
   clientId: '03d5d394-2418-42fa-a345-556b8d7ffcdb',
   authority: 'https://login.microsoftonline.com/ed781348-2f1d-4f1e-bbf8-137da318df39',
-  baseUrl: '',
+  baseUrl: 'https://dandg-api-wedge-dev.azurewebsites.net/v10',  // remove after testing
   baseRedirectUri: '',
   endpointUrl: ''
 }
