beb796f57b38f38329c88b89952d2723465740c3
Manoj Bisht <mbisht@dng.co.uk>
2022-02-23 21:39:35 +0000

    Add countdown service

--- src/app/gamification/rewards/components/rewards-row/rewards-row.component.ts
+++ src/app/gamification/rewards/components/rewards-row/rewards-row.component.ts
@@ -20,7 +20,7 @@ import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/cor
     <div class="p-3">
       <p class="text-xl mb-2">{{ bonus.name }}</p>
       <div class="flex flex-wrap mx-auto gap-5">
-        <app-rewards-timer [timeframe]="timeframe"></app-rewards-timer>
+        <app-rewards-timer [timeframe]="timeframe" [countdown]="countdown" [timewindow]="bonus.timeWindow"></app-rewards-timer>
 
         <ng-container *ngFor="let bc of bonus.bonusDetailCriteria; let i = index">
         <app-rewards-task
@@ -53,7 +53,8 @@ import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/cor
 export class RewardsRowComponent implements OnInit {
   @Input() bonus: any
   @Input() streak: any
-
+  @Input() countdown: any
+ 
   timeframe: string
   goalsHit: boolean = false
 
@@ -66,16 +67,17 @@ export class RewardsRowComponent implements OnInit {
         this.timeframe = 'left of the day'
         break
 
-      case 30:
-        this.timeframe = 'to archive greatness'
+      case 2:
+        this.timeframe = 'to achieve greatness'
         break
 
       case 60: //todo
         this.timeframe = 'to secure your ticket on the slopes'
         break
     }
-
+  
     this.goalsHit = this.bonus.bonusDetailCriteria.every(bc => bc.progress >= bc.target)
     console.log('row rewards:', this.goalsHit);
+    console.log('bonus:', this.bonus);
   }
 }
